8881ca6cc79bf9755de8deaa699660b4336bb75c
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2022-02-23 15:25:06 -0800

    feat: initialize interface skeleton

---
+++ inst/include/interface/interface.hpp
@@ -0,0 +1,17 @@
+#ifndef INST_INCL_INTERFACE_INTERFACE_HPP
+#define INST_INCL_INTERFACE_INTERFACE_HPP
+/*
+* Interface file. Uses pre-processing macros
+* to interface with multiple modeling platforms.
+*/
+
+
+//traits for interfacing with TMB
+#ifdef TMB_MODEL
+
+#define TMB_LIB_INIT R_init_FIMS
+#include <TMB.hpp>
+
+#endif
+
+#endif
