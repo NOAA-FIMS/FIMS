288d20341cf8d07b63f84c993fd9beddd2dca747
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2022-02-23 15:23:40 -0800

    feat: initialize R scripts skeleton

---
+++ R/io.R
@@ -0,0 +1,12 @@
+#' fit: example function demonstrating TMB specific Roxygen comments
+#'
+#' @importFrom TMB MakeADFun
+#' @useDynLib FIMS
+#' @return TMB object
+#' @export
+#'
+#' @examples mkObj()
+mkObj <- function(){
+  obj <- MakeADFun(data = list(), parameters = list(p = 1), DLL = 'FIMS')
+}
+
