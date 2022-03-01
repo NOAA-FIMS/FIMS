c5fd2b62c78259ee0d44ab0f7c3aa2b39334cc39
Bai-Li-NOAA <Bai-Li-NOAA@users.noreply.github.com>
2022-03-01 19:11:01 +0000

    style and docs: run devtools::document() and styler::style_pkg()

--- R/io.R
+++ R/io.R
@@ -6,8 +6,7 @@
 #' @export
 #'
 #' @examples mkObj()
-mkObj <- function(){
-  obj <- MakeADFun(data = list(), parameters = list(p = 1), DLL = 'FIMS')
+mkObj <- function() {
+  obj <- MakeADFun(data = list(), parameters = list(p = 1), DLL = "FIMS")
   return(obj)
 }
-
