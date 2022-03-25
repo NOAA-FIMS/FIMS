a38be22262a5dfc60be1c8ce33115ce304f0e369
Bai Li <bai.li@noaa.gov>
2022-03-25 09:59:15 -0400

    test: add logistic selectivity test

---
+++ tests/gtest/test_population_dynamics_selectivity_logistic.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+#include "population_dynamics/selectivity/functors/logistic.hpp"
+
+namespace
+{
+
+  
+  TEST(logistic_selectivity, create_object)
+  {
+    EXPECT_EQ(1.0, 1.0);
+
+  }
+
+}
