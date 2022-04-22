fef801bae9d0448ab315037772b5446a2b2ad60a
Clang Format Robot <cfrobot@example.com>
2022-04-22 17:02:05 +0000

    Committing clang-format changes

--- inst/include/common/model.hpp
+++ inst/include/common/model.hpp
@@ -32,22 +32,21 @@
 #define FIMS_COMMON_MODEL_HPP
 
 #include <memory>
-#include "../population_dynamics/population/population.hpp"
+
 #include "../population_dynamics/fleet/fleet.hpp"
+#include "../population_dynamics/population/population.hpp"
 
-namespace fims
-{
+namespace fims {
 
-    /**
-     * Model base
-     */
-    template <typename T>
-    class Model
-    {
-        std::vector < std::shared_ptr<Population<T>> populations;
-        std::vector < std::shared_ptr<Fleet<T>> fleets;
-    };
+/**
+ * Model base
+ */
+template <typename T>
+class Model {
+  std::vector < std::shared_ptr<Population<T>> populations;
+  std::vector < std::shared_ptr<Fleet<T>> fleets;
+};
 
-} // namespace fims
+}  // namespace fims
 
 #endif /* FIMS_COMMON_MODEL_HPP */
