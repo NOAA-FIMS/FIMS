93faeb27330bdb948bea0d38091d8fe8b70b8dbd
Bai Li - NOAA <59936250+Bai-Li-NOAA@users.noreply.github.com>
2022-05-10 17:39:54 -0400

    feat: add TMB exp and log functions (#102)
    * feat: add TMB exp and log functions
    
    * chore: remove place holder files
    
    * fix: fix a typo in STD LIB

--- tests/gtest/benchmark_place_holder1.cpp
+++
@@ -1,9 +0,0 @@
-#include "benchmark/benchmark.h"
-#include "interface/place_holder1.hpp"
-
-void BM_dlgnorm(benchmark::State &state)
-{
-  for (auto _ : state)
-    dlognorm(5.0, 10.0, 2.5);
-}
-BENCHMARK(BM_dlgnorm);
