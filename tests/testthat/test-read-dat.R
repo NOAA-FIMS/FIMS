8a015d05cf362f8262382f206690444d98707ba5
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2022-05-18 11:51:57 -0700

    feat: adds weight-at-age data to input data frame
    As well as adding weight-at-age data to the input data frame,
    this commit also uses a development branch of the {ASSAMC}
    to run a single iteration of the Operating Model, and thus,
    simplifies the code needed here by quite a bit.
    
    Lines with 80 # are used to separate out major sections of the code.

--- tests/testthat/test-read-dat.R
+++ tests/testthat/test-read-dat.R
@@ -1,186 +1,152 @@
-# Use code from model comparison project to make a database
-
-#based on https://github.com/Bai-Li-NOAA/Age_Structured_Stock_Assessment_Model_Comparison#usage
-
-
-# Set up OM ---------------------------------------------------------------
-
-
-maindir <- file.path(".")
-
-om_sim_num <- 1 # total number of iterations per case
-keep_sim_num <- 1 # number of kept iterations per case
-figure_number <- 1 # number of individual iteration to plot
-
-seed_num <- 9924
-
-## Life-history parameters
-year <- 1:30
-ages <- 1:12 # Age structure of the popn
-
-initial_equilibrium_F <- TRUE
-median_R0 <- 1000000 # Average annual unfished recruitment (scales the popn)
-median_h <- 0.75 # Steepness of the Beverton-Holt spawner-recruit relationship.
-mean_R0 <- NULL
-mean_h <- NULL
-SRmodel <- 1 # 1=Beverton-Holt; 2=Ricker
-M <- 0.2 # Age-invariant natural mortality
-
-Linf <- 800 # Asymptotic average length
-K <- 0.18 # Growth coefficient
-a0 <- -1.36 # Theoretical age at size 0
-a.lw <- 0.000000025 # Length-weight coefficient
-b.lw <- 3.0 # Length-weight exponent
-A50.mat <- 2.25 # Age at 50% maturity
-slope.mat <- 3 # Slope of maturity ogive
-pattern.mat <- 1 # Simple logistic maturity
-female.proportion <- 0.5 # Sex ratio
-
-## Fleet settings
-fleet_num <- 1
-
-# CV of landings for OM
-cv.L <- list()
-cv.L$fleet1 <- 0.005
-
-# Input CV of landings for EMs
-input.cv.L <- list()
-input.cv.L$fleet1 <- 0.01
-
-# Annual sample size (nfish) of age comp samples
-n.L <- list()
-n.L$fleet1 <- 200
-
-# Define fleet selectivity
-sel_fleet <- list()
-
-sel_fleet$fleet1$pattern <- 1
-sel_fleet$fleet1$A50.sel1 <- 2
-sel_fleet$fleet1$slope.sel1 <- 1
-
-## Survey settings
-survey_num <- 1
-
-# CV of surveys for OM
-cv.survey <- list()
-cv.survey$survey1 <- 0.1
-
-# Input CV of surveys for EMs
-input.cv.survey <- list()
-input.cv.survey$survey1 <- 0.2
-
-# Annual sample size (nfish) of age comp samples
-n.survey <- list()
-n.survey$survey1 <- 200
-
-# Define survey selectivity
-sel_survey <- list()
-
-sel_survey$survey1$pattern <- 1
-sel_survey$survey1$A50.sel1 <- 1.5
-sel_survey$survey1$slope.sel1 <- 2
-
-## Other settings
-logf_sd <- 0.2
-f_dev_change <- FALSE
-f_pattern <- 1
-start_val <- 0.01
-middle_val <- NULL
-end_val <- 0.39
-f_val <- NULL
-start_year <- 1
-middle_year <- NULL
-
-logR_sd <- 0.4
-r_dev_change <- TRUE
-
-om_bias_cor <- FALSE
-bias_cor_method <- "none" # Options: "none", "median_unbiased", and "mean_unbiased"
-em_bias_cor <- FALSE
-
-# Case 1 : null case -----------------------------------------------------------
-
-null_case_input <- ASSAMC::save_initial_input(base_case = TRUE, case_name = "C1")
-
-## Run OM
-ASSAMC::run_om(input_list = null_case_input, show_iter_num = T)
-
-
-# Case 2 : change logR_sd from 0.4 to 0.6 --------------------------------------
-
-# updated_input <- save_initial_input(
-#   base_case = FALSE,
-#   input_list = null_case_input,
-#   case_name = "C2",
-#   logR_sd = 0.6
+#' Create a simulated data set using OM from model comparison project
+#'
+#' Use a simulated data set from {ASSAMC} to create an input data set
+#' for an age-structured stock assessment model fit using {FIMS}.
+#'
+#' @details This script only works for a simulation with one fleet and
+#' one survey. Additionally, there are many items that need to be looked at,
+#' which are referenced with the "TODO" tag.
+#' @author Kathryn L. Doering and Kelli F. Johnson
+#'
+###############################################################################
+# Simulate the data
+###############################################################################
+# Install the {ASSAMC} package, which is not a dependency of {FIMS}
+# remotes::install_github(
+#   "Bai-Li-NOAA/Age_Structured_Stock_Assessment_Model_Comparison@feat_eg-data"
 # )
-# run_om(input_list = updated_input, show_iter_num = F)
+returnedom <- ASSAMC::save_om_example()
 
-# modfy the generated data set ------
-
-rm(list = ls()) # to get rid of all the other obj cluttering workspace
-load("C1/output/OM/OM1.RData")
-
-# landings - assume fishery occurs continuously over the whole year.
-
+###############################################################################
+# Landings
+###############################################################################
 landings_data <- data.frame(
-    type = "landings", # cor catch?
-    name = names(om_output$L.mt)[1],
-    age = NA, # The inputs are not by age in this case, but there is a by age option.
-    year = om_input$year, #keep this column if we add timing start and end?
-    timingstart = 0,
-    timingend = 1, #how to indicate end of the year?
-    value = om_output$L.mt[[1]], # note only 1 fleet in this case tho
-    unit = "mt", # metric tons
-    family = paste0("ilnorm(CV=", om_input$cv.L[[1]], ")")
+# TODO: decide on the term used here for catch/landings.
+#       Should there be a type that are not removed but just noted,
+#       where obviously in this instance they are removed.
+  type = "landings",
+  name = names(returnedom[["om_output"]]$L.mt)[1],
+  age = NA, # Not by age in this case, but there is a by age option.
+# TODO: check that the fishery occurs continuously over the whole year.
+  datestart = as.Date(
+    paste(returnedom[["om_input"]]$year, 1, 1, sep = "-"),
+    format = "%Y-%m-%d"
+  ),
+  dateend = as.Date(
+    paste(returnedom[["om_input"]]$year, 12, 31, sep = "-"),
+    format = "%Y-%m-%d"
+  ),
+  value = returnedom[["om_output"]]$L.mt[[1]], # note only 1 fleet in this case
+  unit = "mt", # metric tons
+  # TODO: is CV the appropriate input here?
+  uncertainty = returnedom[["om_input"]]$cv.L[[1]]
 )
 
-# survey trend (index of abundance) - assume occurs instantaneously at the beginning of the year.
-
+###############################################################################
+# Survey trend
+###############################################################################
 trend_data <- data.frame(
-    type = "trend",
-    name = names(om_output$survey_index)[1],
-    age = NA, # The inputs are not by age in this case.
-    year = om_input$year, # may want to add fractional components to this to indicate timing.
-    timingstart = 0,
-    timingend = 0,
-    value = om_output$survey_index[[1]],
-    unit = "numbers", # I think?
-    family = paste0("ilnorm(CV=", om_input$cv.survey[[1]], ")") # Is this ok?
+# TODO: ensure that "trend" is understood as a type
+  type = "trend",
+  name = names(returnedom[["om_output"]]$survey_index)[1],
+  age = NA, # Not by age in this case, but there is a by age option.
+# TODO: check that survey trend occurs instantaneously on Jan. 01
+  datestart = as.Date(
+    paste(returnedom[["om_input"]]$year, 1, 1, sep = "-"),
+    format = "%Y-%m-%d"
+  ),
+  dateend = as.Date(
+    paste(returnedom[["om_input"]]$year, 1, 1, sep = "-"),
+    format = "%Y-%m-%d"
+  ), # Maybe use NA if instantaneous?
+  value = returnedom[["om_output"]]$survey_index[[1]],
+# TODO: check the unit of the survey
+  unit = "numbers",
+  uncertainty = returnedom[["om_input"]]$cv.survey[[1]]
 )
 
-# survey comp data
-matrix_convert_comp_data <- as.data.frame(om_output$survey_age_comp[[1]])
-colnames(matrix_convert_comp_data)  <- om_input$ages
-matrix_convert_comp_data$year <- om_input$year
-age_comp_data <- tidyr::pivot_longer(matrix_convert_comp_data, 
-  cols = om_input$ages, 
-  names_to = "age",
-  values_to = "value")
+###############################################################################
+# Age-composition data
+###############################################################################
+age_comp_data <- rbind(
+  data.frame(
+    name = names(returnedom[["em_input"]]$n.L),
+    returnedom[["em_input"]]$L.age.obs$fleet1,
+# TODO: is proportions okay as a unit?
+#       What would be other units, e.g., frequency or numbers?
+    unit = "proportions",
+    uncertainty = returnedom[["em_input"]]$n.L$fleet1,
+    datestart = as.Date(
+      paste(returnedom[["om_input"]][["year"]], 1, 1, sep = "-"),
+      "%Y-%m-%d"
+    ),
+    dateend = as.Date(
+      paste(returnedom[["om_input"]][["year"]], 12, 31, sep = "-"),
+      "%Y-%m-%d"
+    )
+  ),
+  data.frame(
+    name = names(returnedom[["om_output"]]$survey_age_comp)[1],
+    returnedom[["om_output"]]$survey_age_comp[[1]],
+# TODO: check the unit b/c I am not sure why there are decimals
+    unit = "numbers",
+    uncertainty = returnedom[["om_input"]][["n.survey"]][["survey1"]],
+    datestart = as.Date(
+      paste(returnedom[["om_input"]][["year"]], 1, 1, sep = "-"),
+      "%Y-%m-%d"
+    ),
+    dateend = as.Date(
+      paste(returnedom[["om_input"]][["year"]], 1, 1, sep = "-"),
+      "%Y-%m-%d"
+    )
+  )
+) %>%
+  dplyr::mutate(
+    type = "age"
+  ) %>%
+  tidyr::pivot_longer(
+    cols = dplyr::starts_with("X"),
+    names_prefix = "X",
+    names_to = "age",
+    values_to = "value"
+  )
 
-age_comp_data$name <- names(om_output$survey_age_comp)[1]
-age_comp_data$type <- "agecomp" # or rather agefrequency???
-age_comp_data$unit <- "numbers"
-age_comp_data$family <- paste0("imultinom(size=", om_input$n.survey[[1]], ")") # I think size is consistent with r function??
-age_comp_data$timingstart <- 0
-age_comp_data$timingend <- 0
+###############################################################################
+# Weight-at-age data
+###############################################################################
+timingfishery <- data.frame(
+  datestart = as.Date(
+      paste(returnedom[["om_input"]][["year"]], 1, 1, sep = "-"),
+      "%Y-%m-%d"
+    ),
+  dateend = as.Date(
+    paste(returnedom[["om_input"]][["year"]], 12, 31, sep = "-"),
+    "%Y-%m-%d"
+  )
+)
+weightsfishery <- data.frame(
+# TODO: decide on type for empirical weight-at-age data.
+  type = "weight-at-age",
+  name = names(returnedom[["em_input"]]$n.L),
+  age = seq_along(returnedom[["om_input"]][["W.kg"]]),
+  value = returnedom[["om_input"]][["W.mt"]],
+  uncertainty = NA,
+  unit = "mt"
+)
+empiricalweight_data <- merge(timingfishery, weightsfishery)
 
-# order the same as trend data.
-age_comp_data <- 
-  age_comp_data[,c("type", "name", "age", "year", "timingstart", "timingend", "value", "unit", "family" )]
-
-# From the paper it sounds like there should be agecomp for the fishery, but I don't
-# see this in the om_output.
-
-# bind together into a single data frame
-
-data_df <- rbind(landings_data, trend_data, age_comp_data)
-data_df <- type.convert(data_df, as.is = TRUE)
-write.csv(data_df, 
-  file.path("C1", "output", "OM", "FIMS_input_data.csv"),
-  row.names = FALSE)
+###############################################################################
+# {FIMS} data
+###############################################################################
+data_df <- type.convert(
+  rbind(landings_data, trend_data, age_comp_data, empiricalweight_data),
+  as.is = TRUE
+)
+write.csv(data_df,
+  file.path("FIMS_input_data.csv"),
+  row.names = FALSE
+)
 
 # check csv can be read into r well ----
-test_read <- read.csv(file.path("C1", "output", "OM", "FIMS_input_data.csv"))
-
-# TODO: need any adjustments to timing?
+test_read <- read.csv(file.path("FIMS_input_data.csv"))
+all(test_read == data_df, na.rm = TRUE)
