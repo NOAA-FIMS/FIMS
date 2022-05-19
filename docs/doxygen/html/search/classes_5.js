73ae8613837640bbd554c375da8fdd890370a780
Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
2022-05-19 11:42:52 -0700

    123 feature update to fims docs (#130)
    * try to modify the sample code to work in our repo
    
    * update tab on the uses line 44
    
    * GHA: update GHA inputs
    
    * fix: create docs folder if it does not exist
    
    * fix: fix typo in commit message variable
    
    * fix user name and repo name
    
    * add dot after docs/ to specify it's a directory
    
    * chore: remove docs and update run doxygen GHA
    
    * GHA: fix rename command using mv
    
    * GHA: remove destination folder
    
    * GHA: only run GHA when push to main
    
    Co-authored-by: Bai Li - NOAA <59936250+Bai-Li-NOAA@users.noreply.github.com>
    Co-authored-by: Bai Li <bai.li@noaa.gov>

--- docs/doxygen/html/search/classes_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['selectivitybase_27',['SelectivityBase',['../structfims_1_1SelectivityBase.html',1,'fims']]],
-  ['subpopulation_28',['Subpopulation',['../classfims_1_1Subpopulation.html',1,'fims']]]
-];
