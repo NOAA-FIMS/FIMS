c848d66a0dfcfddb29c9641e08fc9ab7fa84ad14
Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
2022-05-24 16:34:47 -0700

    46 feature module skeleton function created (#128)
    * add draft R function to create module
    
    * add template files and base R function skeleton
    
    * update use module function
    
    * keep working on use_module, getting closer but not copyign right
    
    * update and correct templates
    
    * change sub_folder to module_type to avoid confusion
    
    * use_module function seems to be working
    
    * update use_module function documentation, NAMESPACE, and DESCRIPTION based on code review comments
    
    * update template files
    
    * make growth.hpp match main branch
    
    * fix example to run
    
    * test: add a test for use_module()
    
    * add \dontrun{}, replace default path, and setwd back on.exit
    
    * fix use_module
    
    * fix use_module
    
    * comment out test case
    
    Co-authored-by: Bai Li <bai.li@noaa.gov>

---
+++ inst/templates/module_functor_template.hpp
@@ -0,0 +1,50 @@
+/*
+ * This File is part of the NOAA, National Marine Fisheries Service
+ * Fisheries Integrated Modeling System project.
+ * Refer to the LICENSE file for reuse information.
+ * 
+ * The purpose of this file is to declare the module_name functor class
+ * which is the base class for all module_name functors.
+ */
+ // Change {{{module_type}}} and {{{module_name}}} to ALL CAPS below
+#ifndef POPULATION_DYNAMICS_{{{module_type}}}_{{{module_name}}}_HPP
+#define POPULATION_DYNAMICS_{{{module_type}}}_{{{module_name}}}_HPP
+
+
+//#include "../../../interface/interface.hpp"
+// The fims_math.hpp file is only needed if you are using a function 
+// programmed in fims_math
+// Leave {{{module_type}}} lowercase below
+#include "../../../common/fims_math.hpp"
+#include "{{{module_type}}}_base.hpp"
+
+namespace fims {
+
+ /**
+  *  @brief {{{module_name}}}{{{module_type}}} class that returns the sub_module function value
+ * from fims_math.
+ */ 
+ // {{{module_name}}} and {{{module_type}}} should be CamelCase below
+template<typename T>
+struct {{{module_name}}}{{{module_type}}} : public {{{module_type}}}Base<T> {
+    //add submodule class members here
+    //these include parameters of the submodule
+
+    {{{module_name}}}{{{module_type}}}(): {{{module_type}}}Base<T>(){
+
+    }
+
+    /**
+    * @brief 
+    * 
+    * @param x  description
+    */
+    virtual const T evaluate(const T& x) {
+        //you will need to add class members as arguments to the function below
+        return fims::{{{module_name}}}<T>(x);
+    }
+};
+
+}
+//Change {{{module_type}}} and {{{module_name}}} to ALL CAPS below
+#endif /* POPULATION_DYNAMICS_{{{module_type}}}_{{{module_name}}}_HPP */
--- inst/templates/module_template.hpp
+++ inst/templates/module_template.hpp
@@ -4,14 +4,20 @@
  * Fisheries Integrated Modeling System project. See LICENSE in the 
  * source folder for reuse information.
  *
- * growth base file
+ * {{{ module_type }}} base file
  * The purpose of this file is to include any .hpp files within the 
  * subfolders so that only this file needs to included in the model.hpp file.
  * 
  * DEFINE guards for module_type module outline to define the 
  * module_type hpp file if not already defined.
  */
-#define POPULATION_DYNAMICS_GROWTH_HPP
-#define POPULATION_DYNAMICS_GROWTH_HPP
+ // Change {{{module_type}}} to ALL CAPS below
+#define POPULATION_DYNAMICS_{{{ module_type }}}_HPP
+#define POPULATION_DYNAMICS_{{{ module_type }}}_HPP
 
-#endif /* FIMS_POPULATION_DYNAMICS_GROWTH_HPP */
+// leave {{{module_name}}} and {{{module_type}}} lowercase below
+#include "functors/{{{module_type}}}_base.hpp"
+#include "functors/{{{module_name}}}.hpp"
+
+//Change {{{module_type}}} to ALL CAPS below
+#endif /* FIMS_POPULATION_DYNAMICS_{{{module_type}}}_HPP */
