9c8dd49ffa2508b7efe1d9a6a5b5905f18d8617c
Jane Sullivan <jane.sullivan@noaa.gov>
2022-05-25 11:34:27 -0800

    Fixed typo, all tests now running smoothly.

--- tests/gtest/test_population_dynamics_selectivity_double_logistic.cpp
+++ tests/gtest/test_population_dynamics_selectivity_double_logistic.cpp
@@ -14,8 +14,8 @@ namespace
     fishery_selectivity.median2 = 15.0;
     fishery_selectivity.slope2 = 0.05;
     double fishery_x = 34.5;
-    // 1.0/(1.0+exp(-(34.5-10.5)*2.0)) * (1.0 - 1.0/(1.0+exp(-(34.5-15.0)*0.05))) = 0.273885
-    double expect_fishery = 0.273885;
+    // 1.0/(1.0+exp(-(34.5-10.5)*0.2)) * (1.0 - 1.0/(1.0+exp(-(34.5-15.0)*0.05))) = 0.2716494
+    double expect_fishery = 0.2716494;
     EXPECT_NEAR(fishery_selectivity.evaluate(fishery_x), expect_fishery, 0.0001);
 
   }
