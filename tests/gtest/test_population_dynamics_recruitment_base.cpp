0687e8a9cd015d4c6f89aadbe416240bd5ebbb02
Bai Li <bai.li@noaa.gov>
2022-05-31 14:59:11 -0400

    test: add deviation test

---
+++ tests/gtest/test_population_dynamics_recruitment_base.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+#include "population_dynamics/recruitment/functors/recruitment_base.hpp"
+//#include "population_dynamics/recruitment/functors/sr_beverton_holt.hpp"
+
+ 
+namespace
+{
+  TEST(recruitment_deviations, constraint_works)
+  {
+    EXPECT_EQ(1.0, 1.0);
+  }
+
+}
