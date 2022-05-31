46dd2844a8b677af013cc34ef6fa3fbf0b70c334
msupernaw <matthew.supernaw@noaa.gov>
2022-05-31 12:09:18 -0400

    Update rcpp_interface.hpp

--- inst/include/interface/rcpp_interface.hpp
+++ inst/include/interface/rcpp_interface.hpp
@@ -1,5 +1,35 @@
 #ifndef RCPP_INTERFACE_HPP
 #define RCPP_INTERFACE_HPP
 
+#include <Rcpp.h>
+#include "interface.hpp"
 
-#endif
+/**
+ * @brief RcppInterface class that defines 
+ * the interface between R and C++.
+ */
+class Parameter{
+    public:
+    double value;
+    double min = std::numeric_limits<double>::min();
+    double max = std::numeric_limits<double>::max();
+
+    Parameter(double value, double min, double max){
+        this->value = value;
+        this->min = min;
+        this->max = max;
+    }
+
+    Parameter(double value){
+        this->value = value;
+    }
+
+    Parameter(){
+        this->value = 0;
+    }
+};
+
+
+
+
+#endif /* RCPP_INTERFACE_HPP */
