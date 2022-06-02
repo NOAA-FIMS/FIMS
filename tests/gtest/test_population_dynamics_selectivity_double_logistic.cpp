13e766782a7dc4476c86bd7d0ca601cf7ba2506e
Jane Sullivan <jane.sullivan@noaa.gov>
2022-06-02 11:47:35 -0800

    Add double logistic selectivity submodule #103 feat dbl logistic slx (#135)
    * Functor for double logistic selectivity, includes modifications to fims_math and new tests. One test is failing... I think it's related to tests/gtest/test_population_dynamics_selectivity_double_logistic.cpp but can't find the problem
    
    Co-authored-by: Chris Legault <chris.legault@noaa.gov>
    Co-authored-by: Jon Kenton Tarsus Brodziak <jon.brodziak@noaa.gov>
    
    * Fixed typo, all tests now running smoothly.
    
    * running devtools::document() as part of PR. no changes related to our branch but there was one update related to the use_module() function
    
    * none of these changes caused by styler::style_pkg() were related to our branch BUT i did review them and its all cosmetic.
    
    * update parameter names for the dbl logistic function to be more descriptive (e.g. median_asc instead of median1)
    
    * fix typo
    
    Co-authored-by: Chris Legault <chris.legault@noaa.gov>
    Co-authored-by: Jon Kenton Tarsus Brodziak <jon.brodziak@noaa.gov>
    Co-authored-by: Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
    Co-authored-by: Christine Stawitz <christine.stawitz@noaa.gov>

---
+++ tests/gtest/test_population_dynamics_selectivity_double_logistic.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+#include "population_dynamics/selectivity/functors/double_logistic.hpp"
+
+namespace
+{
+
+  TEST(double_logistic_selectivity, create_object)
+  {
+    
+    fims::DoubleLogisticSelectivity<double> fishery_selectivity;
+    fishery_selectivity.median_asc = 10.5;
+    fishery_selectivity.slope_asc = 0.2;
+    fishery_selectivity.median_desc = 15.0;
+    fishery_selectivity.slope_desc = 0.05;
+    double fishery_x = 34.5;
+    // 1.0/(1.0+exp(-(34.5-10.5)*0.2)) * (1.0 - 1.0/(1.0+exp(-(34.5-15.0)*0.05))) = 0.2716494
+    double expect_fishery = 0.2716494;
+    EXPECT_NEAR(fishery_selectivity.evaluate(fishery_x), expect_fishery, 0.0001);
+
+  }
+
+}
