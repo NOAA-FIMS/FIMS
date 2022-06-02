c134877f725e63c4271462aa87671ad5df9a51bc
Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
2022-06-02 13:26:35 -0700

    fix typo in arguments

--- inst/include/common/fims_math.hpp
+++ inst/include/common/fims_math.hpp
@@ -144,7 +144,7 @@ inline const T double_logistic(const T &median_asc, const T &slope_asc,
    * @return
    */
   template <typename T>
-  inline const T ad_min(const T& a, constT& b,  T C = 1e-5) {
+  inline const T ad_min(const T& a, const T& b,  T C = 1e-5) {
       return  (a + b - fims::ad_fabs(a - b,C))*.5;
   }
 
