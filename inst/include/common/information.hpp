e208752e1ac1a053e0af3265f0121f1b34750b10
msupernaw <matthew.supernaw@noaa.gov>
2022-06-03 11:04:55 -0400

    updated header guard

--- inst/include/common/information.hpp
+++ inst/include/common/information.hpp
@@ -22,68 +22,67 @@
 
 namespace fims {
 
-/**
- * Contains all objects and data pre-model construction
- */
-template <typename T>
-class Information {
-  public:
-  static std::shared_ptr<Information<T> > fims_information;
-  std::vector<T*> parameters;                // list of all estimated parameters
-  std::vector<T*> random_effects_parameters; // list of all random effects parameters
-  std::vector<T*> fixed_effects_parameters;  // list of all fixed effects parameters
-  
-  std::map<uint32_t, std::shared_ptr<fims::RecruitmentBase<T> > > recruitment_models;
-  std::map<uint32_t, std::shared_ptr<fims::SelectivityBase<T> > > selectivity_models;
-  
-/**
- * Returns a single Information object for type T.
- * 
- * @return singleton for type T
- */  
-static std::shared_ptr<Information<T> > GetInstance(){
-   if(Information<T>::fims_information == nullptr){
-       Information<T>::fims_information =  std::make_shared<fims::Information<T> >();
-   }  
-   return Information<T>::fims_information;
+    /**
+     * Contains all objects and data pre-model construction
+     */
+    template <typename T>
+    class Information {
+    public:
+        static std::shared_ptr<Information<T> > fims_information;
+        std::vector<T*> parameters; // list of all estimated parameters
+        std::vector<T*> random_effects_parameters; // list of all random effects parameters
+        std::vector<T*> fixed_effects_parameters; // list of all fixed effects parameters
 
-}
+        std::map<uint32_t, std::shared_ptr<fims::RecruitmentBase<T> > > recruitment_models;
+        std::map<uint32_t, std::shared_ptr<fims::SelectivityBase<T> > > selectivity_models;
 
+        /**
+         * Returns a single Information object for type T.
+         * 
+         * @return singleton for type T
+         */
+        static std::shared_ptr<Information<T> > GetInstance() {
+            if (Information<T>::fims_information == nullptr) {
+                Information<T>::fims_information = std::make_shared<fims::Information<T> >();
+            }
+            return Information<T>::fims_information;
 
-/**
- * Register a parameter as estimable.
- * 
- * @param p
- */
-void RegisterRarameter( T& p){
-    this->parameters.push_back(&p);
-}
+        }
 
-/**
- * Register a random effect as estimable.
- * 
- * @param p
- */
-void RegisterRandomEffect( T& re){
-    this->random_effects_parameters.push_back(&re);
-}
+        /**
+         * Register a parameter as estimable.
+         * 
+         * @param p
+         */
+        void RegisterRarameter(T& p) {
+            this->parameters.push_back(&p);
+        }
 
-/**
- * Create the generalized stock assessment model that will evaluate the
- * objective function.
- *  
- * @return 
- */
- bool CreateModel() {
-     std::cout<<"Information::CreateModel(): Not yet implemented.\n";
-     return false;
- }
- 
-};
+        /**
+         * Register a random effect as estimable.
+         * 
+         * @param p
+         */
+        void RegisterRandomEffect(T& re) {
+            this->random_effects_parameters.push_back(&re);
+        }
 
-template<typename T>
-std::shared_ptr<Information<T> > Information<T>::fims_information = nullptr;//singleton instance
+                /**
+         * Create the generalized stock assessment model that will evaluate the
+         * objective function.
+         *  
+         * @return 
+         */
+        bool CreateModel() {
+            std::cout << "Information::CreateModel(): Not yet implemented.\n";
+            return false;
+        }
 
-}  // namespace fims
+    };
+
+    template<typename T>
+    std::shared_ptr<Information<T> > Information<T>::fims_information = nullptr; //singleton instance
+
+} // namespace fims
 
 #endif /* FIMS_COMMON_INFORMATION_HPP */
