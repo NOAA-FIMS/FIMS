4e03e85ab956f1e91a086340089bbc4dea4e3730
ChristineStawitz-NOAA <ChristineStawitz-NOAA@users.noreply.github.com>
2022-06-06 16:35:25 +0000

    style and docs: run devtools::document() and styler::style_pkg()

--- R/create_rcpp_interface_object.R
+++ R/create_rcpp_interface_object.R
@@ -128,10 +128,10 @@ create_fims_rcpp_interface <- function(interface_name = character(), # class nam
 }
 
 
-#create_fims_rcpp_interface(
+# create_fims_rcpp_interface(
 #  "logistic_selectivity",
-#"LogisticSelectivity",
+# "LogisticSelectivity",
 # "selectivity_interface_base",
 # "selectivity_models",
 # c("slope", "median")
-#)
+# )
