cf65126a3d89b350eaf18fd8b2a9587631b66b53
Christine Stawitz <christine.stawitz@noaa.gov>
2022-06-06 15:00:31 -0700

    remove derivatives for EWAA

--- inst/include/interface/rcpp/rcpp_objects/rcpp_growth.hpp
+++ inst/include/interface/rcpp/rcpp_objects/rcpp_growth.hpp
@@ -65,7 +65,7 @@ std::map<uint32_t, GrowthInterfaceBase*>
  */
 class EWAAGrowthInterface : public GrowthInterfaceBase {
  public:
-  Parameter ewaa;
+  std::map<double, double> ewaa;
 
   EWAAGrowthInterface() : GrowthInterfaceBase() {}
 
@@ -84,79 +84,9 @@ class EWAAGrowthInterface : public GrowthInterfaceBase {
     // set relative info
     b0->id = this->id;
     b0->ewaa = this->ewaa.value;
-    if (this->ewaa.estimated) {
-      if (this->ewaa.is_random_effect) {
-        d0->RegisterRandomEffect(b0->ewaa);
-      } else {
-        d0->RegisterRarameter(b0->ewaa);
-      }
-    }
    
     // add to Information
     d0->Growth_models[b0->id] = b0;
-
-    // first-order derivative
-    std::shared_ptr<fims::Information<TMB_FIMS_FIRST_ORDER> > d1 =
-        fims::Information<TMB_FIMS_FIRST_ORDER>::GetInstance();
-
-    std::shared_ptr<fims::EWAAgrowth<TMB_FIMS_FIRST_ORDER> > b1 =
-        std::make_shared<fims::EWAAgrowth<TMB_FIMS_FIRST_ORDER> >();
-
-    // set relative info
-    b1->id = this->id;
-    b1->ewaa = this->ewaa.value;
-    if (this->ewaa.estimated) {
-      if (this->ewaa.is_random_effect) {
-        d1->RegisterRandomEffect(b1->ewaa);
-      } else {
-        d1->RegisterRarameter(b1->ewaa);
-      }
-    }
-
-    // add to Information
-    d1->Growth_models[b1->id] = b1;
-
-    // second-order derivative
-    std::shared_ptr<fims::Information<TMB_FIMS_SECOND_ORDER> > d2 =
-        fims::Information<TMB_FIMS_SECOND_ORDER>::GetInstance();
-
-    std::shared_ptr<fims::SRBevertonHolt<TMB_FIMS_SECOND_ORDER> > b2 =
-        std::make_shared<fims::SRBevertonHolt<TMB_FIMS_SECOND_ORDER> >();
-
-    // set relative info
-    b2->id = this->id;
-    b2->ewaa = this->ewaa.value;
-    if (this->ewaa.estimated) {
-      if (this->ewaa.is_random_effect) {
-        d2->RegisterRandomEffect(b2->ewaa);
-      } else {
-        d2->RegisterRarameter(b2->ewaa);
-      }
-    }
-
-    // add to Information
-    d2->Growth_models[b2->id] = b2;
-
-    // third-order derivative
-    std::shared_ptr<fims::Information<TMB_FIMS_THIRD_ORDER> > d3 =
-        fims::Information<TMB_FIMS_THIRD_ORDER>::GetInstance();
-
-    std::shared_ptr<fims::SRBevertonHolt<TMB_FIMS_THIRD_ORDER> > b3 =
-        std::make_shared<fims::SRBevertonHolt<TMB_FIMS_THIRD_ORDER> >();
-
-    // set relative info
-    b3->id = this->id;
-    b3->ewaa = this->ewaa.value;
-    if (this->ewaa.estimated) {
-      if (this->ewaa.is_random_effect) {
-        d3->RegisterRandomEffect(b3->ewaa);
-      } else {
-        d3->RegisterRarameter(b3->ewaa);
-      }
-    }
-    
-    // add to Information
-    d3->Growth_models[b3->id] = b3;
 
     return true;
   }
