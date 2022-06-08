b1db946617dc4921e59c56531c4be518d3294b92
Christine.Stawitz <christine.stawitz>
2022-06-08 12:50:05 -0700

    add ewaa growth test case for Rcpp

--- tests/testthat/FIMS_Rcpp_test.R
+++ tests/testthat/FIMS_Rcpp_test.R
@@ -6,9 +6,13 @@ fims <- Rcpp::Module("fims", PACKAGE = "FIMS")
 a <- new(fims$Parameter, .1)
 expect_success(beverton_holt <- new(fims$BevertonHoltRecruitment))
 expect_success(logistic_selectivity <- new(fims$LogisticSelectivity))
+expect_success(ewaa_growth <- new(fims$EWAAgrowth))
 logistic_selectivity$slope$value <- .7
 logistic_selectivity$median$value <- 5.0
 
 expect_equal(logistic_selectivity$slope$value, 0.7)
 expect_equal(logistic_selectivity$get_id(), 0)
+ewaa_growth$ages <- 1.0
+ewaa_growth$weights <- 2.5
+expect_equal(ewaa_growth$ages, 1.0)
 })
