2fa16baa87a6af6e01cf017ce1f973cae2785c12
Bai Li <bai.li@noaa.gov>
2022-06-09 14:36:35 -0400

    test: update expected value

--- tests/gtest/test_population_dynamics_recruitment_base.cpp
+++ tests/gtest/test_population_dynamics_recruitment_base.cpp
@@ -13,23 +13,24 @@ namespace
     // Test that the constrain_deviations = false works
     recruit.constrain_deviations = false;
     recruit.PrepareConstrainedDeviations();
-
+    
+    std::vector<double> expected_deviations_false = {-1.0, 2.0, 3.0};
     for (int i = 0; i < recruit.rec_deviations.size(); i++)
     {
       EXPECT_EQ(recruit.rec_deviations[i],
-                recruit.rec_deviations[i]);
+                expected_deviations_false[i]);
     }
 
     // Test that the constrain_deviations = true works
     recruit.constrain_deviations = true;
     recruit.PrepareConstrainedDeviations();
     // c(-1.0, 2.0, 3.0)-sum(c(-1.0, 2.0, 3.0))/3 = -2.3333333  0.6666667  1.6666667
-    std::vector<double> expected_deviations = {-2.3333333, 0.6666667, 1.6666667};
+    std::vector<double> expected_deviations_true = {-2.3333333, 0.6666667, 1.6666667};
 
     for (int i = 0; i < recruit.rec_deviations.size(); i++)
     {
       EXPECT_NEAR(recruit.rec_deviations[i],
-                expected_deviations[i], 0.0000001);
+                expected_deviations_true[i], 0.0000001);
     }
   }
 
