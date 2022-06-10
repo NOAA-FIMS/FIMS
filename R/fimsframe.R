cf4b5320315edf6eb1ded48f5a5b2a2e581321d0
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2022-06-10 11:59:51 -0700

    Combines class .Rd into one file

--- R/fimsframe.R
+++ R/fimsframe.R
@@ -1,68 +1,97 @@
 
-# VALIDATION FUNCTIONS --------------------------------------------------------
+# VALIDATION FUNCTIONS ----
 # These validate functions must be the first entries of this file otherwise
-# the documentation via [document()] will not work.
+# [document()] will not work.
 # Another option is to use @include in the documentation of the class and put
 # the validation functions in another file.
-#' @export
 validFIMSFrame <- function(object) {
   errors <- character()
 
-  NROW(object@data) -> n
-  if (n == 0) {
+  if (NROW(object@data) == 0) {
     errors <- c(errors, "data must have at least one row")
   }
 
+  # Check columns
+  if (!"type" %in% colnames(object@data)) {
+    errors <- c(errors, "data must contain 'type'")
+  }
+  if (!"datestart" %in% colnames(object@data)) {
+    errors <- c(errors, "data must contain 'datestart'")
+  }
+  if (!"dateend" %in% colnames(object@data)) {
+    errors <- c(errors, "data must contain 'dateend'")
+  }
+
+  # Return
   if (length(errors) == 0) {
-    TRUE
+    return(TRUE)
   } else {
-    errors
+    return(errors)
   }
 }
 
-#' @export
 validFIMSFrameAge <- function(object) {
- # validation code goes here
- TRUE
+  errors <- character()
+
+  # Check columns
+  if (!"age" %in% colnames(object@data)) {
+    errors <- c(errors, "data must contain 'age'")
+  }
+
+  # Return
+  if (length(errors) == 0) {
+    return(TRUE)
+  } else {
+    return(errors)
+  }
 }
 
-# DATA CLASSES ----------------------------------------------------------------
-
-# Class to hold the dataframe
-
+# DATA CLASSES ----
 # example: https://github.com/rbchan/unmarked/blob/master/R/unmarkedFrame.R
-# Note that it's a little outdated. Check Advanced R and R pkgs.
+# Note that the reference is a little outdated. Check Advanced R and R pkgs.
 
-#' FIMSframe Class
+#' FIMSFrame class
 #'
-#' Some details about this class and my plans for it in the body.
+#' The FIMSFrame class has just one slot for a `data.frame`.
+#' The class is extended by other classes specific to certain model types`.
 #'
-#' @slot data Data for FIMS
+#' @aliases FIMSFrame
+#' @rdname FIMSFrame
+#' @slot data A `data.frame` that stores data used to fit a FIMS model.
+#'   `data` can contain as many columns as you want and in any order.
+#'   But, the following columns are mandatory:
+#'   `type`, `datestart`, `dateend`, value, unit, and uncertainty.
+
 setClass("FIMSFrame",
     slots = list(
         data = "data.frame"
     ),
     validity = validFIMSFrame
 )
-#' The title for my S4 class that extends \code{"character"} class.
+#' `FIMSFrameAge` is an S4 class that extends the `FIMSFrame` class
 #'
 #' Some details about this class and my plans for it in the body.
 #' @name FIMSFrameAge-class
-#' @rdname FIMSFrameAge-class
+#' @rdname FIMSFrame-class
 #' @exportClass FIMSFrameAge
 setClass("FIMSFrameAge",
     slots = list(
-        weightatage = "vector",
-        ages = "vector"
+        weightatage = "data.frame",
+        ages = "numeric"
     ),
     contains = "FIMSFrame",
     validity = validFIMSFrameAge
 )
 
 
-# CONSTRUCTORS ----------------------------------------------------------------
-#' FIMSFrame base class constructor
+# CONSTRUCTORS ----
+#' Base class constructor for FIMSFrame
+#'
+#' @param data A `data.frame` that contains the necessary columns
+#'   to construct a data frame of a given `FIMSFrame-class`, which are useable
+#'   by models supported within the \pkg{FIMS} package.
 #' @export
+#' @rdname FIMSFrame
 FIMSFrame <- function(data) {
   stopifnot(inherits(data, "data.frame"))
 
@@ -74,8 +103,8 @@ FIMSFrame <- function(data) {
   return(out)
 }
 #' FIMSFrameAge
-#'
 #' @export
+#' @rdname FIMSFrame
 FIMSFrameAge <- function(data) {
   stopifnot(inherits(data, "data.frame"))
 
@@ -93,12 +122,12 @@ FIMSFrameAge <- function(data) {
   return(out)
 }
 
-# SELECTORS -------------------------------------------------------------------
+# SELECTORS ----
 # these can define for e.g., subsetting functions. This could 
 # be helpful for example if you want to take out all data for a fleet
 # across multiple slots in the object.
 
-# Accessors -----
+# Accessors ----
 # Methods for accessing info in the slots
 # e.g., something like name.
 
