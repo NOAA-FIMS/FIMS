f9dc7695261608d052015ae15c2f5da4914ba87d
Christine Stawitz <christine.stawitz@noaa.gov>
2022-06-14 11:45:44 -0700

    move call to make_map back

--- inst/include/interface/rcpp/rcpp_objects/rcpp_growth.hpp
+++ inst/include/interface/rcpp/rcpp_objects/rcpp_growth.hpp
@@ -67,7 +67,8 @@ class EWAAGrowthInterface : public GrowthInterfaceBase {
     }
     return mymap;
   }
-  
+
+  std::map<double, double> ewaa = make_map(this->ages, this->weights);
 
   virtual bool add_to_fims_tmb() {
     // base model
@@ -76,9 +77,6 @@ class EWAAGrowthInterface : public GrowthInterfaceBase {
 
     std::shared_ptr<fims::EWAAgrowth<TMB_FIMS_REAL_TYPE> > b0 =
         std::make_shared<fims::EWAAgrowth<TMB_FIMS_REAL_TYPE> >();
-
-    
-    std::map<double, double> ewaa = make_map(this->ages, this->weights);
 
     // set relative info
     b0->id = this->id;
