52f4277b98748855ec5be0391eed76237b31e195
msupernaw <matthew.supernaw@noaa.gov>
2022-06-16 10:15:24 -0400

    atomic test

--- src/FIMS.cpp
+++ src/FIMS.cpp
@@ -4,6 +4,46 @@
 #include "../inst/include/interface/interface.hpp"
 #include "../inst/include/interface/init.hpp"
 
+
+
+// Double version of Lambert W function
+double LambertW(double x) {
+  double logx = log(x);
+  double y = (logx > 0 ? logx : 0);
+  int niter = 100, i=0;
+  for (; i < niter; i++) {
+    if ( fabs( logx - log(y) - y) < 1e-9) break;
+    y -= (y - exp(logx - y)) / (1 + y);
+  }
+  if (i == niter) Rf_warning("W: failed convergence");
+  return y;
+}
+TMB_ATOMIC_VECTOR_FUNCTION(
+    // ATOMIC_NAME
+    LambertW
+    ,
+    // OUTPUT_DIM
+    1,
+    // ATOMIC_DOUBLE
+    ty[0] = LambertW(tx[0]); // Call the 'double' version
+    ,
+    // ATOMIC_REVERSE
+    Type W  = ty[0];                    // Function value from forward pass
+    Type DW = 1. / (exp(W) * (1. + W)); // Derivative
+    px[0] = DW * py[0];                 // Reverse mode chain rule
+)
+// Scalar version
+template<class Type>
+Type LambertW(Type x){
+  CppAD::vector<Type> tx(1);
+  tx[0] = x;
+  return LambertW(tx)[0];
+}
+// Vectorized version
+VECTORIZE1_t(LambertW)
+
+
+
 /*
  * @brief TMB objective function
  *
@@ -13,9 +53,9 @@ template<class Type>
 Type objective_function<Type>::operator()() {
 
 
-    PARAMETER_VECTOR(p);
+    PARAMETER_VECTOR(x);
 
-    Type nll = 0;
+    Type nll = LambertW(x).sum();
 
     return nll;
 
