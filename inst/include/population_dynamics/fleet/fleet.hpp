46f183434a388ebdb5ec3b55b5bf7a566e7e60cc
Christine Stawitz <christine.stawitz@noaa.gov>
2022-06-22 17:09:47 -0700

    add fleet module

--- inst/include/population_dynamics/fleet/fleet.hpp
+++ inst/include/population_dynamics/fleet/fleet.hpp
@@ -1,42 +1,60 @@
 /*
- * File:   fleet.hpp
- *
- * Author: Matthew Supernaw
- * National Oceanic and Atmospheric Administration
- * National Marine Fisheries Service
- * Email: matthew.supernaw@noaa.gov
- *
- * Created on September 30, 2021, 1:12 PM
- *
  * This File is part of the NOAA, National Marine Fisheries Service
  * Fisheries Integrated Modeling System project.
- *
- * This software is a "United States Government Work" under the terms of the
- * United States Copyright Act.  It was written as part of the author's official
- * duties as a United States Government employee and thus cannot be copyrighted.
- * This software is freely available to the public for use. The National Oceanic
- * And Atmospheric Administration and the U.S. Government have not placed any
- * restriction on its use or reproduction.  Although all reasonable efforts have
- * been taken to ensure the accuracy and reliability of the software and data,
- * the National Oceanic And Atmospheric Administration and the U.S. Government
- * do not and cannot warrant the performance or results that may be obtained by
- * using this  software or data. The National Oceanic And Atmospheric
- * Administration and the U.S. Government disclaim all warranties, express or
- * implied, including warranties of performance, merchantability or fitness
- * for any particular purpose.
- *
- * Please cite the author(s) in any work or product based on this material.
- *
+ * Refer to the LICENSE file for reuse information.
+ * 
+ * The purpose of this file is to declare the growth functor class
+ * which is the base class for all growth functors.
  */
 #ifndef FIMS_POPULATION_DYNAMICS_FLEET_HPP
 #define FIMS_POPULATION_DYNAMICS_FLEET_HPP
 
-#include "../../common/model_object.hpp"
+#include "../../../common/model_object.hpp"
 
 namespace fims {
 
-template <typename T>
-struct Fleet {};
+/* @brief Base class for all fleets.
+ *
+ * @tparam T The type of the fleet object.
+ * */
+template<typename T>
+struct Fleet : public FIMSObject<T> {
+    
+    static uint32_t id_g; /*!< reference id for fleet object*/
+    static ModelTraits<T>::DataVector* observed_index_data;
+    static ModelTraits<T>::DataVector* observed_agecomp_data;
+     
+    static LikelihoodBase<T>* index_likelihood;
+    static LikelihoodBase<T>* agecomp_likelihood;
+    
+
+    /** @brief Constructor.
+    */
+   Fleet(){ this->id = Fleet::id_g++; }
+
+   void SetNormalIndexLikelihood(T mean, T sd){
+    this->index_likelihood& = fims::Dnorm<T>();
+    this->index_likelihood.mean = mean;
+    this->index_likelihood.sd = sd;
+   }
+
+   void SetMultinomAgeCompLikelihood(ModelTraits<T>::EigenVector x,
+       ModelTraits<T>::EigenVector p){
+    this->agecomp_likelihood& = fims::Dmultinom<T>();
+    this->agecomp_likelihood.x = x;
+    this->agecomp_likelihood.p = p;
+   }
+
+    T likelihood(){
+         return this->index_likelihood->evaluate(do_log=true)
+         + this->agecomp_likelihood->evaluate(do_log=true);
+         
+     }
+
+};
+
+template <class Type>
+uint32_t Fleet<Type>::id_g = 0;
 
 }  // namespace fims
 
