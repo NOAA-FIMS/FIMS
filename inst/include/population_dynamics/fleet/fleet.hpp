da3208d72b0c4f950f2a73b376fe04cf958a9d0f
msupernaw <matthew.supernaw@noaa.gov>
2022-06-29 13:55:43 -0400

    create fleet module

--- inst/include/population_dynamics/fleet/fleet.hpp
+++ inst/include/population_dynamics/fleet/fleet.hpp
@@ -9,53 +9,78 @@
 #ifndef FIMS_POPULATION_DYNAMICS_FLEET_HPP
 #define FIMS_POPULATION_DYNAMICS_FLEET_HPP
 
-#include "../../../common/model_object.hpp"
+#include "../../common/model_object.hpp"
+#include "../../common/data_object.hpp"
+//#include "../../common/likelihood.hpp"
+#include "../selectivity/selectivity.hpp"
 
 namespace fims {
 
-/* @brief Base class for all fleets.
- *
- * @tparam T The type of the fleet object.
- * */
-template<typename T>
-struct Fleet : public FIMSObject<T> {
-    
-    static uint32_t id_g; /*!< reference id for fleet object*/
-    static ModelTraits<T>::DataVector* observed_index_data;
-    static ModelTraits<T>::DataVector* observed_agecomp_data;
-     
-    static LikelihoodBase<T>* index_likelihood;
-    static LikelihoodBase<T>* agecomp_likelihood;
-    
+    /* @brief Base class for all fleets.
+     *
+     * @tparam T The type of the fleet object.
+     * */
+    template<typename T>
+    struct Fleet : public FIMSObject<T> {
+        static uint32_t id_g; /*!< reference id for fleet object*/
 
-    /** @brief Constructor.
-    */
-   Fleet(){ this->id = Fleet::id_g++; }
 
-   void SetNormalIndexLikelihood(T mean, T sd){
-    this->index_likelihood& = fims::Dnorm<T>();
-    this->index_likelihood.mean = mean;
-    this->index_likelihood.sd = sd;
-   }
+        //data objects
+        uint32_t observed_index_data_id;
+        std::shared_ptr<fims::DataObject<double> observed_index_data;
 
-   void SetMultinomAgeCompLikelihood(ModelTraits<T>::EigenVector x,
-       ModelTraits<T>::EigenVector p){
-    this->agecomp_likelihood& = fims::Dmultinom<T>();
-    this->agecomp_likelihood.x = x;
-    this->agecomp_likelihood.p = p;
-   }
+        uint32_t observed_agecomp_data_id;
+        std::shared_ptr<fims::DataObject<double> observed_agecomp_data;
 
-    T likelihood(){
-         return this->index_likelihood->evaluate(do_log=true)
-         + this->agecomp_likelihood->evaluate(do_log=true);
-         
-     }
+        //likelihood components
+        uint32_t index_likelihood_id;
+        std::shared_ptr<fims::LikelihoodBase> index_likelihood;
+        
+        uint32_t agecomp_likelihood_id;
+        std::shared_ptr<fims::LikelihoodBase> agecomp_likelihood;
+        
+        //selectivity
+        uint32_t selectivity_id;
+        std::shared_ptr<fims::SelectivityBase> selectivity;
+        
+        //derived quantities
+        std::vector<T> catch_at_age;
+        std::vector<T> catch_index;
+        std::vector<T> age_composition;
+        
 
-};
+        /** @brief Constructor.
+         */
+        Fleet() {
+            this->id = Fleet::id_g++;
+        }
+        
+ 
 
-template <class Type>
-uint32_t Fleet<Type>::id_g = 0;
+//        void SetNormalIndexLikelihood(T mean, T sd) {
+//            this->index_likelihood& = fims::Dnorm<T>();
+//            this->index_likelihood.mean = mean;
+//            this->index_likelihood.sd = sd;
+//        }
+//
+//        void SetMultinomAgeCompLikelihood(ModelTraits<T>::EigenVector x,
+//                ModelTraits<T>::EigenVector p) {
+//            this->agecomp_likelihood& = fims::Dmultinom<T>();
+//            this->agecomp_likelihood.x = x;
+//            this->agecomp_likelihood.p = p;
+//        }
 
-}  // namespace fims
+        T likelihood() {
+            return this->index_likelihood->evaluate(do_log = true)
+                    + this->agecomp_likelihood->evaluate(do_log = true);
+
+        }
+
+    };
+
+    template <class T>
+    uint32_t Fleet<T>::id_g = 0;
+
+} // namespace fims
 
 #endif /* FIMS_POPULATION_DYNAMICS_FLEET_HPP */
