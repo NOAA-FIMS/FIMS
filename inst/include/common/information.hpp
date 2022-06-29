db02f60c62529ba79defa790f5cf8cae07e3a43e
msupernaw <matthew.supernaw@noaa.gov>
2022-06-29 13:55:43 -0400

    create fleet module

--- inst/include/common/information.hpp
+++ inst/include/common/information.hpp
@@ -19,75 +19,107 @@
 #include "../population_dynamics/recruitment/recruitment.hpp"
 #include "../population_dynamics/selectivity/selectivity.hpp"
 #include "../population_dynamics/growth/growth.hpp"
+#include "../population_dynamics/fleet/fleet.hpp"
+#include "../population_dynamics/population/population.hpp"
+
 #include "model_object.hpp"
 
 namespace fims {
 
-/**
- * Contains all objects and data pre-model construction
- */
-template <typename T>
-class Information {
- public:
-  static std::shared_ptr<Information<T> > fims_information;
-  std::vector<T*> parameters;  // list of all estimated parameters
-  std::vector<T*>
-      random_effects_parameters;  // list of all random effects parameters
-  std::vector<T*>
-      fixed_effects_parameters;  // list of all fixed effects parameters
+    /**
+     * Contains all objects and data pre-model construction
+     */
+    template <typename T>
+    class Information {
+    public:
+        static std::shared_ptr<Information<T> > fims_information;
+        std::vector<T*> parameters; // list of all estimated parameters
+        std::vector<T*> random_effects_parameters; // list of all random effects parameters
+        std::vector<T*> fixed_effects_parameters; // list of all fixed effects parameters
 
-  std::map<uint32_t, std::shared_ptr<fims::RecruitmentBase<T> > >
-      recruitment_models;
-  std::map<uint32_t, std::shared_ptr<fims::SelectivityBase<T> > >
-      selectivity_models;
-  std::map<uint32_t, std::shared_ptr<fims::GrowthBase<T> > > growth_models;
+        //data objects
+        std::map<uint32_t, std::shared_ptr<fims::DataObject<double> > data_objects;
 
-  /**
-   * Returns a single Information object for type T.
-   *
-   * @return singleton for type T
-   */
-  static std::shared_ptr<Information<T> > GetInstance() {
-    if (Information<T>::fims_information == nullptr) {
-      Information<T>::fims_information =
-          std::make_shared<fims::Information<T> >();
-    }
-    return Information<T>::fims_information;
-  }
+        //life history modules
+        std::map<uint32_t, std::shared_ptr<fims::RecruitmentBase<T> > >;
+        typedef typename std::map<uint32_t, std::shared_ptr<fims::RecruitmentBase<T> > >::iterator recruitment_model_iterator;
 
-  /**
-   * Register a parameter as estimable.
-   *
-   * @param p
-   */
-  void RegisterParameter(T& p) { this->parameters.push_back(&p); }
+        std::map<uint32_t, std::shared_ptr<fims::SelectivityBase<T> > > selectivity_models;
+        typedef typename std::map<uint32_t, std::shared_ptr<fims::SelectivityBase<T> > >::iterator selectivity_models_iterator;
 
-  /**
-   * Register a random effect as estimable.
-   *
-   * @param p
-   */
-  void RegisterRandomEffect(T& re) {
-    this->random_effects_parameters.push_back(&re);
-  }
+        std::map<uint32_t, std::shared_ptr<fims::GrowthBase<T> > > growth_models;
+        typedef typename std::map<uint32_t, std::shared_ptr<fims::GrowthBase<T> > >::iterator growth_models_iterator;
 
 
-  /**
-   * Create the generalized stock assessment model that will evaluate the
-   * objective function.
-   *
-   * @return
-   */
-  bool CreateModel() {
-    std::cout << "Information::CreateModel(): Not yet implemented.\n";
-    return false;
-  }
-};
+        //fleet modules
+        std::map<uint32_t, std::shared_ptr<fims::Fleet<T> > > fleets;
+        typedef typename std::map<uint32_t, std::shared_ptr<fims::Fleet<T> > >::iterator fleet_iterator;
 
-template <typename T>
-std::shared_ptr<Information<T> > Information<T>::fims_information =
-    nullptr;  // singleton instance
+        //populations
+        std::map<uint32_t, std::shared_ptr<fims::Population<T> > > populations;
+        typedef typename std::map<uint32_t, std::shared_ptr<fims::Population<T> > >::iterator population_iterator;
 
-}  // namespace fims
+        /**
+         * Returns a single Information object for type T.
+         *
+         * @return singleton for type T
+         */
+        static std::shared_ptr<Information<T> > GetInstance() {
+            if (Information<T>::fims_information == nullptr) {
+                Information<T>::fims_information =
+                        std::make_shared<fims::Information<T> >();
+            }
+            return Information<T>::fims_information;
+        }
+
+        /**
+         * Register a parameter as estimable.
+         *
+         * @param p
+         */
+        void RegisterParameter(T& p) {
+            this->parameters.push_back(&p);
+        }
+
+        /**
+         * Register a random effect as estimable.
+         *
+         * @param p
+         */
+        void RegisterRandomEffect(T& re) {
+            this->random_effects_parameters.push_back(&re);
+        }
+
+        /**
+         * Create the generalized stock assessment model that will evaluate the
+         * objective function.
+         *
+         * @return
+         */
+        bool CreateModel() {
+            std::cout << "Information::CreateModel(): Not yet implemented.\n";
+
+            for (fleet_iterator it = this->fleets.begin();
+                    it != this->fleets.end(); ++it) {
+
+                std::shared_ptr<fims::Fleet<T> > f = (*it).second;
+                //set fleet elements here
+            }
+
+            for (population_iterator it = this->populations.begin();
+                    it != this->populations.end(); ++it) {
+
+                std::shared_ptr<fims::Population<T> > p = (*it).second;
+                //set population elements here
+            }
+            return false;
+        }
+    };
+
+    template <typename T>
+    std::shared_ptr<Information<T> > Information<T>::fims_information =
+    nullptr; // singleton instance
+
+} // namespace fims
 
 #endif /* FIMS_COMMON_INFORMATION_HPP */
