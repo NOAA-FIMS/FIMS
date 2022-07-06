3c4fca73e407100c93ff2de98583143904f26d1d
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2022-07-06 11:32:45 -0700

    test: fix bugs in Rcpp dnorm test

--- tests/testthat/test-dnorm.R
+++ tests/testthat/test-dnorm.R
@@ -5,17 +5,17 @@ test_that("dnorm",{
   #generate data using R stats:rnorm
   set.seed(123)
   #simulate normal data
-  y <- stats::rnorm(100)
+  y <- stats::rnorm(1)
 
   #create a fims Rcpp object
   fims <- Rcpp::Module("fims", PACKAGE = "FIMS")
   #initialize the Dnorm module
   dnorm_ <- new(fims$TMBDnormDistribution)
   #populate class members
-  dnorm_$x <- y
-  dnorm_$mean <- 0
-  dnorm_$sd <- 1
+  dnorm_$x$value <- y
+  dnorm_$mean$value <- 0
+  dnorm_$sd$value <- 1
   #evaluate the density and compare with R
-  expect_equal(dnorm_$evaluate(), sum(stats::dnorm(y, 0, 1, true)))
+  expect_equal(dnorm_$evaluate(), stats::dnorm(y, 0, 1, TRUE))
 
 })
