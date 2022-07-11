08fd185224946508a52f07bfef6b45cccfcba96d
Bai Li <bai.li@noaa.gov>
2022-07-11 10:06:37 -0400

    update expect error test

--- tests/testthat/test-ewaa.r
+++ tests/testthat/test-ewaa.r
@@ -1,9 +1,7 @@
-  library(Rcpp)
+library(Rcpp)
 data(package = "FIMS")
 
-test_that("ewaa data can be added to model",{
-  
-
+test_that("ewaa data can be added to model", {
   fims <- Rcpp::Module("fims", PACKAGE = "FIMS")
   ewaa_growth <- new(fims$EWAAgrowth)
   age_frame <- FIMSFrameAge(data_mile1)
@@ -11,9 +9,13 @@ test_that("ewaa data can be added to model",{
   ewaa_growth$ages <- unique(ewaa_data$age)
   ewaa_growth$weights <- unique(ewaa_data$value)
   expect_equal(ewaa_growth$evaluate(1), 0.00053065552)
+  
   ewaa_growth2 <- new(fims$EWAAgrowth)
   ewaa_growth2$ages <- c(unique(ewaa_data$age), 12)
   ewaa_growth2$weights <- unique(ewaa_data$value)
-  expect_error(ewaa_growth2$evaluate(1), regexp = NULL)
-
+  expect_error(
+    ewaa_growth2$evaluate(1),
+    regexp = "ages and weights must be the same length",
+    ignore.case = FALSE
+  )
 })
