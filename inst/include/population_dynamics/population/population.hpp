9e30d720fc57deee59aae8a653ad3e9c202ec3af
Christine Stawitz <christine.stawitz@noaa.gov>
2022-07-22 12:52:38 -0700

    start work on population file

--- inst/include/population_dynamics/population/population.hpp
+++ inst/include/population_dynamics/population/population.hpp
@@ -40,34 +40,49 @@
 namespace fims {
 
     /**
-     * Population class. Contains subpopulations
+     * @brief Population class. Contains subpopulations
      * that are divided into generic partitions (eg. sex, area).
      */
-    template <typename T>
-    struct Population : public FIMSObject<T> {
+    template <typename Type>
+    struct Population : public FIMSObject<Type> {
         size_t nyears;
         size_t nseasons;
         size_t nages;
-        std::vector<T> ages;
+        std::vector<Type> ages;
 
-
-        std::vector<T> mortality;
-        std::vector<T> maturity;
-
+        std::vector<Type> mortality;
+        std::vector<Type> maturity;
 
         std::shared_ptr<fims::RecruitmentBase> recruitment_model;
         std::shared_ptr<fims::GrowthBase> growth_model;
+        std::shared_ptr<fims::MaturityBase> maturity_model;
 
         std::vector<std::shared_ptr<fims::Fleet> > fleets;
         std::vector<std::shared_ptr<fims::Fleet> > surveys;
 
-        //derived quantities
-        std::vector<T> initial_numbers;
-        std::vector<T> weight_at_age;
-        std::vector<T> fecundity;
-        std::vector<T> numbers_at_age;
-        std::vector<T> catch_at_age;
-        std::vector<T> biomass;
+        Population() {
+            this->id = Population::id_g++;
+
+             //derived quantities
+            std::vector<Type> initial_numbers;
+            std::vector<Type> weight_at_age;
+            std::vector<Type> fecundity;
+            std::vector<Type> numbers_at_age;
+            std::vector<Type> catch_at_age;
+            std::vector<Type> biomass;
+
+            //size all the vectors to length of nages
+            ages.resize(nages);
+            mortality.resize(nages);
+            maturity.resize(nages);
+            initial_numbers.resize(nages);
+            weight_at_age.resize(nages);
+            fecundity.resize(nages);
+            numbers_at_age.resize(nages);
+            catch_at_age.resize(nages);
+            biomass.resize(nyears);
+        
+        }
 
         void Initialize() {
         }
@@ -102,6 +117,9 @@ namespace fims {
 
 
     };
+    template <class Type>
+    uint32_t Population<Type>::id_g = 0;
+
 } // namespace fims
 
 #endif /* FIMS_POPULATION_DYNAMICS_POPULATION_HPP */
