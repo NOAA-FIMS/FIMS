dfc62fa3b3209feb85b82ef1614e62f7faeb9a4f
Bai Li <bai.li@noaa.gov>
2022-08-16 10:23:31 -0400

    test: add error checking tests for fleet SetSelectivity

--- tests/testthat/test-fleet-interface.R
+++ tests/testthat/test-fleet-interface.R
@@ -15,15 +15,22 @@ fleet module", {
   expect_equal((selectivity_fleet2$get_id()), 2)
 
   # Add selectivity to fleet
-  fleet <- new(fims$Fleet)
+  fleet1 <- new(fims$Fleet)
+  fleet2 <- new(fims$Fleet)
   
   # Expect code produces no output, error, message, or warnings
-  expect_silent(fleet$SetSelectivity(selectivity_fleet1$get_id()))
-  expect_silent(fleet$SetSelectivity(selectivity_fleet2$get_id()))
+  expect_silent(fleet1$SetSelectivity(selectivity_fleet1$get_id()))
+  expect_silent(fleet2$SetSelectivity(selectivity_fleet2$get_id()))
+
+  # Expect code produces error when adding selectivity_fleet2 to fleet1
+  expect_error(fleet1$SetSelectivity(selectivity_fleet2$get_id()))
 
   # Expect code produces error when ID of selectivity is not an integer
   expect_error(fleet$SetSelectivity(1.2))
 
- # Expect code produces error when ID of selectivity is a character string
+  # Expect code produces error when ID of selectivity does not exist
+  expect_error(fleet$SetSelectivity(5))
+
+  # Expect code produces error when ID of selectivity is a character string
   expect_error(fleet$SetSelectivity("id"))
 })
