c86035b62b59d57cb909a0632d8df0efc04b486b
Bai Li <bai.li@noaa.gov>
2022-08-22 17:48:36 -0400

    feat: add warnings to fleet module and update fleet R test

--- tests/testthat/test-fleet-interface.R
+++ tests/testthat/test-fleet-interface.R
@@ -17,20 +17,17 @@ fleet module", {
   # Add selectivity to fleet
   fleet1 <- new(fims$Fleet)
   fleet2 <- new(fims$Fleet)
-  
+
   # Expect code produces no output, error, message, or warnings
   expect_silent(fleet1$SetSelectivity(selectivity_fleet1$get_id()))
   expect_silent(fleet2$SetSelectivity(selectivity_fleet2$get_id()))
 
   # Expect code produces error when adding selectivity_fleet2 to fleet1
-  expect_error(fleet1$SetSelectivity(selectivity_fleet2$get_id()))
-
-  # Expect code produces error when ID of selectivity is not an integer
-  expect_error(fleet$SetSelectivity(1.2))
-
-  # Expect code produces error when ID of selectivity does not exist
-  expect_error(fleet$SetSelectivity(5))
+  expect_warning(
+    fleet1$SetSelectivity(selectivity_fleet2$get_id()),
+    regexp = paste0("Selectivity has been set already.")
+  )
 
   # Expect code produces error when ID of selectivity is a character string
-  expect_error(fleet$SetSelectivity("id"))
+  expect_error(fleet1$SetSelectivity("id"))
 })
