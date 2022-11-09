286b4b8c157164f02ff43de2da58ebf63585fa6b
Bai Li <bai.li@noaa.gov>
2022-11-09 13:17:29 -0500

    test: use for loop to test individual elements

--- tests/gtest/test_population_Li.cpp
+++ tests/gtest/test_population_Li.cpp
@@ -4,29 +4,40 @@
 
 namespace
 {
+
     TEST_F(PopulationPrepareTestFixture, CalculateMortality_works)
     {
+        // Heap corruption (Exit code 0xc0000374) occurs when using
+        // year <= population.nyears. Details from the failed test:
+        // Failure Expected equality of these values:
+        // population.mortality_Z[index_ya]
+        //  Which is: -5.20867e-98
+        // mortality_Z[index_ya]
+        //  Which is: 1
 
-        // Change Fmort[index_yf] to this->Fmort[index_yf] in population.hpp?
-        int year = 4;
-        int age = 6;
-        int index_ya = year * population.nages + age;
-
-        population.CalculateMortality(index_ya, year, age);
-
-        std::vector<double> mortality_F(nyears * nages, 0);
-        
-        for (int fleet_index = 0; fleet_index < population.nfleets; fleet_index++)
+        // for (size_t year = 0; year <= population.nyears; year++)
+        for (size_t year = 0; year < population.nyears; year++)
         {
-            int index_yf = year * population.nfleets + fleet_index;
-            mortality_F[index_ya] += population.Fmort[index_yf] *
-                                     population.fleets[fleet_index]->selectivity->evaluate(age);
-        }
-        EXPECT_EQ(population.mortality_F[index_ya], mortality_F[index_ya]);
+            for (size_t age = 0; age < population.nages; age++)
+            {
+                size_t index_ya = year * population.nages + age;
+                population.CalculateMortality(index_ya, year, age);
 
-        std::vector<double> mortality_Z(nyears * nages, 0);
-        mortality_Z[index_ya] = fims::exp(population.log_M[index_ya]) +
-                                mortality_F[index_ya];
-        EXPECT_EQ(population.mortality_Z[index_ya], mortality_Z[index_ya]);
+                std::vector<double> mortality_F(nyears * nages, 0);
+
+                for (size_t fleet_index = 0; fleet_index < population.nfleets; fleet_index++)
+                {
+                    size_t index_yf = year * population.nfleets + fleet_index;
+                    mortality_F[index_ya] += population.Fmort[index_yf] *
+                                             population.fleets[fleet_index]->selectivity->evaluate(age);
+                }
+                EXPECT_EQ(population.mortality_F[index_ya], mortality_F[index_ya]);
+
+                std::vector<double> mortality_Z(nyears * nages, 0);
+                mortality_Z[index_ya] = fims::exp(population.log_M[index_ya]) +
+                                        mortality_F[index_ya];
+                EXPECT_EQ(population.mortality_Z[index_ya], mortality_Z[index_ya]);
+            }
+        }
     }
 }
