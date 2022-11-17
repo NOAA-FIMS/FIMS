dfeb6ab512ecef50628a6a38328bef5540d9181e
Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
2022-11-17 09:40:47 -0800

    modify recruitment tests

--- tests/testthat/test-recruitment-interface.R
+++ tests/testthat/test-recruitment-interface.R
@@ -3,13 +3,17 @@ test_that("Recruitment input settings work as expected", {
 
   # Create recruitment
   recruitment <- new(fims$BevertonHoltRecruitment)
+  h <- 0.75
+  r0 <- 1000.0
+  spawns <- 500.0
+  ssb0 <- 800.0
 
-  recruitment$steep$value <- 0.75
+  recruitment$steep$value <- h
   recruitment$steep$min <- 0.21
   recruitment$steep$max <- 1.0
   recruitment$steep$is_random_effect <- TRUE
   recruitment$steep$estimated <- TRUE
-  recruitment$rzero$value <- 1000.0
+  recruitment$rzero$value <- r0
 
   expect_equal(recruitment$get_id(), 1)
   expect_equal(recruitment$steep$value, 0.75)
@@ -18,4 +22,8 @@ test_that("Recruitment input settings work as expected", {
   expect_true(recruitment$steep$is_random_effect)
   expect_true(recruitment$steep$estimated)
   expect_equal(recruitment$rzero$value, 1000.0)
+
+  answer <- (h*r0*0.75*spawns)/(0.2*r0*ssb0*(1-h)+spawns*(h-0.2))
+
+  expect_equal(recruitment$evaluate(spawns, ssb0), answer)
 })
