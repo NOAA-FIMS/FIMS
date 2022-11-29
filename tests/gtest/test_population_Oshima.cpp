09ceae19d51fc9177c544789a39e445bebe957e7
MOshima-PIFSC <megumi.oshima@noaa.gov>
2022-11-29 11:42:26 -0500

    recruitment unit test

--- tests/gtest/test_population_Oshima.cpp
+++ tests/gtest/test_population_Oshima.cpp
@@ -5,32 +5,57 @@
 namespace
 {
 
-    TEST_F(PopulationPrepareTestFixture, CalculateCatchWeightAA_works)
+    TEST_F(PopulationPrepareTestFixture, CalculateRecruitment_works)
     {
+        // calculating recruitment for year 4
+        int year = 4;
+        // specifying steepness and rzero values for calculations
+        // values from test_population_test_fixture.hpp 
+        double steep = 0.75;
+        double rzero = 1000000.0;
+        double rec_dev = 0.2;
+        // vector for storing expeceted recruitment 
+        // check dims on this
+        std::vector<double> expect_recruitment(year * population.nages, 0);
+        // temporary storage for recruitment to use in loop 
+        double recruitment_temp;
+        //int age = 6; Need to include all ages to calculate recruitment?
+        int index_ya = year * population.nages;
+        //int index_ya2 = (year - 1) * population.nages; should I be using this one instead 
+        //since the index for ssb and ssbzero are year-1 in recruitment equation?
 
-        for (size_t year = 0; year < population.nyears; year++)
-        {
-            for (size_t age = 0; age < population.nages; age++)
-            {
-                //size_t index_yaf = year * population.nages + age * population.nfleets + age * population.nfleets + fleet_index;
-                // don't need to create index_yaf here because the function only needs year and age
-                population.CalculateCatchWeightAA(year, age);
-                // dimensions based on the catch_weight_at_age object in population.hpp line 132
-                std::vector<double> catch_weight_at_age(nyears * nages * nfleets, 0);
+        // calculate recruitment in population module
+        population.CalculateRecruitment(index_ya, year);
+        
+// for each age, age 0 to max age
+        for (size_t age = 0; age < nages; age++){
+              // create index for year 4, and age
+              // Why do we need to do this in the loop?
+               int index_ya = year * population.nages + age;
+               // for year-age, calculate the spawning biomass
+               population.CalculateSpawningBiomass(index_ya, year, age);
+               // for year-age calculate the unfished spawning biomass
+               population.CalculateInitialNumbersAA(index_ya, age);
+               // trying to create an ssbzero variable to use in the recruitment equation by
+               // multiplying the initial numbers-at-age and the population weight-at-age
+               double ssbzero = population.numbers_at_age[index_ya] * population.weight_at_age[age];
+               recruitment_temp = 
+               (0.8 * rzero * steep * population.spawning_biomass[age]) / 
+               (0.2 * ssbzero * (1.0 - steep) + population.spawning_biomass[age] * (steep - 0.2)) *
+               rec_dev;
+               expect_recruitment[index_ya] += recruitment_temp;
+           }
 
-                for (size_t fleet_index = 0; fleet_index < population.nfleets; fleet_index++)
-                {
-                    size_t index_yaf = year * population.nages * population.nfleets + age * population.nfleets + fleet_index;
-                //    catch_weight_at_age[index_yaf] += population.catch_numbers_at_age[index_yaf] *
-                //                             population.weight_at_age[age];
-                // Not sure how to write the catch_numbers_at_age part, because it is another function or 
-                // will we create something in the population test fixture?
+        EXPECT_EQ(population.numbers_at_age[index_ya], expect_recruitment[index_ya]);
+    }
+}
 
-                    catch_weight_at_age[index_yaf] += CalculateCatchNumbersAA->evaluate(index_yaf) *
-                                                      population.weight_at_age[age]
-                }
-                EXPECT_EQ(population.CalculateCatchWeightAA[index_yaf], catch_weight_at_age[index_yaf]);
+// Issues: 
+// how to get recruit_deviations 
+//    see lines 118-125 in test_population_test_fixture.hpp
+//    but don't think that will work because rec_devs isn't a variable in population.hpp
+// index should be year-1 for spawners and ssbzero
+// 
 
-            }
-        }
+
 
