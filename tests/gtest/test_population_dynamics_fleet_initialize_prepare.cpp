feeac0747a9708b6b107bb793e56cb2c9609cc6d
Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
2022-12-08 22:08:59 +0000

    add tests for q

--- tests/gtest/test_population_dynamics_fleet_initialize_prepare.cpp
+++ tests/gtest/test_population_dynamics_fleet_initialize_prepare.cpp
@@ -34,19 +34,28 @@ namespace
         double log_Fmort_min = fims::log(0.1);
         double log_Fmort_max = fims::log(2.3);
         std::uniform_real_distribution<double> log_Fmort_distribution(log_Fmort_min, log_Fmort_max);
+
+        double log_q_min = fims::log(0.1);
+        double log_q_max = fims::log(1);
+        std::uniform_real_distribution<double> log_q_distribution(log_q_min, log_q_max);
         for(int i = 0; i < nyears; i++)
         {
             fleet.log_Fmort[i] = log_Fmort_distribution(generator);
+            fleet.log_q[i] = log_q_distribution(generator);
         }
         fleet.Prepare();
 
-        // Test fleet.Fmort
+        // Test fleet.Fmort and fleet.q
         std::vector<double> Fmort(nyears, 0);
+        std::vector<double> q(nyears, 0);
         for (int i = 0; i < nyears; i++)
         {
             Fmort[i] = fims::exp(fleet.log_Fmort[i]);
             EXPECT_EQ(fleet.Fmort[i], Fmort[i]);
+            q[i] = fims::exp(fleet.log_q[i]);
+            EXPECT_EQ(fleet.q[i], q[i]);
         }
         EXPECT_EQ(fleet.Fmort.size(), nyears);
+        EXPECT_EQ(fleet.q.size(), nyears);
     }
 } // namespace
