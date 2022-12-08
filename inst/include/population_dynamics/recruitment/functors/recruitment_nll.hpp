1b3f817efaddc9069ae6b247ca650a418a7d6458
Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
2022-12-08 14:56:59 -0800

    update nll

--- inst/include/population_dynamics/recruitment/functors/recruitment_nll.hpp
+++ inst/include/population_dynamics/recruitment/functors/recruitment_nll.hpp
@@ -58,17 +58,17 @@ struct RecruitmentNLL : public RecruitmentBase<Type> {
 
       fims::Dnorm<Type> dnorm;
       dnorm.sd = this->log_sigma_recruit;
-      Rcout << "Rec devs being passed to C++ are "
-            << this->recruit_deviations << std::endl;
+      //std::cout << "Rec devs being passed to C++ are "
+      //      << this->recruit_deviations << std::endl;
       for (size_t i = 0; i < this->recruit_deviations.size(); i++) { 
         dnorm.mean = 0.0;
         if(this->use_recruit_bias_adjustment){
           dnorm.mean -= this->recruit_bias_adjustment[i];
-          Rcout << "mean" << dnorm.mean << std::endl;
+        //  std::cout << "mean" << dnorm.mean << std::endl;
         } 
           dnorm.x = log(this->recruit_deviations[i]);
           
-          Rcout << "x" << dnorm.x << std::endl;
+          //std::cout << "x" << dnorm.x << std::endl;
           nll += dnorm.evaluate(true);
         }
       return nll;
