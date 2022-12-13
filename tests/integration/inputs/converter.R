e40dbff82ee4554eda7b93c755e0fc2821dd0341
msupernaw <matthew.supernaw@noaa.gov>
2022-12-13 11:06:49 -0500

    Update converter.R

--- tests/integration/inputs/converter.R
+++ tests/integration/inputs/converter.R
@@ -7,9 +7,9 @@ for(i in 0:12){
     inp<-paste0(paste0(paste0(dir,"/OM"),j),".RData")
     load(inp)
     out<-paste0(paste0(paste0(dir,"/OM"),j),".json")
-    write_json(om_input,paste0(paste0(paste0(dir,"/om_output"),j),".json"))
+    write_json(om_output,paste0(paste0(paste0(dir,"/om_output"),j),".json"))
     write_json(om_input,paste0(paste0(paste0(dir,"/om_input"),j),".json"))
-    write_json(om_input,paste0(paste0(paste0(dir,"/em_input"),j),".json"))
+    write_json(em_input,paste0(paste0(paste0(dir,"/em_input"),j),".json"))
     rm(om_input, em_input,om_output)
   }
 }
