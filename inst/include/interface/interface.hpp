294bc39e4745530fdf8ae16a26735770b2db2212
Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
2023-02-03 10:00:29 -0800

    Adding REPORT_F,ADREPORT_F
    Co-authored-by: Chris Legault <chris.legault@noaa.gov>
    Co-authored-by: Andrea-Havron-NOAA <Andrea-Havron-NOAA@users.noreply.github.com>
    Co-authored-by: E.J. Dick <EJDick-NOAA@users.noreply.github.com>
    Co-authored-by: KyleShertzer-NOAA <KyleShertzer-NOAA@users.noreply.github.com>

--- inst/include/interface/interface.hpp
+++ inst/include/interface/interface.hpp
@@ -24,6 +24,18 @@
 //#define TMB_LIB_INIT R_init_FIMS
 #include <TMB.hpp>
 
+//define REPORT, ADREPORT, and SIMULATE
+#define REPORT_F(name,F)					\
+  if(isDouble<Type>::value && F->current_parallel_region<0) {	\
+    Rf_defineVar(Rf_install(#name),					\
+	      PROTECT(asSEXP(name)),F->report);			\
+    UNPROTECT(1);						\
+  }
+#define ADREPORT_F(name,F) F->reportvector.push(name,#name);
+
+#define SIMULATE_F(F)				\
+  if(isDouble<Type>::value && F->do_simulate)
+
 /**
  *  @brief ModelTraits class that contains the DataVector
  * and ParameterVector types.
