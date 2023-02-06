dc53ea6dd9c6d2138c799414c4340f4dd45b68b2
Ian Taylor <ian.taylor@noaa.gov>
2023-02-06 11:47:37 -0800

    Added pointers a population
    Co-authored-by: Andrea-Havron-NOAA <Andrea-Havron-NOAA@users.noreply.github.com>
    Co-authored-by: Kathryn Doering <kathryn.doering@noaa.gov>
    Co-authored-by: Christine Stawitz - NOAA <ChristineStawitz-NOAA@users.noreply.github.com>

--- inst/include/common/model.hpp
+++ inst/include/common/model.hpp
@@ -31,12 +31,46 @@ class Model {  // may need singleton
  public:
   static std::shared_ptr<Model<T> >
       fims_model; /*!< Create a shared fims_model as a pointer to Model*/
-  std::vector<std::shared_ptr<Population<T> > >
-      populations;                                 /*!< list of all population*/
-  std::vector<std::shared_ptr<Fleet<T> > > fleets; /*!< list of all fleets*/
+  static std::shared_ptr<Information<T> >
+      fims_information; /*!< Create a shared fims_information as a pointer to Information*/
+  // constructor
   virtual ~Model() {}
+  
+    /**
+    * Returns a single Information object for type T.
+    *
+    * @return singleton for type T
+    */
+    static std::shared_ptr<Model<T> > GetInstance() {
+    if (Model<T>::fims_model == nullptr) {
+        Model<T>::fims_model = std::make_shared<fims::Model<T> >();
+        Model<T>::fims_model->fims_information = fims::Information<T>::GetInstance();  
+    }
+    return Model<T>::fims_model;
+    }
+  
+
+  const T Evaluate() {
+    typename fims::Information<T>::population_iterator it;
+    for (it = this->fims_information->populations.begin();
+      it != this->fims_information->populations.end(); ++it)
+    {
+        //(*it).second points to the Population module
+      (*it).second->Prepare();
+      (*it).second->Evaluate();
+      
+    }
+    
+    // nll = negative-log-likelihood (the objective function)
+    T nll = 0.0;
+    return nll;
+  }
 };
 
+// Create singleton instance of Model class
+template <typename T>
+std::shared_ptr<Model<T> > Model<T>::fims_model =
+   nullptr;  // singleton instance
 }  // namespace fims
 
 #endif /* FIMS_COMMON_MODEL_HPP */
