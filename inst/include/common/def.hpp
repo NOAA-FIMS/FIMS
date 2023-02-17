6877388f9899828dc80b6ef24bcbb64ad98e1c8a
msupernaw <matthew.supernaw@noaa.gov>
2023-02-16 22:08:05 -0500

    updated traits

--- inst/include/common/def.hpp
+++ inst/include/common/def.hpp
@@ -25,26 +25,83 @@ std::ofstream FIMS_LOG("fims.log"); /**< Log file */
 
 namespace fims {
 
-#ifdef STD_LIB
+    //#ifdef STD_LIB
 
-/**
- * @brief Default trait. These are "T" specific
- * traits that depend on modeling platform.
- */
-template <typename T>
-struct FIMSTraits {
-  typedef double real_t;                            /**< The real type */
-  typedef double variable_t;                        /**< The variable type */
-  typedef typename std::vector<double> data_vector; /**< The data vector type */
-  typedef typename std::vector<double> variable_vector; /**< The variable vector
+    /**
+     * @brief Default trait. These are "T" specific
+     * traits that depend on modeling platform.
+     */
+    template <typename T>
+    struct FIMSTraits {
+        typedef T real_t; /**< The real type */
+        typedef T variable_t; /**< The variable type */
+        typedef typename std::vector<T> data_vector; /**< The data vector type */
+        typedef typename std::vector<T> variable_vector; /**< The variable vector
    type */
-  typedef typename std::vector<std::vector<double> > data_matrix;     /**< The
+        typedef typename std::vector<std::vector<T> > data_matrix; /**< The
        data matrix type */
-  typedef typename std::vector<std::vector<double> > variable_matrix; /**< The
+        typedef typename std::vector<std::vector<T> > variable_matrix; /**< The
    variable matrix type */
-};
+    };
+
+
+#ifdef TMB_MODEL
+
+    //template <>
+    //struct FIMSTraits<TMB_FIMS_REAL_TYPE> {
+    //  typedef double real_t;                            /**< The real type */
+    //  typedef TMB_FIMS_REAL_TYPE variable_t;                        /**< The variable type */
+    //  typedef typename std::vector<double> data_vector; /**< The data vector type */
+    //  typedef typename std::vector<double> variable_vector; /**< The variable vector
+    //   type */
+    //  typedef typename std::vector<std::vector<TMB_FIMS_REAL_TYPE> > data_matrix;     /**< The
+    //       data matrix type */
+    //  typedef typename std::vector<std::vector<TMB_FIMS_REAL_TYPE> > variable_matrix; /**< The
+    //   variable matrix type */
+    //};
+
+    template <>
+    struct FIMSTraits<TMB_FIMS_FIRST_ORDER> {
+        typedef double real_t; /**< The real type */
+        typedef TMB_FIMS_FIRST_ORDER variable_t; /**< The variable type */
+        typedef typename std::vector<double> data_vector; /**< The data vector type */
+        typedef typename std::vector<double> variable_vector; /**< The variable vector
+   type */
+        typedef typename std::vector<std::vector<TMB_FIMS_FIRST_ORDER> > data_matrix; /**< The
+       data matrix type */
+        typedef typename std::vector<std::vector<TMB_FIMS_FIRST_ORDER> > variable_matrix; /**< The
+   variable matrix type */
+    };
+
+    template <>
+    struct FIMSTraits<TMB_FIMS_SECOND_ORDER> {
+        typedef double real_t; /**< The real type */
+        typedef TMB_FIMS_SECOND_ORDER variable_t; /**< The variable type */
+        typedef typename std::vector<double> data_vector; /**< The data vector type */
+        typedef typename std::vector<double> variable_vector; /**< The variable vector
+   type */
+        typedef typename std::vector<std::vector<TMB_FIMS_SECOND_ORDER> > data_matrix; /**< The
+       data matrix type */
+        typedef typename std::vector<std::vector<TMB_FIMS_SECOND_ORDER> > variable_matrix; /**< The
+   variable matrix type */
+    };
+
+    template <>
+    struct FIMSTraits<TMB_FIMS_THIRD_ORDER> {
+        typedef double real_t; /**< The real type */
+        typedef TMB_FIMS_THIRD_ORDER variable_t; /**< The variable type */
+        typedef typename std::vector<double> data_vector; /**< The data vector type */
+        typedef typename std::vector<double> variable_vector; /**< The variable vector
+   type */
+        typedef typename std::vector<std::vector<TMB_FIMS_THIRD_ORDER> > data_matrix; /**< The
+       data matrix type */
+        typedef typename std::vector<std::vector<TMB_FIMS_THIRD_ORDER> > variable_matrix; /**< The
+   variable matrix type */
+    };
+
+
 
 #endif
-}  // namespace fims
+} // namespace fims
 
 #endif /* TRAITS_HPP */
--- inst/include/interface/interface.hpp
+++ inst/include/interface/interface.hpp
@@ -9,6 +9,7 @@
 
 #ifndef FIMS_INTERFACE_HPP
 #define FIMS_INTERFACE_HPP
+
 /*
  * Interface file. Uses pre-processing macros
  * to interface with multiple modeling platforms.
@@ -21,34 +22,112 @@
 // use isnan macro in math.h instead of TMB's isnan for fixing the r-cmd-check
 // issue
 #include <math.h>
+
 //#define TMB_LIB_INIT R_init_FIMS
 #include <TMB.hpp>
+#include "../common/def.hpp"
 
 //define REPORT, ADREPORT, and SIMULATE
-#define REPORT_F(name,F)					\
-  if(isDouble<Type>::value && F->current_parallel_region<0) {	\
-    Rf_defineVar(Rf_install(#name),					\
-	      PROTECT(asSEXP(name)),F->report);			\
-    UNPROTECT(1);						\
+#define REPORT_F(name,F)     \
+  if(isDouble<Type>::value && F->current_parallel_region<0) { \
+    Rf_defineVar(Rf_install(#name),     \
+              PROTECT(asSEXP(name)),F->report);   \
+    UNPROTECT(1);      \
   }
 #define ADREPORT_F(name,F) F->reportvector.push(name,#name);
 
-#define SIMULATE_F(F)				\
+#define SIMULATE_F(F)    \
   if(isDouble<Type>::value && F->do_simulate)
+
+#endif
+
+/**
+ * @brief Default trait. These are "T" specific
+ * traits that depend on modeling platform.
+ */
+template <typename T>
+struct ModelTraits {
+    typedef T real_t; /**< The real type */
+    typedef T variable_t; /**< The variable type */
+    typedef typename std::vector<T> DataVector; /**< The data vector type */
+    typedef typename std::vector<T> VariableVector; /**< The variable vector*/
+
+    static VariableVector MakeVariableVector(const std::vector<T>& v) {
+        return v;
+    }
+};
+
+
+
+#ifdef TMB_MODEL
+
+
+template <>
+struct ModelTraits<TMB_FIMS_FIRST_ORDER> {
+    typedef double real_t; /**< The real type */
+    typedef TMB_FIMS_FIRST_ORDER variable_t; /**< The variable type */
+    typedef typename std::vector<double> DataVector; /**< The data vector type */
+    typedef typename CppAd::vector<double> VariableVector; /**< The variable vector*/
+
+    static VariableVector MakeVariableVector(const std::vector<TMB_FIMS_FIRST_ORDER>& v) {
+        VariableVector ret(v.size());
+
+        for (int i = 0; i < v.size(); i++) {
+            ret[i] = v[i];
+        }
+        return ret;
+    }
+};
+
+template <>
+struct ModelTraits<TMB_FIMS_SECOND_ORDER> {
+    
+    typedef double real_t; /**< The real type */
+    typedef TMB_FIMS_SECOND_ORDER variable_t; /**< The variable type */
+    typedef typename std::vector<double> DataVector; /**< The data vector type */
+    typedef typename CppAd::vector<double> VariableVector; /**< The variable vector*/
+
+    static VariableVector MakeVariableVector(const std::vector<TMB_FIMS_SECOND_ORDER>& v) {
+        VariableVector ret(v.size());
+
+        for (int i = 0; i < v.size(); i++) {
+            ret[i] = v[i];
+        }
+        return ret;
+    }
+};
+
+template <>
+struct ModelTraits<TMB_FIMS_THIRD_ORDER> {
+    typedef double real_t; /**< The real type */
+    typedef TMB_FIMS_THIRD_ORDER variable_t; /**< The variable type */
+    typedef typename std::vector<double> DAtaVector; /**< The data vector type */
+    typedef typename std::vector<double> VariableVector; /**< The variable vector
+   type */
+
+    static VariableVector MakeVariableVector(const std::vector<TMB_FIMS_THIRD_ORDER>& v) {
+        VariableVector ret(v.size());
+
+        for (int i = 0; i < v.size(); i++) {
+            ret[i] = v[i];
+        }
+        return ret;
+    }
+};
 
 /**
  *  @brief ModelTraits class that contains the DataVector
  * and ParameterVector types.
  */
-template <typename T>
-struct ModelTraits {
-  typedef typename CppAD::vector<T> DataVector;      /**< This is a vector
-        of the data that is differentiable */
-  typedef typename CppAD::vector<T> ParameterVector; /**< This is a
-  vector of the parameters that is differentiable */
-  typedef typename tmbutils::vector<T>
-      EigenVector; /**< This is a vector as defined in TMB's namespace Eigen */
-};
+//template <typename T>
+//struct ModelTraits {
+//    typedef typename CppAD::vector<T> DataVector; /**< This is a vector
+//        of the data that is differentiable */
+//    typedef typename CppAD::vector<T> ParameterVector; /**< This is a
+//  vector of the parameters that is differentiable */
+//    typedef typename tmbutils::vector<T>
+//    EigenVector; /**< This is a vector as defined in TMB's namespace Eigen */
+//};
 
 #endif /* TMB_MODEL */
 
