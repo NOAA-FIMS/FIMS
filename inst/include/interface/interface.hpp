fad70c2a42ad7f10de62b321ac3476cafa9c2427
msupernaw <matthew.supernaw@noaa.gov>
2023-02-16 22:15:18 -0500

    Update interface.hpp

--- inst/include/interface/interface.hpp
+++ inst/include/interface/interface.hpp
@@ -103,7 +103,7 @@ struct ModelTraits<TMB_FIMS_THIRD_ORDER> {
     typedef double real_t; /**< The real type */
     typedef TMB_FIMS_THIRD_ORDER variable_t; /**< The variable type */
     typedef typename CppAd::vector<double> DataVector; /**< The data vector type */
-    typedef typename CppAd::vector<TMB_FIMS_THIRD_ORDER> ParameterVector; /**< The variable vector
+    typedef typename CppAd::vector<TMB_FIMS_THIRD_ORDER> ParameterVector; /**< The variable vector*/
   
 
     static ParameterVector MakeVariableVector(const std::vector<TMB_FIMS_THIRD_ORDER>& v) {
