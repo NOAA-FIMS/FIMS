4f7fcd28d690194cbff5ae672916e461033a0e08
Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
2023-02-23 11:16:44 -0800

    separate out fleet_nll tests from distributions

--- inst/extdata/TMB_tests/distributions/test_distributions.R
+++ inst/extdata/TMB_tests/distributions/test_distributions.R
@@ -21,8 +21,6 @@ project_path <- getwd()
 TMB::compile(paste0(path, "/test_dnorm_distribution.cpp"), flags = "-DTMB_MODEL")
 TMB::compile(paste0(path, "/test_dlnorm_distribution.cpp"), flags = "-DTMB_MODEL")
 TMB::compile(paste0(path, "/test_dmultinom_distribution.cpp"), flags = "-DTMB_MODEL")
-TMB::compile(paste0(path, "/test_fleet_index_nll.cpp"), flags = "-DTMB_MODEL")
-TMB::compile(paste0(path, "/test_fleet_acomp_nll.cpp"), flags = "-DTMB_MODEL")
 
 test_that("dnorm unit test", {
 
@@ -73,86 +71,4 @@ test_that("dlnorm unit test", {
   dyn.unload(dynlib(paste0(path, "/test_dlnorm_distribution")))
   file.remove(paste0(path, "/", dynlib("test_dlnorm_distribution")))
   file.remove( paste0(path, "/test_dlnorm_distribution.o"))
-})
-
-test_that("dmultinom unit test", {
-
-  # setwd(project_path)
-  # on.exit(setwd(old_wd), add = TRUE)
-
-  # # dmultinom unit test
-  # # load test
-  dyn.load(dynlib(paste0(path, "/test_dmultinom_distribution")))
-  set.seed(123)
-  #Simulate new data with R
-  p = (1:10)/sum(1:10)
-  x = stats::rmultinom(1, 100, p)
-  #Calculate negative log-likelihood with R dnmultinom
-  nll = -stats::dmultinom(x, 100,p, TRUE)
-  #Initialize TMB model object with true values
-  mod = MakeADFun(data = list(x =x),
-                  parameters = list(p = p),
-                  DLL = "test_dmultinom_distribution")
-  #Compare R nll to TMB nll
-  expect_equal(nll, mod$fn())
-
-  dyn.unload(dynlib(paste0(path, "/test_dmultinom_distribution")))
-  file.remove(paste0(path, "/", dynlib("test_dmultinom_distribution")))
-  file.remove( paste0(path, "/test_dmultinom_distribution.o"))
-})
-
-
-test_that("fleet index nll unit test", {
-
-  # setwd(project_path)
-  # on.exit(setwd(old_wd), add = TRUE)
-
-  # # dmultinom unit test
-  # # load test
-  dyn.load(dynlib(paste0(path, "/test_fleet_index_nll")))
-
-  set.seed(123)
-  #Simulate new data with R
-  p = (1:10)/sum(1:10)
-  x = stats::rmultinom(1, 100, p)
-  #Calculate negative log-likelihood with R dnmultinom
-  nll = -stats::dmultinom(x, 100,p, TRUE)
-  #Initialize TMB model object with true values
-  mod = MakeADFun(data = list(y =x),
-                  parameters = list(p = p),
-                  DLL = "test_fleet_index_nll")
-  #Compare R nll to TMB nll
-  expect_equal(nll, mod$fn())
-
-  dyn.unload(dynlib(paste0(path, "/test_fleet_index_nll")))
-  file.remove(paste0(path, "/", dynlib("test_fleet_index_nll")))
-  file.remove( paste0(path, "/test_fleet_index_nll.o"))
-  
-})
-
-test_that("fleet acomp nll unit test", {
-
-  # setwd(project_path)
-  # on.exit(setwd(old_wd), add = TRUE)
-
-  # # dmultinom unit test
-  # # load test
-  dyn.load(dynlib(paste0(path, "/test_fleet_acomp_nll")))
-
-  set.seed(123)
-  #Simulate new data with R
-  y = stats::rlnorm(10, 2, 1)
-  #Calculate negative log-likelihood with R dlnorm
-  nll = -sum(stats::dlnorm(y, 2, 1, TRUE))
-  #Initialize TMB model object with true values
-  mod = MakeADFun(data = list(y = y),
-                  parameters = list(p = c(2, log(1))),
-                  DLL = "test_fleet_acomp_nll")
-  #Compare R nll to TMB nll
-  expect_equal(nll, mod$fn())
-
-  dyn.unload(dynlib(file.path(path, "test_fleet_acomp_nll")))
-  file.remove(file.path(path, dynlib("test_fleet_acomp_nll")))
-  file.remove( file.path(path, "test_fleet_acomp_nll.o"))
-  
 })
