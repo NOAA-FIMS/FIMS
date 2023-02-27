e05c3a69f1e62a2833bcd3b7f47d5219d9be4b48
Bai Li <bai.li@noaa.gov>
2023-02-27 15:00:47 -0500

    fix: export setup_gtest() and update R documents

--- R/setup_gtest.R
+++ R/setup_gtest.R
@@ -1,7 +1,8 @@
 #' function for setting up your local environment to run the gtest
 #' integration test locally. Intended for developers.
-#' @examples setup_gtest()
-setup_gtest <- function(){
+#' @examples \dontrun{setup_gtest()}
+#' @export
+setup_gtest <- function() {
   # loop over iterations within the model comparison project output
   # currently don't need, but may need once we need more than one file.
   for (i_iter in 1) {
@@ -18,10 +19,14 @@ setup_gtest <- function(){
     inputname <- paste0("om_input", i_iter, ".json")
     json_folder <- file.path("tests", "integration", "inputs", "FIMS-deterministic")
     dir.create(json_folder)
-    jsonlite::write_json(x = om_output, path = file.path(json_folder, outputname),
-        pretty = TRUE)
-    jsonlite::write_json(x = om_input, path = file.path(json_folder, inputname),
-        pretty = TRUE)
+    jsonlite::write_json(
+      x = om_output, path = file.path(json_folder, outputname),
+      pretty = TRUE
+    )
+    jsonlite::write_json(
+      x = om_input, path = file.path(json_folder, inputname),
+      pretty = TRUE
+    )
   }
   TRUE
 }
@@ -36,7 +41,7 @@ setup_and_run_gtest <- function(...) {
   setup_gtest()
   system("cmake -S . -B build -G Ninja")
   system("cmake --build build")
-  system(paste0("ctest --test-dir build",...))
+  system(paste0("ctest --test-dir build", ...))
   TRUE
 }
 
@@ -49,6 +54,6 @@ setup_and_run_gtest <- function(...) {
 run_gtest <- function(...) {
   system("cmake -S . -B build -G Ninja")
   system("cmake --build build")
-  system(paste0("ctest --test-dir build",...))
+  system(paste0("ctest --test-dir build", ...))
   TRUE
 }
