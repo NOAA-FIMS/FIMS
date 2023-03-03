274e34d97cb12dc22b16139df0ae63db9cc2bde6
Bai Li <bai.li@noaa.gov>
2023-03-03 16:15:03 -0500

    format file

--- tests/gtest/integration_test_population.cpp
+++ tests/gtest/integration_test_population.cpp
@@ -4,100 +4,6 @@
 
 namespace
 {
-    TEST(Integrated_test_works, run_all)
-    {
-        // Declare IntegrationTest object
-        IntegrationTest t(1, 1);
-        std::stringstream ss;
-        typename rapidjson::Document::MemberIterator it;
-
-        bool good = true;
-
-        // Read in input and output json files
-        rapidjson::Document input;
-        rapidjson::Document output;
-
-        // Read inputs
-        ss.str("");
-        // GoogleTest operates in the folder with executables "build/tests/gtest"
-        // so we have to go up three directories to get into FIMS folder
-        ss << "../../../tests/integration/inputs/FIMS-deterministic/om_input" << 1 << ".json";
-        t.ReadJson(ss.str(), input);
-        ss.str("");
-
-        // Read in outputs
-        ss << "../../../tests/integration/inputs/FIMS-deterministic/om_output" << 1 << ".json";
-        t.ReadJson(ss.str(), output);
-
-        // Declare singleton of population class
-        fims::Population<double> pop;
-
-        // ConfigurePopulationModel, RunModelLoop, and CheckModelOutput
-        //  methods are in integration_class.hpp
-        good = t.ConfigurePopulationModel(pop, input);
-
-        pop.numbers_at_age = t.RunModelLoop(pop, input);
-        good = t.CheckModelOutput(pop, output);
-
-        // declare vector of doubles to hold
-        // dimension folded numbers at age vector
-        // resize to nyears * nages
-        std::vector<double> test_numbers_at_age;
-        test_numbers_at_age.resize(pop.nages * pop.nyears);
-
-        // find the json member called "N.age"
-        it = output.FindMember("N.age");
-
-        if (it != output.MemberEnd())
-        {
-            rapidjson::Value &e = (*it).value;
-            for (int year = 0; year < e.Size(); year++)
-            {
-                for (int age = 0; age < e[year].Size(); age++)
-                {
-                    int index_ya = year * pop.nages + age;
-                    int index_ya2 = (year - 1) * pop.nages + (age - 1);
-                    test_numbers_at_age[index_ya] = e[year][age].GetDouble();
-
-                    // test_log << "year " << year << std::endl;
-                    // test_log << "age " << age << std::endl;
-                    // test_log << "index_ya " << index_ya << std::endl;
-                    // test_log << "pop.numbers_at_age[index_ya2] " << pop.numbers_at_age[index_ya2] * exp(-pop.mortality_Z[index_ya2])<< std::endl;
-                    // test_log << "pop.fleets.size() " << pop.fleets.size() << std::endl;
-                    // test_log << "pop.fleets[0]->selectivity->evaluate(pop.ages[age]))  " << pop.fleets[0]->selectivity->evaluate(pop.ages[age]) << std::endl;
-                    // test_log << "pop.fleets[0]->q[year] " << pop.fleets[0]->q[year] << std::endl;
-                    // test_log << "pop.fleets[0]->Fmort[year] " << pop.fleets[0]->Fmort[year] << std::endl;
-                    // test_log << "pop.fleets[0]->expected_index[year] " << pop.fleets[0]->expected_index[year] << std::endl;
-                    // test_log << "pop.fleets[0]->catch_numbers_at_age[index_ya] " << pop.fleets[0]->expected_index[year] << std::endl;
-                    // test_log << "pop.fleets[1]->q[year] " << pop.fleets[1]->q[year] << std::endl;
-                    // test_log << "pop.fleets[1]->Fmort[year] " << pop.fleets[1]->Fmort[year] << std::endl;
-                    // test_log << "pop.fleets[1]->expected_index[year] " << pop.fleets[1]->expected_index[year] << std::endl;
-                    // test_log << "pop.fleets[1]->catch_numbers_at_age[index_ya] " << pop.fleets[1]->expected_index[year] << std::endl;
-                    // test_log << "pop.proportion_female " << pop.proportion_female << std::endl;
-                    // test_log << "pop.proportion_mature_at_age[index_ya] " << pop.proportion_mature_at_age[index_ya] << std::endl;
-                    // test_log << "pop.growth " << pop.spawning_biomass[year] / pop.numbers_at_age[index_ya] / pop.proportion_female / pop.proportion_mature_at_age[index_ya] << std::endl;
-                    // test_log << "pop.spawning_biomass[year] " << pop.spawning_biomass[year] << std::endl;
-                    // test_log << "pop.recruitment->recruit_deviations[year] " << pop.recruitment->recruit_deviations[year] << std::endl;
-                    // test_log << "pop.mortality_F[index_ya] " << pop.mortality_F[index_ya] << std::endl;
-                    // test_log << "pop.mortality_Z[index_ya] " << pop.mortality_Z[index_ya] << std::endl;
-                    // test_log << "pop.numbers_at_age[index_ya] " << pop.numbers_at_age[index_ya] << std::endl;
-                    // test_log << "test_numbers_at_age[index_ya] " << test_numbers_at_age[index_ya] << "\n"
-                    //          << std::endl;
-                    // EXPECT_LE((pop.numbers_at_age[index_ya] - test_numbers_at_age[index_ya])/test_numbers_at_age[index_ya]*100,
-                    //             .01) << "differ at index " << index_ya;
-                    // EXPECT_GT(pop.fleets[0]->catch_numbers_at_age[index_ya],
-                    //           0.0)
-                    //     << "differ at index " << index_ya << "; year " << year << "; age" << age;
-                    // EXPECT_GT(pop.fleets[0]->expected_index[year],
-                    //             0.0) << "differ at index " << index_ya << "; year " << year << "; age" << age;
-                    // EXPECT_GT(pop.fleets[0]->q[year],
-                    //           0.0)
-                    //     << "differ at index " << index_ya << "; year " << year << "; age" << age;
-                }
-            }
-        }
-    }
-
     TEST(integration_test, mcp_c0_om1_works)
     {
         std::ofstream integration_test_log("log_integration_test.txt");
@@ -456,7 +362,7 @@ namespace
         }
     }
 
-    // TEST_F(PopulationPrepareTestFixture, CalculateNumbersAA_forloop_works)
+        // TEST_F(PopulationPrepareTestFixture, CalculateNumbersAA_forloop_works)
     // {
 
     //     for (size_t year = 0; year < nyears; year++)
