9ae1ff70b75ff71723fcef6a298411934dfb0e8b
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2023-03-22 16:51:16 -0700

    remove include statement

--- inst/include/interface/interface.hpp
+++ inst/include/interface/interface.hpp
@@ -15,7 +15,6 @@
  */
 
 // traits for interfacing with TMB
-#include "../population_dynamics/growth/growth.hpp"
 
 #ifdef TMB_MODEL
 // use isnan macro in math.h instead of TMB's isnan for fixing the r-cmd-check
