1f802063705f7549d39b478e7c98c6155af55148
Andrea-Havron-NOAA <Andrea-Havron-NOAA@users.noreply.github.com>
2023-03-31 17:20:49 +0000

    style: run clang format

--- inst/include/common/model.hpp
+++ inst/include/common/model.hpp
@@ -32,28 +32,29 @@ class Model {  // may need singleton
   static std::shared_ptr<Model<T> >
       fims_model; /*!< Create a shared fims_model as a pointer to Model*/
   std::shared_ptr<Information<T> >
-      fims_information; /*!< Create a shared fims_information as a pointer to Information*/
+      fims_information; /*!< Create a shared fims_information as a pointer to
+                           Information*/
 
+#ifdef TMB_MODEL
+  ::objective_function<T> *of;
+#endif
 
-    #ifdef TMB_MODEL
-        ::objective_function<T> *of;
-    #endif
+  // constructor
+  virtual ~Model() {}
 
-    // constructor
-    virtual ~Model() {}
-
-    /**
-    * Returns a single Information object for type T.
-    *
-    * @return singleton for type T
-    */
-    static std::shared_ptr<Model<T> > GetInstance() {
+  /**
+   * Returns a single Information object for type T.
+   *
+   * @return singleton for type T
+   */
+  static std::shared_ptr<Model<T> > GetInstance() {
     if (Model<T>::fims_model == nullptr) {
-        Model<T>::fims_model = std::make_shared<fims::Model<T> >();
-        Model<T>::fims_model->fims_information = fims::Information<T>::GetInstance();
+      Model<T>::fims_model = std::make_shared<fims::Model<T> >();
+      Model<T>::fims_model->fims_information =
+          fims::Information<T>::GetInstance();
     }
     return Model<T>::fims_model;
-    }
+  }
 
   /**
    * @brief Evaluate. Calculates the joint negative log-likelihood function.
@@ -61,23 +62,22 @@ class Model {  // may need singleton
   const T Evaluate() {
     typename fims::Information<T>::population_iterator it;
     for (it = this->fims_information->populations.begin();
-      it != this->fims_information->populations.end(); ++it)
-    {
-      //(*it).second points to the Population module
-      #ifdef TMB_MODEL
+         it != this->fims_information->populations.end(); ++it) {
+//(*it).second points to the Population module
+#ifdef TMB_MODEL
       (*it).second->of = this->of;
-       #endif
+#endif
       (*it).second->Prepare();
       (*it).second->Evaluate();
-
     }
 
     // nll = negative-log-likelihood (the objective function)
     T nll = 0.0;
-    //nll will loop over fleets (Fleet module does not have evaluate function yet)
-    //Sum up nlls here in model.hpp or in fleet.hpp?
-    //for(jt = this->fims_information->fleets.begin(); jt != this->fims_information->fleets.end(); ++jt ){
-        //nll += (*jt).second.Evaluate();
+    // nll will loop over fleets (Fleet module does not have evaluate function
+    // yet) Sum up nlls here in model.hpp or in fleet.hpp? for(jt =
+    // this->fims_information->fleets.begin(); jt !=
+    // this->fims_information->fleets.end(); ++jt ){ nll +=
+    // (*jt).second.Evaluate();
     //}
 
     return nll;
@@ -87,7 +87,7 @@ class Model {  // may need singleton
 // Create singleton instance of Model class
 template <typename T>
 std::shared_ptr<Model<T> > Model<T>::fims_model =
-   nullptr;  // singleton instance
+    nullptr;  // singleton instance
 }  // namespace fims
 
 #endif /* FIMS_COMMON_MODEL_HPP */
