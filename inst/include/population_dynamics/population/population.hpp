95f1d7954361e03d3371cd7fc27ee21022bb2bb4
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2023-10-19 14:10:16 -0700

    fix: exp typo

--- inst/include/population_dynamics/population/population.hpp
+++ inst/include/population_dynamics/population/population.hpp
@@ -394,9 +394,9 @@ struct Population : public fims_model_object::FIMSObject<Type> {
                    << std::endl;
     this->numbers_at_age[i_age_year] =
         this->recruitment->evaluate(this->spawning_biomass[year - 1], phi0) *
-        this->recruitment->fims::exp(log_recruit_devs[year]);
+        fims::exp(this->recruitment->log_recruit_devs[year]);
     this->expected_recruitment[year] = this->numbers_at_age[i_age_year];
-    POPULATION_LOG << " numbers at age at indexya " << i_age_year << " is "
+    POPULATION_LOG << " numbers at age at index i_age_year " << i_age_year << " is "
                    << this->numbers_at_age[i_age_year] << std::endl;
   }
 
