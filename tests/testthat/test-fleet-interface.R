62f7666fa0abb41fea149e45ec8c6374098175f5
Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
2023-10-25 16:44:32 +0000

    add fims_estimation code to fleet interface test

--- tests/testthat/test-fleet-interface.R
+++ tests/testthat/test-fleet-interface.R
@@ -1,3 +1,5 @@
+data(package = "FIMS")
+
 test_that("Fleet: selectivity IDs can be added to the
 fleet module", {
   fims <- Rcpp::Module("fims", PACKAGE = "FIMS")
@@ -64,8 +66,26 @@ test_that("Fleet: SetObservedIndexData works", {
 test_that("Fleet: evaluate_index_nll works", {
   fims <- Rcpp::Module("fims", PACKAGE = "FIMS")
   fleet_ <- new(fims$Fleet)
-  fleet_$SetObservedIndexData(1)
-  fleet_$nyears = 10;
+  nyears <- 10
+  nages <- 12
+  fleet_$nyears = nyears
+  fleet_$nages <- nages
+  fleet_$log_Fmort <- log(rep(1.0, 10))
+  fleet_$estimate_F <- TRUE
+  fleet_$random_F <- FALSE
+  fleet_$log_q <- log(1.0)
+  fleet_$estimate_q <- FALSE
+  fleet_$random_q <- FALSE
+  fleet_$log_obs_error$value <- log(sqrt(log(0.5^2 + 1)))
+  fleet_$log_obs_error$estimated <- FALSE
+  
+  survey_fleet_index <- new(fims$Index, nyears)
+  survey_fleet_index$SetIndexLikelihood(1)
+  survey_fleet_index$SetObservedIndexData(survey_fleet_index$get_id())
+  fleet_$SetObservedIndexData(survey_fleet_index$get_id())
+
+  survey_fleet_index$index_data <- seq_len(1,10,length.out=nyears)
+
   expect_equal(0.0, fleet_$evaluate_index_nll())
   fims$clear()
 })
@@ -73,7 +93,23 @@ test_that("Fleet: evaluate_index_nll works", {
 test_that("Fleet: evaluate_age_comp_nll works", {
   fims <- Rcpp::Module("fims", PACKAGE = "FIMS")
   fleet_ <- new(fims$Fleet)
-  fleet_$SetObservedAgeCompData(1)
+nages <- 12
+nyears <- 30
+  fleet_$nages <- nages
+  fleet_$nyears <- nyears
+  fleet_$log_Fmort <- log(om_output$f)
+  fleet_$estimate_F <- TRUE
+  fleet_$random_F <- FALSE
+  fleet_$log_q <- log(1.0)
+  fleet_$estimate_q <- FALSE
+  fleet_$random_q <- FALSE
+  fleet_$log_obs_error$value <- log(sqrt(log(1.0^2 + 1)))
+  fleet_$log_obs_error$estimated <- FALSE
+  fleet_$SetAgeCompLikelihood(1)
+
+  fishing_fleet_age_comp <- new(fims$AgeComp, length(catch), om_input$nages)
+  fishing_fleet_age_comp$age_comp_data <- c(t(em_input$L.age.obs$fleet1)) * em_input$n.L$fleet1
+  fleet_$SetObservedAgeCompData(fishing_fleet_age_comp$get_id())
   expect_equal(0.0, fleet_$evaluate_age_comp_nll())
   fims$clear()
 })
