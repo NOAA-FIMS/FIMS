fc3e0ffa98c893256b825c6de1d254ee2becdc8d
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2023-10-26 14:59:35 -0700

    delete extra files

--- tests/test-fims-compile.R
+++
@@ -1,8 +0,0 @@
-withr::local_options(pkg.build_extra_flags = FALSE)
-devtools::load_all()
-
-devtools::install_version("TMB", version = "1.8.0", repos = "http://cran.us.r-project.org")
-
-#run from command line: Rscript tests/test-fims-compile.R >output.txt 2>&1
-
-TMB::gdbsource("tests/testthat/test-fims-estimation-bug.R", interactive = TRUE)
