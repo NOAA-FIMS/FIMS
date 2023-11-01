3e2506a784527672c318f1a0a0ed266ace54222f
Matthew-Supernaw-NOAA <matthew.supernaw@noaa.gov>
2023-11-01 10:39:34 -0400

    Update fims_vector.hpp

--- inst/include/common/fims_vector.hpp
+++ inst/include/common/fims_vector.hpp
@@ -13,8 +13,8 @@ template<typename Type>
 class Vector{
     std::vector<Type> vec_m;
     
-//    friend bool operator==( const fims::Vector<T>& lhs,
-//                           const fims::Vector<T>& rhs );
+    friend bool operator==( const fims::Vector<T>& lhs,
+                           const fims::Vector<T>& rhs );
 public:
     //Member Types
     typedef typename std::vector<Type>::value_type value_type;
