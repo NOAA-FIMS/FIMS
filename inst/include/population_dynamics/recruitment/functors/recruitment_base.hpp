aa9e76b1cbf3c8a6ddcf6fff5f5f4d9911484dd2
Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
2023-11-08 00:16:01 +0000

    use fims::Vector<Type> for recruitment deviations

--- inst/include/population_dynamics/recruitment/functors/recruitment_base.hpp
+++ inst/include/population_dynamics/recruitment/functors/recruitment_base.hpp
@@ -31,7 +31,7 @@ template <class Type>
 struct RecruitmentBase : public fims_model_object::FIMSObject<Type> {
   static uint32_t id_g; /*!< reference id for recruitment object*/
 
-  typename fims::ModelTraits<Type>::ParameterVector
+  fims::Vector<Type>
       recruit_deviations;            /*!< A vector of recruitment deviations */
   bool constrain_deviations = false; /*!< A flag to indicate if recruitment
                                  deviations are summing to zero or not */
