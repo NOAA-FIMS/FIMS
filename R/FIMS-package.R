befefd36d8fb83cc681e594debc5e366367aad82
Andrea-Havron-NOAA <85530309+Andrea-Havron-NOAA@users.noreply.github.com>
2023-11-16 22:18:06 +0000

    add exported Rcpp classes and functions to NAMESPACE

--- R/FIMS-package.R
+++ R/FIMS-package.R
@@ -6,8 +6,23 @@
 #' @import methods
 #' @importFrom ggplot2 .data
 #' @importFrom usethis use_template ui_stop
-#' @export Population
-#' @export clear
+#' @export CreateTMBModel
 #' @export get_fixed
+#' @export get_random
+#' @export clear
+#' @export clear_logs
+#' @export Parameter
+#' @export BevertonHoltRecruitment
+#' @export Fleet
+#' @export AgeComp
+#' @export Index
+#' @export Population
+#' @export TMBDnormDistribution
+#' @export LogisticMaturity
+#' @export LogisticSelectivity
+#' @export DoubleLogisticSelectivity
+#' @export EWAAgrowth
+#' @export TMBDlnormDistribution
+#' @export TMBDmultinomDistribution
 ## usethis namespace: end
 NULL
