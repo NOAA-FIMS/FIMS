0d9697d6130f1b62103209ca1c0532ede5905da3
k-doering-NOAA <kathryn.doering@noaa.gov>
2024-05-02 09:59:07 -0700

    add skeleton of FIMS output object

--- R/output_object.R
+++ R/output_object.R
@@ -1,8 +1,33 @@
 #' Create FIMS Output Object
 #'
-#' @return The FIMS output object as a S4 object
+#' @return The FIMS output S4 object of class FIMSOutput.
 #' @examples
 #' create_fims_output()
-create_fims_output <- function() { 
+create_fims_output <- function() {
 
 }
+
+# This file defines a parent class and its children by
+# (1) setting the class;
+# (2) defining methods, using setMethod();
+# (3) setting the validators; and
+# (4) establishing the constructors (i.e., functions called by users)
+# where only the constructors are documented using roxygen.
+
+# setClass: ----
+# Classes are not currently exported, and therefore, do not need documentation.
+# See the following link if we do want to document them in the future:
+# https://stackoverflow.com/questions/7368262/how-to-properly-document-s4-class-slots-using-roxygen2
+
+setClass(
+  Class = "FIMSOutput",
+  slots = c(
+    estimates = "tibble",
+    fits = "tibble",
+    tmb = "list",
+    sdreport = "list",
+    call = "call",
+    timestamp = "vector",
+    version = "list"
+  )
+)
