a9a9943ae76c1b8bdc15a4bb8b36ae85f868b436
Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
2024-05-28 10:44:41 -0700

    Add variablevector class and expose in Rcpp

--- inst/include/interface/rcpp/rcpp_objects/rcpp_interface_base.hpp
+++ inst/include/interface/rcpp/rcpp_objects/rcpp_interface_base.hpp
@@ -35,6 +35,9 @@ class Parameter {
   bool estimated_m =
       false; /**< Is the parameter estimated? Default value is false.*/
 
+  bool random_m = 
+    false; /**< is the parameter random? Default value is false.*/
+
   /**
    * @brief Constructor for initializing Parameter.
    * @details Inputs include value, min, max, estimated.
@@ -54,6 +57,129 @@ class Parameter {
    */
   Parameter() { value_m = 0; }
 };
+
+/**
+ * @brief Rcpp representation of a Parameter vector
+ * interface between R and cpp.
+ */
+class ParameterVector{
+    static uint32_t id_g;
+   
+public:
+    Rcpp::List storage_m; //use a list because it's easier
+    uint32_t id;
+    
+    
+    /**
+     * default constructor
+     */
+    ParameterVector(){
+        this->id = ParameterVector::id_g++;
+        Parameter p;
+        this->storage_m.push_back(Rcpp::wrap(p));
+    }
+    /**
+     * constructor
+     */
+    ParameterVector(size_t size ){
+        this->id = ParameterVector::id_g++;
+        for(size_t i =0; i < size; i++){
+            Parameter p;
+            this->storage_m.push_back(Rcpp::wrap(p));
+        }
+    }
+    /**
+     * vector constructor
+     */
+    ParameterVector(Rcpp::NumericVector x, size_t size){
+        this->id = ParameterVector::id_g++;
+        for(size_t i =0; i < size; i++){
+            Parameter p = x[i];
+            this->storage_m.push_back(Rcpp::wrap(p));
+        }
+    }
+    
+    /**
+     * Accessor. First index starts is zero.
+     */
+    inline Parameter operator[](size_t pos) {
+        return this->storage_m[pos]; }
+    
+    /**
+     * Accessor. First index is one. For calling from R.
+     */
+    SEXP at(size_t pos){
+        if(pos == 0 || pos > this->storage_m.size()){
+            Rcpp::Rcout <<"Index out of range.\n";
+            return NULL;
+        }
+        return this->storage_m[pos-1];
+    }
+    
+    /**
+     * returns vector length
+     */
+    size_t size(){
+        return this->storage_m.size();
+    }
+    
+    /**
+     * resize to length "size"
+     */
+    void resize(size_t size){
+        size_t n = this->storage_m.size();
+        
+        if(size > n){
+            size_t m = size - n;
+            
+            for(size_t i = 0; i < m; i++){
+                Parameter p;
+                this->storage_m.push_back(Rcpp::wrap(p));
+            }
+        }else if(n > size){
+            size_t m = size;
+            Rcpp::List l(m);
+            for(size_t i = 0; i < m; i++){
+                l[i] = this->storage_m[i];
+            }
+            this->storage_m = l;
+        }
+        
+    }
+    
+    void set_all_estimable(bool estimable){
+        for(size_t i = 0; i < this->storage_m.size(); i++){
+            Parameter p = Rcpp::as<Parameter>(this->storage_m[i]);
+            p.estimated_m = estimable;
+            this->storage_m[i] = Rcpp::wrap(p);
+        }
+    }
+
+    void set_all_random(bool random){
+        for(size_t i = 0; i < this->storage_m.size(); i++){
+            Parameter p = Rcpp::as<Parameter>(this->storage_m[i]);
+            p.random_m = random;
+            this->storage_m[i] = Rcpp::wrap(p);
+        }
+    }
+    
+    void fill(double value){
+        for(size_t i = 0; i < this->storage_m.size(); i++){
+            Parameter p = Rcpp::as<Parameter>(this->storage_m[i]);
+            p.value_m = value;
+            this->storage_m[i] = Rcpp::wrap(p);
+        }
+    }
+    
+};
+uint32_t ParameterVector::id_g = 0;
+
+
+
+ParameterVector CreateVector(size_t size){
+    return ParameterVector(size);
+}
+
 
 /**
  *@brief Base class for all interface objects
---
+++ tests/testthat/test-unit-rcpp-interface-variable-vector.R
@@ -0,0 +1,98 @@
+
+test_that("Parameter vector works as expected", {
+
+v_size <- 10
+v1_value <- 1.0
+v2_value <- 2.0
+
+v1 <- create_vector(v_size)
+v1$fill(v1_value)
+
+v2 <- create_vector(v_size)
+v2$fill(v2_value)
+
+
+v3 <- v1+v2
+v3_value<-v1_value+v2_value
+
+for(i in 1:v_size){
+   expect_equal(v3$at(i)$value, v3_value)
+}
+
+v3 <- v1-v2
+v3_value <- v1_value-v2_value
+
+for(i in 1:v_size){
+   expect_equal(v3$at(i)$value, v3_value)
+}
+
+v3 <- v1*v2
+v3_value <- v1_value*v2_value
+
+for(i in 1:v_size){
+   expect_equal(v3$at(i)$value, v3_value)
+}
+
+v3<- v1/v2
+v3_value<-v1_value/v2_value
+
+for(i in 1:v_size){
+   expect_equal(v3$at(i)$value, v3_value)
+}
+
+
+p<-create_vector(100) #new(ParameterVector, 100)
+
+
+
+p[1]$value
+p[1]$value<-1
+p[1]$value
+
+for( i in 1:length(v)){
+print(p[i]$id)
+}
+
+p$resize(5)
+
+for( i in 1:length(p)){
+print(p[i]$id)
+}
+
+p$resize(10)
+
+for( i in 1:length(p)){
+print(p[i]$id)
+}
+
+
+var<-p[1] + cos(p[2])
+#str(var)
+print(p[1]$value)
+print(p[2]$value)
+print(cos(p[2]$value))
+#str(var)
+par$value<-3.1459
+#str(var)
+#a<-apply(X = v, MARGIN = 1, FUN = sum)
+cc<-c(1,2,3)
+dim(cc)
+l<-p$data
+r<-sum(p)
+#str(r)
+#q()
+#str(l)
+#a<-lapply(X = l, MARGIN = 1, FUN = sum)
+x <- vector("numeric",   length = 10)
+
+
+typeof(x)
+
+
+
+p1<-new(ParameterVector, 10)
+#str(fill)
+#fill(v1,3.1459)
+
+p2<-new(ParameterVector, 10)
+})
