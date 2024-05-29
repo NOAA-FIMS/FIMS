37e659c895f1490a041d7dbfb05111c4982eaeb3
Bai Li - NOAA <59936250+Bai-Li-NOAA@users.noreply.github.com>
2024-05-29 09:42:13 -0400

    doc: document units in FIMS (#591)
    - document units in code snippets of the fims-demo vignette
    - add units to output figures in the fims-demo vignette and add a spawning biomass comparison figure
    - add an R test to confirm that age composition intput can be in numbers or proportions
    - add units for index_data and age_data in data_mile1.R
    
    Co-authored-by: Kelli Johnson <kelli.johnson@noaa.gov>

--- R/data_mile1.R
+++ R/data_mile1.R
@@ -8,7 +8,8 @@
 #' @format A data frame with `r NROW(data_mile1)` observations of
 #' `r NCOL(data_mile1)` variables:
 #' \describe{
-#'   \item{type}{The type of data the row contains.}
+#'   \item{type}{The type of data the row contains. Allowed types include
+#'     `age`, `index`, `landings`, and `weight-at-age` data.}
 #'   \item{name}{A character string providing the name of the information source
 #'     that the data was collected from, e.g., `"Trawl fishery"`}
 #'   \item{age}{An integer age. Entry can be `NA` if information pertains to
@@ -17,7 +18,11 @@
 #'     Format all dates using `yyyy-mm-dd`, which can accommodate fake years
 #'     such as `0001-01-01`.}
 #'   \item{value}{The measurement of interest.}
-#'   \item{unit}{A character string specifying the units of `value`.}
+#'   \item{unit}{A character string specifying the units of `value`. Allowed
+#'     units for each data type are as follows. `mt` is used for `index`,
+#'     `landings`, and `weight-at-age` data. `number` or `proportion` are each
+#'     viable units for the composition data, where the former is the preferred
+#'     unit of measurement.}
 #'   \item{uncertainty}{A real value providing a measurement of uncertainty
 #'     for value.}
 #' }
