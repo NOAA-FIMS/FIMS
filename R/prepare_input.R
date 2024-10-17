ff4e7fa37259c5950c1449dd112c6a051c37915d
Bai-Li-NOAA <bai.li@noaa.gov>
2024-10-17 08:37:04 -0400

    refactor prepare_input()

--- R/prepare_input.R
+++ R/prepare_input.R
@@ -34,41 +34,55 @@
 #' print(initial_input)
 prepare_input <- function(
     data,
-    fleet_name,
-    selectivity = c("LogisticSelectivity"),
-    recruitment_form = c("BevertonHoltRecruitment"),
-    recruitment_distribution = c("TMBDnormDistribution"),
+    fleets,
+    recruitment = list(
+      form = "BevertonHoltRecruitment",
+      process_distribution = c(log_devs = "TMBDnormDistribution")
+    ),
     growth = c("EWAAgrowth"),
     maturity = c("LogisticMaturity")) {
+  # TODO: if there is no fleets info passed into the function,
+  # Use default values for fleets in the data@data
+  if (length(fleets) == 0) {
+    cli::cli_bullets(c(
+      "No fleet settings were provided, FIMS will apply default settings for
+      all fleets listed in the data. These include:",
+      "i" = "LogisticSelectivity for selectivity module",
+      "i" = "TMBDlnormDistribution for index data",
+      "i" = "TMBDmultinomDistribution for age composition data"
+    ))
+  }
 
+  fleet_names <- names(fleets)
 
-  names(selectivity) <- fleet_name
+  mismatch_fleet_names <- fleet_names[!(fleet_names %in% unique(data@data$name))]
+  if (length(mismatch_fleet_names > 0)) {
+    cli::cli_abort(c(
+      "The name of the fleets for selectivity settings must match
+      the fleet names from the data:",
+      "i" = "There {?is/are} {length(missmatch_fleet_names)}
+            fleet{?s} missing from the data.",
+      "x" = "The following fleet name{?s} {?is/are} missing from the data:
+            {paste(missmatch_fleet_names, collapse = ', ')}"
+    ))
+  }
+
   module_list <- list(
-    selectivity = selectivity,
-    recruitment_form = recruitment_form,
-    recruitment_distribution = recruitment_distribution,
+    fleets = fleets,
+    recruitment = recruitment,
     growth = growth,
     maturity = maturity
   )
 
-  if (length(selectivity) != length(fleet_name)) {
-    cli::cli_abort(c(
-      "Number of selectivity forms must match the number of fleets:",
-      "i" = "There {?is/are} {length(fleet_name)} fleet{?s}.",
-      "x" = "{length(selectivity)} selectivity form{?s} {?has/have} been provided."
-    ))
-  }
-
   parameter_input <- list()
 
-  for (i in 1:length(selectivity)) {
-    selectivity_temp <- prepare_selectivity_default(fleet_name[i], selectivity[i])
+  for (i in 1:length(fleets)) {
+    selectivity_temp <- prepare_selectivity_default(names(fleets)[i], fleets[[i]]$selectivity)
     parameter_input <- c(parameter_input, selectivity_temp)
   }
 
   recruitment_temp <- prepare_recruitment_default(
-    recruitment_form = recruitment_form,
-    recruitment_distribution = recruitment_distribution,
+    recruitment = recruitment,
     data = data
   )
   parameter_input <- c(parameter_input, recruitment_temp)
@@ -81,9 +95,12 @@ prepare_input <- function(
 
   maturity_temp <- prepare_maturity_default(form = maturity)
   parameter_input <- c(parameter_input, maturity_temp)
+  # parameter_input_names <- names(unlist(parameter_input, recursive = TRUE))
 
   flat_list <- unlist(parameter_input, recursive = TRUE)
+  logical_names <- grep("estimated", names(flat_list), value = TRUE)
   parameter_input_list <- as.list(flat_list)
+  parameter_input_list[logical_names] <- as.logical(parameter_input_list[logical_names])
 
   output <- list(
     parameter_input_list = parameter_input_list,
@@ -103,20 +120,7 @@ prepare_Logistic_default <- function() {
       value = 1, estimated = TRUE
     )
   )
-  return(default)
-}
-
-prepare_DoubleLogistic_default <- function() {
-  # Can pass data to this function in the future to
-  # dynamically set up initial values
-  default <- list(
-    inflection_point = list(
-      value = 2, estimated = TRUE
-    ),
-    slope = list(
-      value = 1, estimated = TRUE
-    )
-  )
+  attr(default, "types") = rep("Parameter", length(default))
   return(default)
 }
 
@@ -125,6 +129,7 @@ prepare_EWAAgrowth_default <- function(data) {
     ages = data@ages,
     weights = rep(0.001, data@n_ages)
   )
+  attr(default, "types") = rep("data", length(default))
   return(default)
 }
 
@@ -170,41 +175,54 @@ prepare_BevertonHoltRecruitment_default <- function(data) {
       value = -log(1.0 - 0.75) + log(0.75 - 0.2), estimated = TRUE
     ),
     log_devs = list(
-      value = rep(0.01, data@n_years),
+      value = rep(0.0, data@n_years),
       nyears = data@n_years,
       estimated = TRUE
     )
   )
+  attr(default, "types") = c("Parameter", "Parameter", "ParameterVector")
   return(default)
 }
 
-prepare_TMBDnormDistribution_default <- function(data) {
+prepare_TMBDnormDistribution_default <- function(n_obs) {
   default <- list(
     log_sd = list(
-      value = log(0.4), estimated = FALSE
+      # Hard-coded a single value for the dimensions of ParameterVector
+      value = rep(log(0.4), 1), estimated = FALSE
     ),
-    x = list(value = 0),
-    expected_values = list(value = 0)
+    x = list(value = rep(0, n_obs)),
+    expected_values = list(value = rep(0, n_obs))
   )
-  return(default)
+  attr(default, "types") = rep("ParameterVector", length(default))
+  distribution_default <- list(default)
+  names(distribution_default) <- "TMBDnormDistribution"
+  return(distribution_default)
 }
 
-prepare_recruitment_default <- function(recruitment_form,
-                                        recruitment_distribution,
+prepare_recruitment_default <- function(recruitment,
                                         data) {
-  recruitment_default <- list(switch(recruitment_form,
+  form <- recruitment[["form"]]
+  process_default <- list(switch(form,
     "BevertonHoltRecruitment" = prepare_BevertonHoltRecruitment_default(data)
   ))
-  names(recruitment_default) <- recruitment_form
+  names(process_default) <- form
 
-  if (recruitment_default[[recruitment_form]]$log_devs$estimated == TRUE) {
-    distribution_default <- list(switch(recruitment_distribution,
-      "TMBDnormDistribution" = prepare_TMBDnormDistribution_default(data)
-    ))
-    names(distribution_default) <- paste0("Recruitment", recruitment_distribution)
+  distribution_input <- recruitment[["process_distribution"]]
+  distribution <- list()
+  if (length(distribution_input) > 0) {
+    for (i in seq_along(distribution_input)) {
+      distribution_default <- list(switch(distribution_input[i],
+        "TMBDnormDistribution" = prepare_TMBDnormDistribution_default(
+          n_obs = data@n_years
+        )
+      ))
+      names(distribution_default) <- paste0(names(distribution_input)[i])
+
+      distribution[i] <- list(distribution_default)
+    }
   }
-
-  default <- c(recruitment_default, distribution_default)
+  names(distribution) <- "Recruitment"
+  default <- c(process_default, distribution)
   return(default)
 }
 
@@ -239,11 +257,10 @@ prepare_recruitment_default <- function(recruitment_form,
 #' }
 #'
 #' @export
-update_input <- function(input, ...) {
+update_input <- function(input, update_arg) {
   current_input <- input
-  updated_input <- list(...)
-  parameter_vector <- as.vector(input$parameter_input)
-  new_parameter_input <- `[<-`(input$parameter_input, names(updated_input), updated_input)
+  updated_input <- update_arg
+  new_parameter_input <- modifyList(input$parameter_input_list, updated_input)
   new_input <- list(
     parameter_input_list = new_parameter_input,
     module_list = current_input$module_list
@@ -251,80 +268,144 @@ update_input <- function(input, ...) {
   return(new_input)
 }
 
-setup_module <- function(module_name, input, module_id = 1) {
+setup_module <- function(module_name, input) {
   module_list <- input$module_list
   parameter_list <- input$parameter_input_list
-  module_names <- strsplit(names(parameter_list), "\\.")
+  parameter_sections <- strsplit(names(parameter_list), "\\.")
 
-  model_form <- get(module_list[[module_name]][module_id])
+  model_form_char <- module_list[[module_name]]
+  model_form <- get(model_form_char)
 
   # Identify parameter IDs for the model
-  fleet_name <- names(module_list[[module_name]][module_id])
+  fleet_name <- names(model_form_char)
   if (!is.null(fleet_name)) {
-    parameter_ids <- which(sapply(module_names, function(x) all(c(module_list[[module_name]][module_id], fleet_name) %in% x)))
+    parameter_ids <- which(sapply(parameter_sections, function(x) all(c(module_list[[module_name]][module_id], fleet_name) %in% x)))
   } else {
-    parameter_ids <- which(sapply(module_names, function(x) all(c(module_list[[module_name]][module_id]) %in% x)))
+    parameter_ids <- which(sapply(parameter_sections, function(x) all(c(module_list[[module_name]][["form"]]) %in% x)))
   }
 
   # Extract the parameters for the model
   parameter_input <- parameter_list[parameter_ids]
+  param_names <- sapply(strsplit(names(parameter_input), "\\."), `[`, 2)
+  unique_param_names <- unique(param_names)
 
-  # Determine the number of parameters
-  num_parameters <- length(parameter_input)
 
-  # Create a list of Parameter objects
-  parameter_objects <- vector("list", num_parameters / 2) # Assuming each pair consists of value and estimated
+  # parameter_keys <- grep(paste0("^", model_form_char, "\\."), names(parameter_input), value = TRUE)
+  #
+  # param_names <- unique(sub("\\.value$|\\.estimated$", "", parameter_keys))
+  # unique_param_names <- unique(sub(paste0(model_form_char, "."), "", param_names))
+  parameter_objects <- list()
+  for (i in seq_along(unique_param_names)) {
+    parameter_objects[[i]] <- new(Parameter)
+    parameter_objects[[i]][["value"]] <-
+      parameter_input[[paste0(model_form_char, ".", unique_param_names[i], ".value")]]
 
-  # Populate the list with Parameter objects
-  for (i in seq(1, num_parameters, by = 2)) {
-    parameter_objects[[ceiling(i / 2)]] <- new(
-      Parameter,
-      unlist(parameter_input[i]),
-      unlist(parameter_input[i + 1])
-    )
+    parameter_objects[[i]][["estimated"]] <-
+      parameter_input[[paste0(model_form_char, ".", unique_param_names[i], ".estimated")]]
   }
 
   # Create the model object using do.call
   module_object <- do.call(new, c(model_form, parameter_objects))
 
   # Return the created model object
-  return(module_object)
+  return(list(module_object = module_object, parameter_objects = parameter_objects))
+}
+
+setup_recruitment <- function(input) {
+  module_list <- input[["module_list"]]
+  parameter_list <- input[["parameter_input_list"]]
+  parameter_sections <- strsplit(names(parameter_list), "\\.")
+
+  form <- module_list[["recruitment"]][["form"]]
+  module_form <- get(form)
+
+  parameter_ids <- which(sapply(parameter_sections, function(x) all(c(form) %in% x)))
+  # Extract the parameters for the model
+  parameter_input <- parameter_list[parameter_ids]
+  param_names <- sapply(strsplit(names(parameter_input), "\\."), `[`, 2)
+  unique_param_names <- unique(param_names)
+
+  module_object <- new(module_form)
+  for (i in seq_along(unique_param_names)) {
+    module_object[[unique_param_names[i]]][["value"]] <-
+      parameter_input[[paste0(form, ".", unique_param_names[i], ".value")]]
+  }
+  # Create the model object using do.call
+  module_object <- do.call(new, c(module_form, parameter_objects))
 }
 
 setup_input <- function(data, input) {
   module_list <- input[["module_list"]]
-  parameter_list <- input$parameter_input_list
-  fleet_name <- names(module_list[["selectivity"]])
+  parameter_list <- input[["parameter_input_list"]]
+  fleet_name <- names(module_list[["fleets"]])
 
   clear()
 
-  maturity <- setup_module("maturity", input)
+  maturity <- setup_module(
+    module_name = "maturity",
+    input = input
+  )
+
+  recruitment_form <- module_list[["recruitment"]][["form"]]
+  recruitment <- setup_recruitment(
+    form = recruitment_form,
+    input = input
+  )
 
   fishing_selectivity <- setup_module("selectivity", input, module_id = 1)
   survey_selectivity <- setup_module("selectivity", input, module_id = 2)
 }
 
 # example
+fleets <- list(
+  fleet1 = list(
+    selectivity = "LogisticSelectivity",
+    data_distribution = c(
+      Index = "TMBDlnormDistribution",
+      AgeComp = "TMBDmultinomDistribution"
+    )
+  ),
+  survey1 = list(
+    selectivity = "LogisticSelectivity",
+    data_distribution = c(
+      Index = "TMBDlnormDistribution",
+      AgeComp = "TMBDmultinomDistribution"
+    )
+  )
+)
+
+
+load(test_path("fixtures", "integration_test_data.RData"))
+weight_names <- paste0("EWAAgrowth.weights", 1:12)
+weights <- setNames(om_input_list[[1]]$W.mt, weight_names)
+
 data(data_mile1)
-data <- FIMS::FIMSFrame(data_mile1)
+initial_input <- data_mile1 |>
+  FIMS::FIMSFrame() |>
+  prepare_input(
+    fleets = fleets,
+    recruitment = list(
+      form = "BevertonHoltRecruitment",
+      process_distribution = c(log_devs = "TMBDnormDistribution")
+    ),
+    growth = "EWAAgrowth",
+    maturity = "LogisticMaturity"
+  ) |>
+  update_input(
+    update_arg = c(
+      list(
+        LogisticMaturity.slope.value = 1.5,
+        LogisticMaturity.slope.estimated = FALSE,
+        survey1.LogisticSelectivity.inflection_point.estimated = FALSE
+      ),
+      as.list(weights) # Pass the weights as a named list
+    )
+  )
 
-initial_input <- prepare_input(
-  data = data,
-  fleet_name = c("fleet1", "survey1"),
-  selectivity = c("LogisticSelectivity", "LogisticSelectivity"),
-  recruitment_form = "BevertonHoltRecruitment",
-  recruitment_distribution = "TMBDnormDistribution",
-  growth = "EWAAgrowth",
-  maturity = "LogisticMaturity"
-)
+initial_input |>
+  setup_recruitment()
 
 
-updated_input <- update_input(
-  input = initial_input,
-  LogisticMaturity.slope.value = 1.5,
-  LogisticMaturity.slope.estimated = FALSE,
-  survey1.LogisticSelectivity.inflection_point.estimated = FALSE
-)
 
 setup_input(data, input = updated_input)
 
@@ -337,3 +418,8 @@ setup_recruitment("BevertonHoltRecruitment", data, parameter) |>
   setup_fleet("Fleet1", parameter) |>
   setup_selectivity("Survey1", data, parameter) |>
   setup_fleet("Survey1", parameter)
+#
+
+
+
+
