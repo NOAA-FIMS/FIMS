58c36e61964424fdee0a79175a3be1c975c1c45c
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2025-03-03 15:31:36 -0800

    Uses data in template for testthat
    * Implement # * ---- for creating an outline in the file using VS Code or RStudio
    * Use data in the template to autopopulate things, right now just the function name
    * Move documentation to the use_testthat_template() function instead of in the template

--- R/use_template.R
+++ R/use_template.R
@@ -6,6 +6,22 @@
 #' argument `name`. If the test file already exists, an error message is
 #' returned and no changes are made.
 #'
+#' @details
+#' There are three minimum testing criteria for FIMS, which should be validated
+#' for every R function within the package. The template file sets up a
+#' section for each of the three following test criteria:
+#'
+#' 1. Input and output correctness [IO correctness]: ensure that the function
+#'    behaves as expected with correct inputs and returns the expected outputs.
+#' 1. Edge-case handling [Edge handling]: validate the function's performance
+#'    with invalid inputs and unusual scenarios.
+#' 1. Built-in errors and warnings [Error handling]: confirm that appropriate
+#'    error and warning messages are triggered under exceptional conditions.
+#'
+#' Above every expectation within the test file there should be a call to
+#' `#' @description` that fits on one line describing the test. The information
+#' will be used in the bookdown report of the testing results.
+#'
 #' @param name A character string providing the name of the R function that you
 #'   want to test. The name will be used to create the file name, i.e.,
 #'   tests/testthat/test-{name}.R. If `name` is not specified, the function
@@ -27,11 +43,17 @@
 #' FIMS:::use_testthat_template("new_function")
 #' }
 use_testthat_template <- function(name) {
+  # TODO: add the ability to add a function to a file that already exists
+  # TODO: add the ability to also pass the arguments for the function or find
+  #       them within the code base and ensure that the template includes the
+  #       necessary structure for each input argument
   path <- file.path("tests", "testthat", paste0("test-", name, ".R"))
   if (!file.exists(path)) {
+    function_name <- gsub("-", "_", name)
     usethis::use_template(
       template = "testthat_template.R",
       save_as = path,
+      data = list(function_name = function_name),
       package = "FIMS"
     )
   } else {
