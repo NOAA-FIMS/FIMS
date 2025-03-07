8f1b1ae312e8298a353d552044cc96ab74d6dc95
Mark C <mciechan@umich.edu>
2025-03-07 01:23:37 -0500

    refactor(initialize_comp): Combines functions
    Combine functions initialize_length_comp() and initialize_age_comp()
    into one function initialize_comp(). Replaces calls to the
    old functions with calls to the new function.
    Rebase to dev and use of purrr::walk()
    
    test: Also added several tests for the new function, including tests
    that verify the function correctly returns error messages on error.
    test: Added checks to tests to verify that the returned module
    contains the correct class variable and does not contain
    the incorrect class variable.
    
    Close #756

--- tests/testthat/test-initialize_modules.R
+++ tests/testthat/test-initialize_modules.R
@@ -56,3 +56,80 @@ test_that("initialize_fims clears previous FIMS settings before initializing", {
   mockery::expect_called(mock_clear, 1)
   clear()
 })
+
+test_that("initialize_comp correctly returns error on unknown fleet_name", {
+  expect_error(
+    initialize_comp(
+      data = data,
+      fleet_name = "unknownfleet",
+      type = "AgeComp"
+    ),
+    "Fleet `unknownfleet` not found in the data object.")
+  clear()
+})
+
+
+test_that("initialize_comp correctly returns error on unknown type", {
+  expect_error(
+    initialize_comp(
+      data = data,
+      fleet_name = "fleet1",
+      type = "unknown"
+    ),
+    "should be one of")
+
+  clear()
+})
+
+test_that("initialize_comp works for type = AgeComp", {
+  result <-  initialize_comp(
+    data = data,
+    fleet_name = "fleet1",
+    type = "AgeComp"
+  )
+  expect_type(result, "S4")
+  # expect that class contain correct function
+  expect_no_error(result$age_comp_data)
+  expect_true("age_comp_data" %in% names(result))
+  # expect that class does not contain the other
+  #   type of function
+  expect_error(result$length_comp_data)
+  expect_false("length_comp_data" %in% names(result))
+
+  expect_equal(
+    result$age_comp_data$toRVector(),
+    data |>
+      get_data() |>
+      dplyr::filter(type == "age", name == "fleet1") |>
+      dplyr::mutate(out = value * uncertainty) |>
+      dplyr::pull(out)
+  )
+
+  clear()
+})
+
+test_that("initialize_comp works for type = LengthComp", {
+  result <-  initialize_comp(
+    data = data,
+    fleet_name = "fleet1",
+    type = "LengthComp"
+  )
+  expect_type(result, "S4")
+  # expect that class contain correct function
+  expect_no_error(result$length_comp_data)
+  expect_true("length_comp_data" %in% names(result))
+  # expect that class does not contain the other
+  #   type of function
+  expect_error(result$age_comp_data)
+
+  expect_equal(
+    result$length_comp_data$toRVector(),
+    data |>
+      get_data() |>
+      dplyr::filter(type == "length", name == "fleet1") |>
+      dplyr::mutate(out = value * uncertainty) |>
+      dplyr::pull(out)
+  )
+
+  clear()
+})
