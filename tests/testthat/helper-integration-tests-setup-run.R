e48980c9c7c1e47ecd24492646d4831f1078c573
Bai Li - NOAA <59936250+Bai-Li-NOAA@users.noreply.github.com>
2025-03-26 15:59:38 +0000

    refactor(test): Refactor test fixtures
    * move the test datageneration script to a helper script
      under tests/testthat, generating multiple datasets for use in tests
    * create helper-integration-tests-setup-run.R to run FIMS and generate
      multiple fit results for use in tests and a modified_parameters list
      for wrapper tests
    * clean up integration tests with wrappers by directly using test fixtures
    * update .gitignore to exclude test fixtures from the remote repo, as they
      must be regenerated after modifying FIMS, and fit*.RDS files are too
      large for GitHub.

---
+++ tests/testthat/helper-integration-tests-setup-run.R
@@ -0,0 +1,205 @@
+# This R script generates multiple fit results for use in tests located in
+# tests/testthat. For example, it creates fits from FIMS runs with only age 
+# composition, only length composition data, or data with missing values.
+
+# Load necessary data for the integration test
+load(test_path("fixtures", "integration_test_data.RData"))
+
+# Set the iteration ID to 1 for accessing specific input/output list
+iter_id <- 1
+
+# Extract model input and output data for the specified iteration
+om_input <- om_input_list[[iter_id]]
+om_output <- om_output_list[[iter_id]]
+em_input <- em_input_list[[iter_id]]
+
+# Define modified parameters for different modules
+modified_parameters <- vector(mode = "list", length = length(iter_id))
+modified_parameters[[iter_id]] <- list(
+  fleet1 = list(
+    Fleet.log_Fmort.value = log(om_output_list[[iter_id]][["f"]])
+  ),
+  survey1 = list(
+    LogisticSelectivity.inflection_point.value = 1.5,
+    LogisticSelectivity.slope.value = 2,
+    Fleet.log_q.value = log(om_output_list[[iter_id]][["survey_q"]][["survey1"]])
+  ),
+  recruitment = list(
+    BevertonHoltRecruitment.log_rzero.value = log(om_input_list[[iter_id]][["R0"]]),
+    BevertonHoltRecruitment.log_devs.value = om_input_list[[iter_id]][["logR.resid"]][-1],
+    # TODO: integration tests fail after setting BevertonHoltRecruitment.log_devs.estimated
+    # to TRUE. We need to debug the issue, then update the line below accordingly.
+    BevertonHoltRecruitment.log_devs.estimated = FALSE,
+    DnormDistribution.log_sd.value = om_input_list[[iter_id]][["logR_sd"]]
+  ),
+  maturity = list(
+    LogisticMaturity.inflection_point.value = om_input_list[[iter_id]][["A50.mat"]],
+    LogisticMaturity.inflection_point.estimated = FALSE,
+    LogisticMaturity.slope.value = om_input_list[[iter_id]][["slope.mat"]],
+    LogisticMaturity.slope.estimated = FALSE
+  ),
+  population = list(
+    Population.log_init_naa.value = log(om_output_list[[iter_id]][["N.age"]][1, ])
+  )
+)
+saveRDS(
+  modified_parameters,
+  file = testthat::test_path("fixtures", "parameters_model_comparison_project.RDS")
+)
+
+# Estimation run with age and length comp using wrappers ----
+# Run FIMS using the setup_and_run_FIMS_with_wrappers function
+fit_age_length_comp <- setup_and_run_FIMS_with_wrappers(
+  iter_id = iter_id,
+  om_input_list = om_input_list,
+  om_output_list = om_output_list,
+  em_input_list = em_input_list,
+  estimation_mode = TRUE,
+  modified_parameters = modified_parameters
+)
+clear()
+
+# Save FIMS results as a test fixture for additional fimsfit tests
+saveRDS(
+  fit_age_length_comp,
+  file = testthat::test_path("fixtures", "fit_age_length_comp.RDS")
+)
+
+# Estimation run with age comp only using wrappers ----
+# Load test data for age composition from an RDS file
+data_age_comp <- readRDS(test_path("fixtures", "data_age_comp.RDS"))
+
+# Define fleet and survey specifications
+fleet1 <- survey1 <- list(
+  selectivity = list(form = "LogisticSelectivity"),
+  data_distribution = c(
+    Index = "DlnormDistribution",
+    AgeComp = "DmultinomDistribution"
+  )
+)
+
+# Run FIMS model with following steps
+# * Create default parameters with fleet1 and survey1 specifications
+# * Update parameters if any modifications are provided
+# * Initialize FIMS with the provided data and parameters
+# * Fit the FIMS model with optimization enabled
+fit_agecomp <- data_age_comp |>
+  create_default_parameters(
+    fleets = list(fleet1 = fleet1, survey1 = survey1)
+  ) |>
+  update_parameters(modified_parameters = modified_parameters[[iter_id]]) |>
+  initialize_fims(data = data_age_comp) |>
+  fit_fims(optimize = TRUE)
+
+clear()
+
+# Save FIMS results as a test fixture for additional fimsfit tests
+saveRDS(
+  fit_agecomp,
+  file = testthat::test_path("fixtures", "fit_agecomp.RDS")
+)
+
+# Load a second dataset that contains missing age composition data
+data_age_comp_na <- readRDS(test_path("fixtures", "data_age_comp_na.RDS"))
+# Fit the FIMS model using the second dataset (with missing values)
+fit_agecomp_na <- data_age_comp_na |>
+  create_default_parameters(
+    fleets = list(fleet1 = fleet1, survey1 = survey1)
+  ) |>
+  update_parameters(modified_parameters = modified_parameters[[iter_id]]) |>
+  initialize_fims(data = data_age_comp_na) |>
+  fit_fims(optimize = TRUE)
+
+clear()
+
+# Save FIMS results as a test fixture for additional fimsfit tests
+saveRDS(
+  fit_agecomp_na,
+  file = testthat::test_path("fixtures", "fit_agecomp_na.RDS")
+)
+
+# Estimation run with length comp only using wrappers ----
+# Load test data for length composition from an RDS file
+data_length_comp <- readRDS(test_path("fixtures", "data_length_comp.RDS"))
+# Define fleet1 and survey1 specifications
+fleet1 <- survey1 <- list(
+  selectivity = list(form = "LogisticSelectivity"),
+  data_distribution = c(
+    Index = "DlnormDistribution",
+    LengthComp = "DmultinomDistribution"
+  )
+)
+
+# Run FIMS model with following steps:
+# * Create default parameters with fleet1 and survey1 specifications
+# * Update parameters if any modifications are provided
+# * Initialize FIMS with the provided data and parameters
+# * Fit the FIMS model with optimization enabled
+fit_lengthcomp <- data_length_comp |>
+  create_default_parameters(
+    fleets = list(fleet1 = fleet1, survey1 = survey1)
+  ) |>
+  update_parameters(modified_parameters = modified_parameters[[iter_id]]) |>
+  initialize_fims(data = data_length_comp) |>
+  fit_fims(optimize = TRUE)
+
+clear()
+
+# Save FIMS results as a test fixture for additional fimsfit tests
+saveRDS(
+  fit_lengthcomp,
+  file = testthat::test_path("fixtures", "fit_lengthcomp.RDS")
+)
+
+# Load a second dataset that contains missing length composition data
+data_length_comp_na <- readRDS(test_path("fixtures", "data_length_comp_na.RDS"))
+# Fit the FIMS model using the second dataset (with missing values)
+fit_lengthcomp_na <- data_length_comp_na |>
+  create_default_parameters(
+    fleets = list(fleet1 = fleet1, survey1 = survey1)
+  ) |>
+  update_parameters(modified_parameters = modified_parameters[[iter_id]]) |>
+  initialize_fims(data = data_length_comp_na) |>
+  fit_fims(optimize = TRUE)
+
+clear()
+
+# Save FIMS results as a test fixture for additional fimsfit tests
+saveRDS(
+  fit_lengthcomp_na,
+  file = testthat::test_path("fixtures", "fit_lengthcomp_na.RDS")
+)
+
+# Estimation run with age and length comp with NAs ----
+# Load test data with both age and length composition data, which contains missing values
+data_age_length_comp_na <- readRDS(test_path("fixtures", "data_age_length_comp_na.RDS"))
+# Define fleet1 and survey1 specifications
+fleet1 <- survey1 <- list(
+  selectivity = list(form = "LogisticSelectivity"),
+  data_distribution = c(
+    Index = "DlnormDistribution",
+    AgeComp = "DmultinomDistribution",
+    LengthComp = "DmultinomDistribution"
+  )
+)
+
+# Run FIMS model with the following steps:
+# * Create default parameters with fleet1 and survey1 specifications
+# * Update parameters if any modifications are provided
+# * Initialize FIMS with the provided data (age and length composition with missing values)
+# * Fit the FIMS model with optimization enabled
+fit_age_length_comp_na <- data_age_length_comp_na |>
+  create_default_parameters(
+    fleets = list(fleet1 = fleet1, survey1 = survey1)
+  ) |>
+  update_parameters(modified_parameters = modified_parameters[[iter_id]]) |>
+  initialize_fims(data = data_age_length_comp_na) |>
+  fit_fims(optimize = TRUE)
+
+clear()
+
+# Save FIMS results as a test fixture for additional fimsfit tests
+saveRDS(
+  fit_age_length_comp_na,
+  file = testthat::test_path("fixtures", "fit_age_length_comp_na.RDS")
+)
