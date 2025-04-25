146cda03d3502d896bc05f6187c136df4445b7b5
Kathryn Doering <kathryn.doering@noaa.gov>
2025-04-25 18:00:29 +0000

    tests: reformat test-initialize_modules

--- tests/testthat/test-initialize_modules.R
+++ tests/testthat/test-initialize_modules.R
@@ -1,3 +1,5 @@
+# test_initialize_modules ----
+## Setup ----
 data <- FIMS::FIMSFrame(data1)
 
 fleet1 <- survey1 <- list(
@@ -14,63 +16,14 @@ fleets <- list(fleet1 = fleet1, survey1 = survey1)
 
 default_parameters <- create_default_parameters(data, fleets = fleets)
 
-test_that("initialize_fims handles missing parameters input correctly", {
-  expect_error(
-    initialize_fims(data = data),
-    "argument must be a non-missing list."
-  )
-  clear()
-})
-
-test_that("initialize_fims handles non-list parameters input correctly", {
-  expect_error(
-    initialize_fims(parameters = "not_a_list", data = data),
-    "argument must be a non-missing list."
-  )
-  clear()
-})
-
-test_that("initialize_fims fails when no fleets are provided", {
-  parameters_no_fleets <- default_parameters
-  parameters_no_fleets[["modules"]][["fleets"]] <- NULL
-  expect_error(
-    initialize_fims(parameters = parameters_no_fleets, data = data),
-    "No fleets found in the provided"
-  )
-  clear()
-})
+## IO correctness ----
 
 test_that("initialize_fims works", {
+  #' @description initialize_fims works
   result <- initialize_fims(parameters = default_parameters, data = data)
 
   expect_type(result, "list")
   expect_named(result, "parameters")
-  clear()
-})
-
-test_that("initialize_comp correctly returns error on unknown fleet_name", {
-  expect_error(
-    initialize_comp(
-      data = data,
-      fleet_name = "unknownfleet",
-      type = "AgeComp"
-    ),
-    "Fleet `unknownfleet` not found in the data object."
-  )
-  clear()
-})
-
-
-test_that("initialize_comp correctly returns error on unknown type", {
-  expect_error(
-    initialize_comp(
-      data = data,
-      fleet_name = "fleet1",
-      type = "unknown"
-    ),
-    "should be one of"
-  )
-
   clear()
 })
 
@@ -80,12 +33,13 @@ test_that("initialize_comp works for type = AgeComp", {
     fleet_name = "fleet1",
     type = "AgeComp"
   )
+  #' @description initialize_fims works for AgeComp: returns an S4 object
   expect_type(result, "S4")
-  # expect that class contain correct function
+  #' @description initialize_fims works for AgeComp: class contains correct function
   expect_no_error(result$age_comp_data)
   expect_true("age_comp_data" %in% names(result))
-  # expect that class does not contain the other
-  #   type of function
+  #' @description initialize_fims works: class does not contain the other type of
+  #'   function
   expect_error(result$length_comp_data)
   expect_false("length_comp_data" %in% names(result))
 
@@ -107,12 +61,13 @@ test_that("initialize_comp works for type = LengthComp", {
     fleet_name = "fleet1",
     type = "LengthComp"
   )
+  #' @description initialize_fims works for LengthComp: returns an S4 object
   expect_type(result, "S4")
-  # expect that class contain correct function
+  #' @description initialize_fims works for LengthComp: class contains correct function
   expect_no_error(result$length_comp_data)
   expect_true("length_comp_data" %in% names(result))
-  # expect that class does not contain the other
-  #   type of function
+  #' @description initialize_fims works: class does not contain the other type of
+  #'   function
   expect_error(result$age_comp_data)
 
   expect_equal(
@@ -126,3 +81,63 @@ test_that("initialize_comp works for type = LengthComp", {
 
   clear()
 })
+
+## Edge handling ----
+
+## Error handling ----
+
+test_that("initialize_fims handles missing parameters input correctly", {
+  #' @description initialize_fims handles missing parameters input correctly
+  expect_error(
+    initialize_fims(data = data),
+    "argument must be a non-missing list."
+  )
+  clear()
+})
+
+test_that("initialize_fims handles non-list parameters input correctly", {
+  #' @description initialize_fims handles non-list parameters input correctly
+  expect_error(
+    initialize_fims(parameters = "not_a_list", data = data),
+    "argument must be a non-missing list."
+  )
+  clear()
+})
+
+test_that("initialize_fims fails when no fleets are provided", {
+  #' @description initialize_fims fails when no fleets are provided
+  parameters_no_fleets[["modules"]][["fleets"]] <- NULL
+  expect_error(
+    initialize_fims(parameters = parameters_no_fleets, data = data),
+    "No fleets found in the provided"
+  )
+  clear()
+})
+
+test_that("initialize_comp correctly returns error on unknown fleet_name", {
+  expect_error(
+    initialize_comp(
+      data = data,
+      fleet_name = "unknownfleet",
+      type = "AgeComp"
+    ),
+    "Fleet `unknownfleet` not found in the data object."
+  )
+  clear()
+})
+
+test_that("initialize_comp correctly returns error on unknown type", {
+  expect_error(
+    initialize_comp(
+      data = data,
+      fleet_name = "fleet1",
+      type = "unknown"
+    ),
+    "should be one of"
+  )
+
+  clear()
+})
+
+# TODO: most lines with no coverage are error checks that are not verified to 
+# work via the tests.
