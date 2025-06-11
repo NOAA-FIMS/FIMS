ccf6bb6495b4a1e492c479269023a8f29ff2500f
Andrea-Havron-NOAA <85530309+Andrea-Havron-NOAA@users.noreply.github.com>
2025-06-11 20:32:28 +0000

    switch to TMBad
    - add DTMBAD_FRAMEWORK to Makevars
    - add TMBad type to def and interface
    - move init.hpp to src and remove TMB CALLDEF defintions
    - move rcpp module code to fims_modules.hpp in src

--- inst/include/interface/rcpp/rcpp_objects/rcpp_distribution.hpp
+++ inst/include/interface/rcpp/rcpp_objects/rcpp_distribution.hpp
@@ -372,10 +372,15 @@ class DnormDistributionsInterface : public DistributionsInterfaceBase {
    * @return A boolean of true.
    */
   virtual bool add_to_fims_tmb() {
+    #ifdef TMBAD_FRAMEWORK
+        this->add_to_fims_tmb_internal<TMB_FIMS_REAL_TYPE>();
+        this->add_to_fims_tmb_internal<TMBAD_FIMS_TYPE>();
+    #else
     this->add_to_fims_tmb_internal<TMB_FIMS_REAL_TYPE>();
     this->add_to_fims_tmb_internal<TMB_FIMS_FIRST_ORDER>();
     this->add_to_fims_tmb_internal<TMB_FIMS_SECOND_ORDER>();
     this->add_to_fims_tmb_internal<TMB_FIMS_THIRD_ORDER>();
+    #endif
 
     return true;
   }
@@ -655,10 +660,15 @@ class DlnormDistributionsInterface : public DistributionsInterfaceBase {
    * @return A boolean of true.
    */
   virtual bool add_to_fims_tmb() {
+    #ifdef TMBAD_FRAMEWORK
+        this->add_to_fims_tmb_internal<TMB_FIMS_REAL_TYPE>();
+        this->add_to_fims_tmb_internal<TMBAD_FIMS_TYPE>();
+    #else
     this->add_to_fims_tmb_internal<TMB_FIMS_REAL_TYPE>();
     this->add_to_fims_tmb_internal<TMB_FIMS_FIRST_ORDER>();
     this->add_to_fims_tmb_internal<TMB_FIMS_SECOND_ORDER>();
     this->add_to_fims_tmb_internal<TMB_FIMS_THIRD_ORDER>();
+    #endif
 
     return true;
   }
@@ -927,10 +937,15 @@ virtual std::string to_json() {
   }
 
   virtual bool add_to_fims_tmb() {
+    #ifdef TMBAD_FRAMEWORK
+        this->add_to_fims_tmb_internal<TMB_FIMS_REAL_TYPE>();
+        this->add_to_fims_tmb_internal<TMBAD_FIMS_TYPE>();
+    #else
     this->add_to_fims_tmb_internal<TMB_FIMS_REAL_TYPE>();
     this->add_to_fims_tmb_internal<TMB_FIMS_FIRST_ORDER>();
     this->add_to_fims_tmb_internal<TMB_FIMS_SECOND_ORDER>();
     this->add_to_fims_tmb_internal<TMB_FIMS_THIRD_ORDER>();
+    #endif
 
     return true;
   }
---
+++ src/init.hpp
@@ -0,0 +1,44 @@
+/**
+ * @file init.hpp
+ * @brief An interface to dynamically load the functions.
+ * @copyright This file is part of the NOAA, National Marine Fisheries Service
+ * Fisheries Integrated Modeling System project. See LICENSE in the source
+ * folder for reuse information.
+ */
+#ifndef SRC_INIT_HPP
+#define SRC_INIT_HPP
+#include <R_ext/Rdynload.h>
+#include <stdlib.h>
+
+extern "C" {
+
+/**
+ * @brief TODO: Handles the initialization of the fims rcpp module.
+ * 
+ * @return SEXP 
+ */
+SEXP _rcpp_module_boot_fims();
+
+/**
+ * @brief Callback definition to load the FIMS module.
+ */
+static const R_CallMethodDef CallEntries[] = {
+    TMB_CALLDEFS,
+    {"_rcpp_module_boot_fims", (DL_FUNC)&_rcpp_module_boot_fims, 0},
+    {NULL, NULL, 0}};
+
+/**
+ * @brief FIMS shared object initializer.
+ * @param dll TODO: provide a brief description.
+ *
+ */
+void R_init_FIMS(DllInfo *dll) {
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+#ifdef TMB_CCALLABLES
+  TMB_CCALLABLES("FIMS");
+#endif
+}
+}
+
+#endif // SRC_INIT_HPP
