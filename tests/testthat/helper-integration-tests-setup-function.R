0f37ef536c877e9746f04eaee279771375a300e5
MOshima-PIFSC <megumi.oshima@noaa.gov>
2025-08-14 08:29:55 -1000

    adding SPM data

--- tests/testthat/helper-integration-tests-setup-function.R
+++ tests/testthat/helper-integration-tests-setup-function.R
@@ -599,13 +599,14 @@ setup_and_run_sp <- function(estimation_mode = TRUE,
                              map = list()) {
 
   
-#   # path to download the surplus prooduction test data
+# #   # path to download the surplus prooduction test data
 #   path_sp_data <- test_path("fixtures", "integration_test_data_sp.RData")
 #   download.file(
 #     "https://github.com/iagomosqueira/simtest_SP/raw/main/data/sims.RData",
 #     path_sp_data
 #   )
 #   # remotes::install_github("flr/FLCore")
+#   #load om object
 #   load(path_sp_data)
 
 #   # Simulated data and other information (e.g., number of iteration, end year, and start year)
@@ -720,164 +721,164 @@ setup_and_run_sp <- function(estimation_mode = TRUE,
 
 # Simulate data
 
-library(FishLife)
-library(SPMpriors)
-library(FLRef)
-library(FLife)
-library(patchwork)
+# library(FishLife)
+# library(SPMpriors)
+# library(FLRef)
+# library(FLife)
+# library(patchwork)
 
 
-# --- Life history
+# # --- Life history
 
-# A set of parameters are sampled from FishLife
+# # A set of parameters are sampled from FishLife
 
-alb <- flmvn_traits(Genus="Thunnus", Species="alalunga",
-  h=c(0.6,0.9), Plot=FALSE)$traits
+# alb <- flmvn_traits(Genus="Thunnus", Species="alalunga",
+#   h=c(0.6,0.9), Plot=FALSE)$traits
 
-# Other parameters are specified
+# # Other parameters are specified
 
-lh <- list(linf = alb[alb$trait=="Loo","mu.stk"],
-  k =  alb[alb$trait=="K","mu.stk"],
-  t0 = -0.5,
-  tm = alb[alb$trait=="tm","mu.stk"],
-  a = 0.00001,
-  b = 3.04,
-  tmax =  ceiling(alb[alb$trait=="tmax","mu.stk"]),
-  s = alb[alb$trait=="h","mu.stk"])
+# lh <- list(linf = alb[alb$trait=="Loo","mu.stk"],
+#   k =  alb[alb$trait=="K","mu.stk"],
+#   t0 = -0.5,
+#   tm = alb[alb$trait=="tm","mu.stk"],
+#   a = 0.00001,
+#   b = 3.04,
+#   tmax =  ceiling(alb[alb$trait=="tmax","mu.stk"]),
+#   s = alb[alb$trait=="h","mu.stk"])
 
-# Check the corresponding Von Bertalannfy growth curve
+# # Check the corresponding Von Bertalannfy growth curve
 
-lena <- with(lh, vonbert(linf, k, t0, age=0:tmax))
+# lena <- with(lh, vonbert(linf, k, t0, age=0:tmax))
 
-# M using Then (), with tmax1 = 22 and tmax2 = 12
+# # M using Then (), with tmax1 = 22 and tmax2 = 12
 
-lh$M <- 4.899 * lh$tmax ^- 0.916
+# lh$M <- 4.899 * lh$tmax ^- 0.916
 
-#  Leslie matrix `r`
+# #  Leslie matrix `r`
 
-bio <- with(lh, jbio(amax=tmax, nsexes=1, Loo=linf, k=k, t0=t0, aW=1000*a, bW=b,
-  mat=c(tm, tm*1.2, 1), M=M, h=s))
+# bio <- with(lh, jbio(amax=tmax, nsexes=1, Loo=linf, k=k, t0=t0, aW=1000*a, bW=b,
+#   mat=c(tm, tm*1.2, 1), M=M, h=s))
 
-r <- jbleslie(bio)$r
-
-
-# --- Generic OM for ALB
-
-# Life-history parameter set com,pleted by lhPar
-
-par <- with(lh, lhPar(FLPar(linf=linf, k=k, t0=t0, a=a, b=b, a50=2, s=s, m1=M)))
-
-# Constructor a basic `FLBRP()` (equilibrium) object.
-
-eql <- lhEql(par, range = c(min = 0, max = lh$tmax,
-  minfbar = 1, maxfbar = lh$tmax-1, plusgroup=lh$tmax))
-
-# Adjusts the inbuilt Gislason M to a scaled Lorenzen $M$
-
-m(eql)[] <- lorenzen(stock.wt(eql), Mref=lh$M, Aref=2)
-
- # Specifying a new maturity ogive
-
-mat(eql) <- newselex(mat(eql), FLPar(S50=lh$tm, mat95=lh$tm * 1.5, Smax=1000,
-  Dcv=0.5,Dmin=0.1))
-
-# Updates the stock recruitment steepness if biological parameters are modified
-
-params(eql)
-
-eql <- updsr(eql, s=lh$s) 
-
-params(eql)
-
-# Time horizon can be adjusted with `fbar` range, 1981-2020
-
-f0 <- 0.01
-
-fbar(eql) <- FLQuant(f0, dimnames=list(year=1951:2020))
-
-# Convert FLBRP into FLStock + SRR
-
-stk <- as(eql,"FLStock")
-units(stk) <- standardUnits(stk)
-
-srr <- as(as(eql,"predictModel"),"FLSR")
-
-# Set new selectivity parameters
-
-selpar <- FLPar(
-  # catch before maturity
-  S50=3, S95=4.2,
-  # Domed-shaped
-  Smax=7, Dcv=0.5, Dmin=0.2)
-
-# Modify selectivity
-
-harvest(stk) <-  f0 * newselex(harvest(stk), selpar)
-
-# Plots weight-at-age, maturity-at-age, natural mortality and selective
-
-ggplot(FLQuants(stk,"m","catch.sel","mat","catch.wt"))+
-  geom_line(aes(age,data))+
-  facet_wrap(~qname,scale="free")+theme_bw()
+# r <- jbleslie(bio)$r
 
 
-# --- Estimating priors from FLStock and SRR
+# # --- Generic OM for ALB
 
-#' First, it is straight forward to compute the intrinsic rate of population increase from a Leslie for a specified steepness value. However, this $r$ estimate ignores selectivity and should only be used in the context of a Schaefer model with $F_{MSY} = r/2$.  
+# # Life-history parameter set com,pleted by lhPar
 
-r.leslie <- mean(productivity(stk, s=lh$s)$r)
+# par <- with(lh, lhPar(FLPar(linf=linf, k=k, t0=t0, a=a, b=b, a50=2, s=s, m1=M)))
 
-# compare
-c(r=r, r.leslie=r.leslie, FMSY=r.leslie / 2)
+# # Constructor a basic `FLBRP()` (equilibrium) object.
 
-#' An alternative is to estimate $r$ and the shape parameter $n$ as function $MSY$, $VB_{MSY}$ and $VB_0$ from an age-structured equilibrium model (ASEM), where $BB$ denotes that the vulnarable (or exploitable) biomass as function of selectivity. 
+# eql <- lhEql(par, range = c(min = 0, max = lh$tmax,
+#   minfbar = 1, maxfbar = lh$tmax-1, plusgroup=lh$tmax))
 
-brp <- brp(FLBRP(stk, srr))
+# # Adjusts the inbuilt Gislason M to a scaled Lorenzen $M$
 
-r.pella <- asem2spm(brp)
-r.pella
+# m(eql)[] <- lorenzen(stock.wt(eql), Mref=lh$M, Aref=2)
 
-plotpf(brp, rel=FALSE) + plotpf(brp, rel=TRUE) + plot_layout(guides = "collect")
+#  # Specifying a new maturity ogive
+
+# mat(eql) <- newselex(mat(eql), FLPar(S50=lh$tm, mat95=lh$tm * 1.5, Smax=1000,
+#   Dcv=0.5,Dmin=0.1))
+
+# # Updates the stock recruitment steepness if biological parameters are modified
+
+# params(eql)
+
+# eql <- updsr(eql, s=lh$s) 
+
+# params(eql)
+
+# # Time horizon can be adjusted with `fbar` range, 1981-2020
+
+# f0 <- 0.01
+
+# fbar(eql) <- FLQuant(f0, dimnames=list(year=1951:2020))
+
+# # Convert FLBRP into FLStock + SRR
+
+# stk <- as(eql,"FLStock")
+# units(stk) <- standardUnits(stk)
+
+# srr <- as(as(eql,"predictModel"),"FLSR")
+
+# # Set new selectivity parameters
+
+# selpar <- FLPar(
+#   # catch before maturity
+#   S50=3, S95=4.2,
+#   # Domed-shaped
+#   Smax=7, Dcv=0.5, Dmin=0.2)
+
+# # Modify selectivity
+
+# harvest(stk) <-  f0 * newselex(harvest(stk), selpar)
+
+# # Plots weight-at-age, maturity-at-age, natural mortality and selective
+
+# ggplot(FLQuants(stk,"m","catch.sel","mat","catch.wt"))+
+#   geom_line(aes(age,data))+
+#   facet_wrap(~qname,scale="free")+theme_bw()
 
 
-# --- Simulating stock dynamics with evolutionary F-trajectories
+# # --- Estimating priors from FLStock and SRR
 
-# Estimate refpts
-brp <- computeFbrp(stk, srr, proxy="msy", blim=0.3, type="btgt")
-stk <- FLStockR(stk)
-refpts(stk) <- Fbrp(brp)
+# #' First, it is straight forward to compute the intrinsic rate of population increase from a Leslie for a specified steepness value. However, this $r$ estimate ignores selectivity and should only be used in the context of a Schaefer model with $F_{MSY} = r/2$.  
 
-ploteq(brp)
+# r.leslie <- mean(productivity(stk, s=lh$s)$r)
 
-its <- 100 
+# # compare
+# c(r=r, r.leslie=r.leslie, FMSY=r.leslie / 2)
 
-# SSB0 for brp
-b0 <- an(refpts(eql)["virgin","ssb"])
+# #' An alternative is to estimate $r$ and the shape parameter $n$ as function $MSY$, $VB_{MSY}$ and $VB_0$ from an age-structured equilibrium model (ASEM), where $BB$ denotes that the vulnarable (or exploitable) biomass as function of selectivity. 
 
-# one-way downhill projection
-control = FLPar(Feq=0.15,Frate=0.05,Fsigma=0.15,SB0=b0,
-  minyear=dims(stk)$minyear+1,
-  maxyear=dims(stk)$maxyear,its=its)
+# brp <- brp(FLBRP(stk, srr))
 
-# Forecasted with random recruitment under an evolving F-trajectory
+# r.pella <- asem2spm(brp)
+# r.pella
 
-set.seed(1234)
-# Random recruitment deviations with sigR = 0.5 and AR1 rho = 0.3
-rec_devs = ar1rlnorm(0.3, 1951:2020, its, 0, 0.5)
-# propagate desired iterations
-stki <- propagate(stk, its)
-# create OM 
-om <- rffwd(stki, srr, control=control, deviances=rec_devs)
-refpts(om) <- refpts(stk)
+# plotpf(brp, rel=FALSE) + plotpf(brp, rel=TRUE) + plot_layout(guides = "collect")
 
-plotAdvice(om)
 
-# Generate LL CPUE, flat-toped selectivity
+# # --- Simulating stock dynamics with evolutionary F-trajectories
 
-cpue.sel <- newselex(harvest(stk),
-  FLPar(S50=3.5, mat95=4.2, Smax=100, Dcv=0.5, Dmin=0.05))
+# # Estimate refpts
+# brp <- computeFbrp(stk, srr, proxy="msy", blim=0.3, type="btgt")
+# stk <- FLStockR(stk)
+# refpts(stk) <- Fbrp(brp)
 
-idx <- window(bioidx.sim(om,sel=cpue.sel,sigma=0.25,q=0.001),start=1970)
+# ploteq(brp)
+
+# its <- 100 
+
+# # SSB0 for brp
+# b0 <- an(refpts(eql)["virgin","ssb"])
+
+# # one-way downhill projection
+# control = FLPar(Feq=0.15,Frate=0.05,Fsigma=0.15,SB0=b0,
+#   minyear=dims(stk)$minyear+1,
+#   maxyear=dims(stk)$maxyear,its=its)
+
+# # Forecasted with random recruitment under an evolving F-trajectory
+
+# set.seed(1234)
+# # Random recruitment deviations with sigR = 0.5 and AR1 rho = 0.3
+# rec_devs = ar1rlnorm(0.3, 1951:2020, its, 0, 0.5)
+# # propagate desired iterations
+# stki <- propagate(stk, its)
+# # create OM 
+# om <- rffwd(stki, srr, control=control, deviances=rec_devs)
+# refpts(om) <- refpts(stk)
+
+# plotAdvice(om)
+
+# # Generate LL CPUE, flat-toped selectivity
+
+# cpue.sel <- newselex(harvest(stk),
+#   FLPar(S50=3.5, mat95=4.2, Smax=100, Dcv=0.5, Dmin=0.05))
+
+# idx <- window(bioidx.sim(om,sel=cpue.sel,sigma=0.25,q=0.001),start=1970)
 
   # FIMS
   # create index module
