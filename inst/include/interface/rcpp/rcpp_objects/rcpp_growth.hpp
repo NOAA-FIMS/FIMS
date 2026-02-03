8c0655303b0335851b3e37ae88fa62fb865b2c4d
Matthew <matthew.supernaw@noaa.gov>
2026-02-03 13:08:27 -0500

    adds checks for make_map

--- inst/include/interface/rcpp/rcpp_objects/rcpp_growth.hpp
+++ inst/include/interface/rcpp/rcpp_objects/rcpp_growth.hpp
@@ -17,7 +17,7 @@
  * interfaces. This type should be inherited and not called from R directly.
  */
 class GrowthInterfaceBase : public FIMSRcppInterfaceBase {
- public:
+public:
   /**
    * @brief The static id of the GrowthInterfaceBase object.
    */
@@ -80,7 +80,7 @@ std::map<uint32_t, std::shared_ptr<GrowthInterfaceBase>>
  * age and growth is not actually estimated.
  */
 class EWAAGrowthInterface : public GrowthInterfaceBase {
- public:
+public:
   /**
    * @brief Weights (mt) for each age class.
    */
@@ -91,7 +91,7 @@ class EWAAGrowthInterface : public GrowthInterfaceBase {
   RealVector ages;
   /**
    * @brief An integer specifying the number of years.
-   * 
+   *
    */
   SharedInt n_years;
   /**
@@ -121,11 +121,8 @@ class EWAAGrowthInterface : public GrowthInterfaceBase {
    * @param other
    */
   EWAAGrowthInterface(const EWAAGrowthInterface &other)
-      : GrowthInterfaceBase(other),
-        weights(other.weights),
-        ages(other.ages),
-        ewaa(other.ewaa),
-        initialized(other.initialized) {}
+      : GrowthInterfaceBase(other), weights(other.weights), ages(other.ages),
+        ewaa(other.ewaa), initialized(other.initialized) {}
 
   /**
    * @brief The destructor.
@@ -145,120 +142,132 @@ class EWAAGrowthInterface : public GrowthInterfaceBase {
    * @param n_years An integer specifying the number of years.
    * @return std::map<T, T>.
    */
-  inline std::map<double, double> make_map(RealVector ages,
-                                           RealVector weights,
+  inline std::map<double, double> make_map(RealVector ages, RealVector weights,
                                            SharedInt n_years) {
     std::map<int, std::map<double, double>> mymap;
-    for (uint32_t y = 0; y < n_years.get(); y++) {
-      for (uint32_t i = 0; i < ages.size(); i++) {
-        mymap[y][ages[i]] = weights[i];
+
+    if((weights.size() != ages.size() * n_years.get()) &&
+       (weights.size() != ages.size())) {
+      Rcpp::stop("weights size does not match ages size or ages size times "
+                 "n_years");
+    }else if (weights.size() != ages.size() * n_years.get()) {
+      for (uint32_t y = 0; y < n_years.get(); y++) {
+        for (uint32_t i = 0; i < ages.size(); i++) {
+          mymap[y][ages[i]] = weights[i];
+        }
+      }
+    } else if (weights.size() == ages.size() * n_years.get()) {
+
+      for (uint32_t y = 0; y < n_years.get(); y++) {
+        for (uint32_t i = 0; i < ages.size(); i++) {
+          mymap[y][ages[i]] = weights[y * ages.size() + i];
+        }
       }
     }
-    return mymap;
-  }
 
-  /**
-   * @brief Evaluate the growth using empirical weight at age.
-   * @param age The age at of the individual to evaluate weight.
-   * @details This can be called from R using ewaagrowth.evaluate(age).
-   */
-  virtual double evaluate(double age) {
-    fims_popdy::EWAAGrowth<double> EWAAGrowth;
+    /**
+     * @brief Evaluate the growth using empirical weight at age.
+     * @param age The age at of the individual to evaluate weight.
+     * @details This can be called from R using ewaagrowth.evaluate(age).
+     */
+    virtual double evaluate(double age) {
+      fims_popdy::EWAAGrowth<double> EWAAGrowth;
 
-    if (initialized == false) {
-      EWAAGrowth.ewaa = make_map(this->ages, this->weights, this->n_years);
-      initialized = true;
-    } else {
-      Rcpp::stop("this empirical weight at age object is already initialized");
+      if (initialized == false) {
+        EWAAGrowth.ewaa = make_map(this->ages, this->weights, this->n_years);
+        initialized = true;
+      } else {
+        Rcpp::stop(
+            "this empirical weight at age object is already initialized");
+      }
+      return EWAAGrowth.evaluate(0, age);
     }
-    return EWAAGrowth.evaluate(0, age);
-  }
 
-  /**
-   * @brief Converts the data to json representation for the output.
-   * @return A string is returned specifying that the module relates to the
-   * growth interface with empirical weight at age. It also returns the ID, the
-   * rank of 1, the dimensions, age bins, and the calculated values themselves.
-   * This string is formatted for a json file.
-   */
-  virtual std::string to_json() {
-    std::stringstream ss;
-    ss << "{\n";
-    ss << " \"module_name\": \"Growth\",\n";
-    ss << " \"module_type\": \"EWAA\",\n";
-    ss << " \"module_id\":" << this->id << ",\n";
-    ss << " \"parameters\": [\n{\n";
-    ss << " \"name\": null,\n";
-    ss << " \"id\": null,\n";
-    ss << " \"type\": \"vector\",\n";
-    // TODO: change the dimensionality
-    ss << " \"dimensionality\": {\n";
-    ss << "  \"header\": [\"n_ages\"],\n";
-    ss << "  \"dimensions\": [" << this->ages.size() << "]\n},\n";
+    /**
+     * @brief Converts the data to json representation for the output.
+     * @return A string is returned specifying that the module relates to the
+     * growth interface with empirical weight at age. It also returns the ID,
+     * the rank of 1, the dimensions, age bins, and the calculated values
+     * themselves. This string is formatted for a json file.
+     */
+    virtual std::string to_json() {
+      std::stringstream ss;
+      ss << "{\n";
+      ss << " \"module_name\": \"Growth\",\n";
+      ss << " \"module_type\": \"EWAA\",\n";
+      ss << " \"module_id\":" << this->id << ",\n";
+      ss << " \"parameters\": [\n{\n";
+      ss << " \"name\": null,\n";
+      ss << " \"id\": null,\n";
+      ss << " \"type\": \"vector\",\n";
+      // TODO: change the dimensionality
+      ss << " \"dimensionality\": {\n";
+      ss << "  \"header\": [\"n_ages\"],\n";
+      ss << "  \"dimensions\": [" << this->ages.size() << "]\n},\n";
 
-    ss << " \"values\": [\n";
-    for (size_t i = 0; i < weights.size() - 1; i++) {
+      ss << " \"values\": [\n";
+      for (size_t i = 0; i < weights.size() - 1; i++) {
+        ss << "{\n";
+        ss << "\"id\": null,\n";
+        ss << "\"value\": " << weights[i] << ",\n";
+        ss << "\"estimated_value\": " << weights[i] << ",\n";
+        ss << "\"uncertainty\": " << 0 << ",\n";
+        ss << "\"min\": \"-Infinity\",\n";
+        ss << "\"max\": \"Infinity\",\n";
+        ss << "\"estimation_type\": \"constant\"\n";
+        ss << "},\n";
+      }
       ss << "{\n";
       ss << "\"id\": null,\n";
-      ss << "\"value\": " << weights[i] << ",\n";
-      ss << "\"estimated_value\": " << weights[i] << ",\n";
+      ss << "\"value\": " << weights[weights.size() - 1] << ",\n";
+      ss << "\"estimated_value\": " << weights[weights.size() - 1] << ",\n";
       ss << "\"uncertainty\": " << 0 << ",\n";
       ss << "\"min\": \"-Infinity\",\n";
       ss << "\"max\": \"Infinity\",\n";
       ss << "\"estimation_type\": \"constant\"\n";
-      ss << "},\n";
+      ss << "}\n]\n";
+      ss << "}\n]\n}\n";
+      return ss.str();
     }
-    ss << "{\n";
-    ss << "\"id\": null,\n";
-    ss << "\"value\": " << weights[weights.size() - 1] << ",\n";
-    ss << "\"estimated_value\": " << weights[weights.size() - 1] << ",\n";
-    ss << "\"uncertainty\": " << 0 << ",\n";
-    ss << "\"min\": \"-Infinity\",\n";
-    ss << "\"max\": \"Infinity\",\n";
-    ss << "\"estimation_type\": \"constant\"\n";
-    ss << "}\n]\n";
-    ss << "}\n]\n}\n";
-    return ss.str();
-  }
 
 #ifdef TMB_MODEL
 
-  template <typename Type>
-  bool add_to_fims_tmb_internal() {
-    std::shared_ptr<fims_info::Information<Type>> info =
-        fims_info::Information<Type>::GetInstance();
+    template <typename Type> bool add_to_fims_tmb_internal() {
+      std::shared_ptr<fims_info::Information<Type>> info =
+          fims_info::Information<Type>::GetInstance();
 
-    std::shared_ptr<fims_popdy::EWAAGrowth<Type>> ewaa_growth =
-        std::make_shared<fims_popdy::EWAAGrowth<Type>>();
+      std::shared_ptr<fims_popdy::EWAAGrowth<Type>> ewaa_growth =
+          std::make_shared<fims_popdy::EWAAGrowth<Type>>();
 
-    // set relative info
-    ewaa_growth->id = this->id;
-    ewaa_growth->ewaa = make_map(this->ages, this->weights, this->n_years);  // this->ewaa;
-    // add to Information
-    info->growth_models[ewaa_growth->id] = ewaa_growth;
+      // set relative info
+      ewaa_growth->id = this->id;
+      ewaa_growth->ewaa =
+          make_map(this->ages, this->weights, this->n_years); // this->ewaa;
+      // add to Information
+      info->growth_models[ewaa_growth->id] = ewaa_growth;
 
-    return true;
-  }
+      return true;
+    }
 
-  /**
-   * @brief Adds the parameters to the TMB model.
-   * @return A boolean of true.
-   */
-  virtual bool add_to_fims_tmb() {
-    this->add_to_fims_tmb_internal<TMB_FIMS_REAL_TYPE>();
+    /**
+     * @brief Adds the parameters to the TMB model.
+     * @return A boolean of true.
+     */
+    virtual bool add_to_fims_tmb() {
+      this->add_to_fims_tmb_internal<TMB_FIMS_REAL_TYPE>();
 #ifdef TMBAD_FRAMEWORK
-    this->add_to_fims_tmb_internal<TMBAD_FIMS_TYPE>();
+      this->add_to_fims_tmb_internal<TMBAD_FIMS_TYPE>();
 #else
-    this->add_to_fims_tmb_internal<TMB_FIMS_REAL_TYPE>();
-    this->add_to_fims_tmb_internal<TMB_FIMS_FIRST_ORDER>();
-    this->add_to_fims_tmb_internal<TMB_FIMS_SECOND_ORDER>();
-    this->add_to_fims_tmb_internal<TMB_FIMS_THIRD_ORDER>();
+      this->add_to_fims_tmb_internal<TMB_FIMS_REAL_TYPE>();
+      this->add_to_fims_tmb_internal<TMB_FIMS_FIRST_ORDER>();
+      this->add_to_fims_tmb_internal<TMB_FIMS_SECOND_ORDER>();
+      this->add_to_fims_tmb_internal<TMB_FIMS_THIRD_ORDER>();
 #endif
 
-    return true;
-  }
+      return true;
+    }
 
 #endif
-};
+  };
 
 #endif
