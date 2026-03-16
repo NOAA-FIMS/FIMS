fd95c37fe8641fd1c2221d909fd3eac0e9d35830
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2026-03-16 01:30:47 -0700

    fix: broken links to doxygen
    Now the doxygen is in the pkgdown
    
    Part of #994

--- R/Rcpp_exports.R
+++ R/Rcpp_exports.R
@@ -5,35 +5,35 @@
 #' docs.
 #'
 #' @section C++ Documentation:
-#' [NOAA-FIMS C++ Documentation](https://noaa-fims.github.io/doxygen/)
+#' [NOAA-FIMS C++ Documentation](https://noaa-fims.github.io/FIMS/doxygen/)
 #'
 #' @name Cpp_classes
 #' @aliases AgeComp BevertonHoltRecruitment CatchAtAge DlnormDistribution DmultinomDistribution DnormDistribution DoubleLogisticSelectivity EWAAGrowth Fleet Index Landings LengthComp LogDevsRecruitmentProcess LogRRecruitmentProcess LogisticMaturity LogisticSelectivity Parameter ParameterVector Population RealVector SharedInt SharedReal SharedString
 #'
 #' @details
-#' - [AgeComp](https://noaa-fims.github.io/doxygen/classAgeCompDataInterface.html)
-#' - [BevertonHoltRecruitment](https://noaa-fims.github.io/doxygen/classBevertonHoltRecruitmentInterface.html)
-#' - [CatchAtAge](https://noaa-fims.github.io/doxygen/classCatchAtAgeInterface.html)
-#' - [DlnormDistribution](https://noaa-fims.github.io/doxygen/classDlnormDistributionsInterface.html)
-#' - [DmultinomDistribution](https://noaa-fims.github.io/doxygen/classDmultinomDistributionsInterface.html)
-#' - [DnormDistribution](https://noaa-fims.github.io/doxygen/classDnormDistributionsInterface.html)
-#' - [DoubleLogisticSelectivity](https://noaa-fims.github.io/doxygen/classDoubleLogisticSelectivityInterface.html)
-#' - [EWAAGrowth](https://noaa-fims.github.io/doxygen/classEWAAGrowthInterface.html)
-#' - [Fleet](https://noaa-fims.github.io/doxygen/classFleetInterface.html)
-#' - [Index](https://noaa-fims.github.io/doxygen/classIndexDataInterface.html)
-#' - [Landings](https://noaa-fims.github.io/doxygen/classLandingsDataInterface.html)
-#' - [LengthComp](https://noaa-fims.github.io/doxygen/classLengthCompDataInterface.html)
-#' - [LogDevsRecruitmentProcess](https://noaa-fims.github.io/doxygen/classLogDevsRecruitmentInterface.html)
-#' - [LogRRecruitmentProcess](https://noaa-fims.github.io/doxygen/classLogRRecruitmentInterface.html)
-#' - [LogisticMaturity](https://noaa-fims.github.io/doxygen/classLogisticMaturityInterface.html)
-#' - [LogisticSelectivity](https://noaa-fims.github.io/doxygen/classLogisticSelectivityInterface.html)
-#' - [Parameter](https://noaa-fims.github.io/doxygen/classParameter.html)
-#' - [ParameterVector](https://noaa-fims.github.io/doxygen/classParameterVector.html)
-#' - [Population](https://noaa-fims.github.io/doxygen/classPopulationInterface.html)
-#' - [RealVector](https://noaa-fims.github.io/doxygen/classRealVector.html)
-#' - [SharedInt](https://noaa-fims.github.io/doxygen/classSharedInt.html)
-#' - [SharedReal](https://noaa-fims.github.io/doxygen/classSharedReal.html)
-#' - [SharedString](https://noaa-fims.github.io/doxygen/classSharedString.html)
+#' - [AgeComp](https://noaa-fims.github.io/FIMS/doxygen/classAgeCompDataInterface.html)
+#' - [BevertonHoltRecruitment](https://noaa-fims.github.io/FIMS/doxygen/classBevertonHoltRecruitmentInterface.html)
+#' - [CatchAtAge](https://noaa-fims.github.io/FIMS/doxygen/classCatchAtAgeInterface.html)
+#' - [DlnormDistribution](https://noaa-fims.github.io/FIMS/doxygen/classDlnormDistributionsInterface.html)
+#' - [DmultinomDistribution](https://noaa-fims.github.io/FIMS/doxygen/classDmultinomDistributionsInterface.html)
+#' - [DnormDistribution](https://noaa-fims.github.io/FIMS/doxygen/classDnormDistributionsInterface.html)
+#' - [DoubleLogisticSelectivity](https://noaa-fims.github.io/FIMS/doxygen/classDoubleLogisticSelectivityInterface.html)
+#' - [EWAAGrowth](https://noaa-fims.github.io/FIMS/doxygen/classEWAAGrowthInterface.html)
+#' - [Fleet](https://noaa-fims.github.io/FIMS/doxygen/classFleetInterface.html)
+#' - [Index](https://noaa-fims.github.io/FIMS/doxygen/classIndexDataInterface.html)
+#' - [Landings](https://noaa-fims.github.io/FIMS/doxygen/classLandingsDataInterface.html)
+#' - [LengthComp](https://noaa-fims.github.io/FIMS/doxygen/classLengthCompDataInterface.html)
+#' - [LogDevsRecruitmentProcess](https://noaa-fims.github.io/FIMS/doxygen/classLogDevsRecruitmentInterface.html)
+#' - [LogRRecruitmentProcess](https://noaa-fims.github.io/FIMS/doxygen/classLogRRecruitmentInterface.html)
+#' - [LogisticMaturity](https://noaa-fims.github.io/FIMS/doxygen/classLogisticMaturityInterface.html)
+#' - [LogisticSelectivity](https://noaa-fims.github.io/FIMS/doxygen/classLogisticSelectivityInterface.html)
+#' - [Parameter](https://noaa-fims.github.io/FIMS/doxygen/classParameter.html)
+#' - [ParameterVector](https://noaa-fims.github.io/FIMS/doxygen/classParameterVector.html)
+#' - [Population](https://noaa-fims.github.io/FIMS/doxygen/classPopulationInterface.html)
+#' - [RealVector](https://noaa-fims.github.io/FIMS/doxygen/classRealVector.html)
+#' - [SharedInt](https://noaa-fims.github.io/FIMS/doxygen/classSharedInt.html)
+#' - [SharedReal](https://noaa-fims.github.io/FIMS/doxygen/classSharedReal.html)
+#' - [SharedString](https://noaa-fims.github.io/FIMS/doxygen/classSharedString.html)
 NULL
 
 #' C++ Functions Exported via Rcpp
@@ -43,27 +43,27 @@ NULL
 #' C++ docs.
 #'
 #' @section C++ Documentation:
-#' [NOAA-FIMS C++ Documentation](https://noaa-fims.github.io/doxygen/)
+#' [NOAA-FIMS C++ Documentation](https://noaa-fims.github.io/FIMS/doxygen/)
 #'
 #' @name Cpp_functions
 #' @aliases clear get_fixed get_log get_log_errors get_log_warnings get_parameter_names get_random get_random_names inv_logit log_error log_info log_warning logit set_fixed set_log_throw_on_error set_random CreateTMBModel
 #'
 #' @details
-#' - [clear](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [get_fixed](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [get_log](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [get_log_errors](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [get_log_warnings](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [get_parameter_names](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [get_random](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [get_random_names](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [inv_logit](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [log_error](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [log_info](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [log_warning](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [logit](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [set_fixed](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [set_log_throw_on_error](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [set_random](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
-#' - [CreateTMBModel](https://noaa-fims.github.io/doxygen/rcpp__interface_8hpp.html)
+#' - [clear](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [get_fixed](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [get_log](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [get_log_errors](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [get_log_warnings](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [get_parameter_names](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [get_random](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [get_random_names](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [inv_logit](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [log_error](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [log_info](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [log_warning](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [logit](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [set_fixed](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [set_log_throw_on_error](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [set_random](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
+#' - [CreateTMBModel](https://noaa-fims.github.io/FIMS/doxygen/rcpp__interface_8hpp.html)
 NULL
