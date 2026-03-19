96dc0dbfc48940f2c9969ca060d10be828c0ac1e
Matthew <matthew.supernaw@noaa.gov>
2026-03-19 15:08:37 -0400

    removed unnecessary log entries

--- inst/include/common/model.hpp
+++ inst/include/common/model.hpp
@@ -61,8 +61,8 @@ class Model {  // may need singleton
     // Check if fims_information is set
     if (this->fims_information == nullptr) {
       FIMS_ERROR_LOG(
-          "fims_information is not set. Please set fims_information before "
-          "calling Evaluate().");
+          "Model::fims_information is NULL. Ensure that the Model instance 
+          is properly initialized with CreateModel() before calling Evaluate().");
       return jnll;
     }
 
@@ -83,7 +83,6 @@ class Model {  // may need singleton
     typename fims_info::Information<Type>::density_components_iterator d_it;
     int nll_vec_idx = 0;
     size_t n_priors = 0;
-    FIMS_INFO_LOG("Begin evaluating prior densities.")
     for (d_it = this->fims_information->density_components.begin();
          d_it != this->fims_information->density_components.end(); ++d_it) {
       std::shared_ptr<fims_distributions::DensityComponentBase<Type>> d =
@@ -98,11 +97,7 @@ class Model {  // may need singleton
         nll_vec_idx += 1;
       }
     }
-    FIMS_INFO_LOG(
-        "Model: Finished evaluating prior distributions. The jnll after "
-        "evaluating " +
-        fims::to_string(n_priors) + " priors is: " + fims::to_string(jnll));
-
+  
     // Loop over densities and evaluate joint negative log-likelihoods for
     // random effects
     size_t n_random_effects = 0;
@@ -120,11 +115,6 @@ class Model {  // may need singleton
         nll_vec_idx += 1;
       }
     }
-    FIMS_INFO_LOG(
-        "Model: Finished evaluating random effect distributions. The jnll "
-        "after evaluating priors and " +
-        fims::to_string(n_random_effects) +
-        " random_effects is: " + fims::to_string(jnll));
 
     // Loop over and evaluate data joint negative log-likelihoods
     int n_data = 0;
