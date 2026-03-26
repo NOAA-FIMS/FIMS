b4c4d355443583032931c5a79dfe8451cec05688
JonBrodziak <jon.brodziak@noaa.gov>
2026-03-25 17:23:54 -1000

    Upload rcpp_distribution.hpp from dev-diric-multinom

--- inst/include/interface/rcpp/rcpp_objects/rcpp_distribution.hpp
+++ inst/include/interface/rcpp/rcpp_objects/rcpp_distribution.hpp
@@ -1138,4 +1138,291 @@ class DmultinomDistributionsInterface : public DistributionsInterfaceBase {
 #endif
 };
 
+class DDirichletMultinomialDistributionsInterface : public DistributionsInterfaceBase {
+ public:
+  /**
+   * @brief Observed data, which should be a vector of length K of integers.
+   */
+  ParameterVector observed_values;
+  /**
+   * @brief The expected values, which should be a vector of length K where
+   * each value specifies the probability of class k. Note that, unlike in R,
+   * these probabilities must sum to 1.0.
+   */
+  ParameterVector expected_values;
+  /**
+   * @brief The dimensions of the number of rows and columns of the
+   * multivariate dataset.
+   */
+  RealVector dims;
+  Parameter theta;
+  /**
+   * @brief Vector that records the individual log probability function for each
+   * observation.
+   */
+  RealVector lpdf_vec; /**< The vector */
+
+  /**
+   * @brief TODO: document this.
+   *
+   */
+  SharedString notes;
+
+  /**
+   * @brief The constructor.
+   */
+  DDirichletMultinomialDistributionsInterface() : DistributionsInterfaceBase() {
+    DistributionsInterfaceBase::live_objects[this->id_m] =
+        std::make_shared<DirichletMultinomialDistributionsInterface>(*this);
+    FIMSRcppInterfaceBase::fims_interface_objects.push_back(
+        DistributionsInterfaceBase::live_objects[this->id_m]);
+  }
+
+  /**
+   * @brief Construct a new DdirichletMultinomial Distributions Interface object
+   *
+   * @param other
+   */
+  DirichletMultinomialDistributionsInterface(const DirichletMultinomialDistributionsInterface &other)
+      : DistributionsInterfaceBase(other),
+        observed_values(other.observed_values),
+        expected_values(other.expected_values),
+        dims(other.dims),
+        lpdf_vec(other.lpdf_vec),
+        notes(other.notes) {}
+
+  /**
+   * @brief The destructor.
+   */
+  virtual ~DDirichletMultinomialDistributionsInterface() {}
+  /**
+   * @brief Gets the ID of the interface base object.
+   * @return The ID.
+   */
+  virtual uint32_t get_id() { return this->id_m; }
+
+  /**
+   * @brief Set the unique ID for the observed data object.
+   * @param observed_data_id Unique ID for the observed data object.
+   */
+  virtual bool set_observed_data(int observed_data_id) {
+    this->interface_observed_data_id_m.set(observed_data_id);
+    return true;
+  }
+
+  /**
+   * @brief Sets pointers for data observations, random effects, or priors.
+   *
+   * @param input_type String that sets whether the distribution type is for
+   * priors, random effects, or data.
+   * @param ids Vector of unique ids for each linked parameter(s), derived
+   * value(s), or observed data vector.
+   */
+  virtual bool set_distribution_links(std::string input_type,
+                                      Rcpp::IntegerVector ids) {
+    this->input_type_m.set(input_type);
+    this->key_m->resize(ids.size());
+    for (R_xlen_t i = 0; i < ids.size(); i++) {
+      this->key_m->at(i) = ids[i];
+    }
+    return true;
+  }
+
+  /**
+   * @brief Set the note object
+   *
+   * @param note
+   */
+  void set_note(std::string note) { this->notes.set(note); }
+
+  /**
+   * @brief
+   *
+   * @return double
+   */
+  virtual double evaluate() {
+    fims_distributions::Dirichlet_multinomialLPMF<double> dirichlet_multinomial;
+    // Declare TMBVector in this scope
+    dirichlet_multinomial.observed_values.resize(this->observed_values.size());
+    dirichlet_multinomial.expected_values.resize(this->expected_values.size());
+    dirichlet_multinomial.theta = this->theta;
+    for (size_t i = 0; i < observed_values.size(); i++) {
+      dirichlet_multinomial.observed_values[i] = this->observed_values[i].initial_value_m;
+    }
+    for (size_t i = 0; i < expected_values.size(); i++) {
+      dirichlet_multinomial.expected_values[i] = this->expected_values[i].initial_value_m;
+    }
+    dirichlet_multinomial.dims.resize(2);
+    dirichlet_multinomial.dims[0] = this->dims[0];
+    dirichlet_multinomial.dims[1] = this->dims[1];
+    return dirichlet_multinomial.evaluate();
+  }
+
+  void finalize() {
+    if (this->finalized) {
+      // log warning that finalize has been called more than once.
+      FIMS_WARNING_LOG("DDirichletMultinomialDistributions  " +
+                       fims::to_string(this->id_m) +
+                       " has been finalized already.");
+    }
+
+    this->finalized = true;  // indicate this has been called already
+
+    std::shared_ptr<fims_info::Information<double>> info =
+        fims_info::Information<double>::GetInstance();
+
+    fims_info::Information<double>::density_components_iterator it;
+
+    // search for density component in Information
+    it = info->density_components.find(this->id_m);
+    // if not found, just return
+    if (it == info->density_components.end()) {
+      FIMS_WARNING_LOG("DDirichletMultinomialDistributions " + fims::to_string(this->id_m) +
+                       " not found in Information.");
+      return;
+    } else {
+      std::shared_ptr<fims_distributions::Dirichlet_multinomialLPMF<double>> dirichlet_multinomial =
+          std::dynamic_pointer_cast<
+              fims_distributions::Dirichlet_multinomialLPMF<double>>(it->second);
+
+      this->lpdf_value = dirichlet_multinomial->lpdf;
+
+      size_t n_x = dirichlet_multinomial->lpdf_vec.size();
+      this->lpdf_vec = Rcpp::NumericVector(n_x);
+      if (this->expected_values.size() != n_x) {
+        this->expected_values.resize(n_x);
+      }
+      if (this->observed_values.size() != n_x) {
+        this->observed_values.resize(n_x);
+      }
+      for (size_t i = 0; i < this->lpdf_vec.size(); i++) {
+        this->lpdf_vec[i] = dirichlet_multinomial->lpdf_vec[i];
+        this->expected_values[i].final_value_m = dirichlet_multinomial->get_expected(i);
+        if (dirichlet_multinomial->input_type != "data") {
+          this->observed_values[i].final_value_m = dirichlet_multinomial->get_observed(i);
+        }
+      }
+      if (dirichlet_multinomial->input_type == "data") {
+        dims.resize(2);
+        dims[0] = dirichlet_multinomial->dims[0];
+        dims[1] = dirichlet_multinomial->dims[1];
+        for (size_t i = 0; i < dims[0]; i++) {
+          for (size_t j = 0; j < dims[1]; j++) {
+            size_t idx = (i * dims[1]) + j;
+            this->observed_values[idx].final_value_m = dirichlet_multinomial->get_observed(
+                static_cast<size_t>(i), static_cast<size_t>(j));
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * @brief Converts the data to json representation for the output.
+   * @return A string is returned specifying that the module relates to the
+   * distribution interface with a log_normal distribution. It also returns the
+   * ID and the natural log of the probability density function values
+   * themselves. This string is formatted for a json file.
+   */
+  virtual std::string to_json() {
+    std::stringstream ss;
+
+    ss << "{\n";
+    ss << " \"module_name\": \"density\",\n";
+    ss << " \"module_id\": " << this->id_m << ",\n";
+    ss << " \"module_type\": \"Dirichlet-multinomial\",\n";
+    ss << "\"observed_data_id\" : " << this->interface_observed_data_id_m
+       << ",\n";
+    ss << " \"input_type\" : \"" << this->input_type_m << "\",\n";
+    ss << " \"density_component\": {\n";
+    ss << "  \"lpdf_value\": " << this->lpdf_value << ",\n";
+    ss << "  \"value\":[";
+    if (this->lpdf_vec.size() == 0) {
+      ss << "],\n";
+    } else {
+      for (size_t i = 0; i < this->lpdf_vec.size() - 1; i++) {
+        ss << this->value_to_string(this->lpdf_vec[i]);
+        ss << ", ";
+      }
+      ss << this->value_to_string(this->lpdf_vec[this->lpdf_vec.size() - 1]);
+
+      ss << "],\n";
+    }
+    ss << "  \"expected_values\":[";
+    if (this->expected_values.size() == 0) {
+      ss << "],\n";
+    } else {
+      for (size_t i = 0; i < this->expected_values.size() - 1; i++) {
+        ss << this->value_to_string(this->expected_values[i].final_value_m)
+           << ", ";
+      }
+      ss << this->value_to_string(
+          this->expected_values[this->expected_values.size() - 1]
+              .final_value_m);
+
+      ss << "],\n";
+    }
+    // no log_sd_values for Dirichletmultinomial
+    ss << "  \"observed_values\":[";
+    if (this->observed_values.size() == 0) {
+      ss << "]\n";
+    } else {
+      for (size_t i = 0; i < this->observed_values.size() - 1; i++) {
+        ss << this->observed_values[i].final_value_m << ", ";
+      }
+      ss << this->observed_values[this->observed_values.size() - 1]
+                .final_value_m
+         << "]\n";
+    }
+    ss << " }}\n";
+    return ss.str();
+  }
+
+#ifdef TMB_MODEL
+
+  template <typename Type>
+  bool add_to_fims_tmb_internal() {
+    FIMS_INFO_LOG("Adding Dirichlet-multinomial to FIMS.");
+    std::shared_ptr<fims_info::Information<Type>> info =
+        fims_info::Information<Type>::GetInstance();
+
+    std::shared_ptr<fims_distributions::Dirichlet_multinomialLPMF<Type>> distribution =
+        std::make_shared<fims_distributions::Dirichlet_multinomialLPMF<Type>>();
+
+    distribution->id = this->id_m;
+    distribution->theta = this->theta.initial_value_m;
+    distribution->observed_data_id_m = interface_observed_data_id_m;
+    distribution->input_type = this->input_type_m;
+    distribution->key.resize(this->key_m->size());
+    for (size_t i = 0; i < this->key_m->size(); i++) {
+      distribution->key[i] = this->key_m->at(i);
+    }
+    distribution->observed_values.resize(this->observed_values.size());
+    for (size_t i = 0; i < this->observed_values.size(); i++) {
+      distribution->observed_values[i] =
+          this->observed_values[i].initial_value_m;
+    }
+    // set relative info
+    distribution->expected_values.resize(this->expected_values.size());
+    for (size_t i = 0; i < this->expected_values.size(); i++) {
+      distribution->expected_values[i] =
+          this->expected_values[i].initial_value_m;
+    }
+
+    info->density_components[distribution->id] = distribution;
+    FIMS_INFO_LOG("Done adding Dirichlet-multinomial to FIMS.");
+    return true;
+  }
+
+  virtual bool add_to_fims_tmb() {
+    this->add_to_fims_tmb_internal<TMB_FIMS_REAL_TYPE>();
+    this->add_to_fims_tmb_internal<TMBAD_FIMS_TYPE>();
+
+    return true;
+  }
+
+#endif
+};
+
+
 #endif
