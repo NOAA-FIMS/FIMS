101e3656b7dcd62c0a1f3e42472d9697c77e6a8f
e-perl-NOAA <elizabeth.gugliotti@noaa.gov>
2026-07-20 11:48:33 -0400

    implement Nathan's suggestion to make this function simpler in terms of how parameters are defined

--- inst/include/interface/rcpp/rcpp_objects/rcpp_dsem.hpp
+++ inst/include/interface/rcpp/rcpp_objects/rcpp_dsem.hpp
@@ -18,6 +18,8 @@ public:
     Rcpp::IntegerMatrix ram_matrix; 
     /** @brief Fixed starting values for paths when not estimating. */
     Rcpp::NumericVector ram_start_values;
+    /** @brief Vector of path coefficients (beta_z). */
+    ParameterVector beta_z;
     
     size_t n_time = 0;      /**< Total years */
     size_t n_variables = 0; /**< Total variables (e.g., recruitment, temp) */
@@ -47,6 +49,24 @@ public:
         builder->n_time = this->n_time;
         builder->n_variables = this->n_variables;
         
+        // Set up beta_z in the builder and register parameters
+        builder->beta_z.resize(this->beta_z.size());
+        for (size_t i = 0; i < this->beta_z.size(); ++i) {
+            builder->beta_z[i] = this->beta_z[i].initial_value_m;
+            if (this->beta_z[i].estimation_type_m.get() == "fixed_effects") {
+                std::stringstream ss;
+                ss << "dsem." << this->id << ".beta_z." << this->beta_z[i].id_m;
+                info->RegisterParameterName(ss.str());
+                info->RegisterParameter(builder->beta_z[i]);
+            }
+            if (this->beta_z[i].estimation_type_m.get() == "random_effects") {
+                std::stringstream ss;
+                ss << "dsem." << this->id << ".beta_z." << this->beta_z[i].id_m;
+                info->RegisterRandomEffectName(ss.str());
+                info->RegisterRandomEffect(builder->beta_z[i]);
+            }
+        }
+
         // Copy the RAM matrix into structured RAMPath objects [Plan]
         for (int r = 0; r < ram_matrix.nrow(); ++r) {
             typename fims_distributions::DSEMPrecisionMatrixBuilder<Type>::RAMPath path;
@@ -68,6 +88,20 @@ public:
         this->add_to_fims_tmb_internal<double>();
         this->add_to_fims_tmb_internal<TMBad::ad_aug>();
         return true;
+    }
+
+    virtual void finalize() {
+        if (this->finalized) return;
+        std::shared_ptr<fims_info::Information<double>> info =
+            fims_info::Information<double>::GetInstance();
+        auto it = info->dsem_builders.find(this->id);
+        if (it != info->dsem_builders.end()) {
+            auto builder = std::dynamic_pointer_cast<fims_distributions::DSEMPrecisionMatrixBuilder<double>>(it->second);
+            for (size_t i = 0; i < this->beta_z.size(); ++i) {
+                this->beta_z[i].final_value_m = builder->beta_z[i];
+            }
+        }
+        this->finalized = true;
     }
 #endif
 };
