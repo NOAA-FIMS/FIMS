c03b979f0c72567c604155084aa045b5d3f109b8
Bai Li - NOAA <59936250+Bai-Li-NOAA@users.noreply.github.com>
2026-07-30 13:46:08 +0000

    fix(fims-input-types): fix types not found error

--- R/fimsframe.R
+++ R/fimsframe.R
@@ -686,11 +686,11 @@ methods::setValidity(
     present_types <- unique(object@data[["type"]])
 
     # Issues warning if there are any unrecognized types
-    unknown_types <- sort(setdiff(present_types, fims_input_types))
+    unknown_types <- sort(setdiff(present_types, FIMS::fims_input_types))
     if (length(unknown_types) > 0) {
       cli::cli_warn(c(
         "!" = "Data contains unexpected type{?s}: {.var {unknown_types}}.",
-        "i" = "Allowed types are: {.var {fims_input_types}}.",
+        "i" = "Allowed types are: {.var {FIMS::fims_input_types}}.",
         "i" = "Model will run but check that data types are correct."
       ))
     }
