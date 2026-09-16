f4d08102e730b8cc8b91e41b1e99547843448e56
Matthew <matthew.supernaw@noaa.gov>
2026-09-16 13:19:54 -0400

    Extract native prior registration and simplify target resolution

--- tests/testthat/test-native-model-call.R
+++ tests/testthat/test-native-model-call.R
@@ -56,3 +56,43 @@ test_that("native priors link parameter vectors and register hyperparameters", {
     "cannot be random effects"
   )
 })
+
+test_that("native prior targets resolve every supported module and parameter", {
+  on.exit(native_clear(), add = TRUE)
+  cases <- list(
+    list(module = "Fleet", create = function() fleet_create(c(-2, -1), c(0, 1)),
+         parameters = c("log_Fmort", "log_q")),
+    list(module = "Population", create = function() population_create(
+      c(-2, -1), c(0, 1), c(5, 6)),
+      parameters = c("log_M", "log_f_multiplier", "log_init_naa")),
+    list(module = "Recruitment", create = function() recruitment_beverton_holt_create(
+      c(0, 1), c(5, 6), c(-0.1, 0.1)),
+      parameters = c("logit_steep", "log_rzero", "log_devs")),
+    list(module = "Maturity", create = function() maturity_logistic_create(
+      c(2, 3), c(0.5, 1)), parameters = c("inflection_point", "slope")),
+    list(module = "Selectivity", create = function() selectivity_logistic_create(
+      c(2, 3), c(0.5, 1)), parameters = c("inflection_point", "slope")),
+    list(module = "Selectivity", create = function() selectivity_double_logistic_create(
+      c(2, 3), c(0.5, 1), c(8, 9), c(0.5, 1)),
+      parameters = c("inflection_point_asc", "slope_asc", "inflection_point_desc", "slope_desc"))
+  )
+  for (case in cases) {
+    for (parameter in case$parameters) {
+      native_clear()
+      id <- case$create()
+      for (distribution in c("Dnorm", "Dlnorm")) {
+        expect_true(native_add_prior(
+          case$module, id, parameter, distribution = distribution,
+          mean = 1, log_sd = log(0.5)
+        ), info = paste(case$module, parameter, distribution))
+      }
+      expect_equal(native_information_model_counts()[["density_components"]], 2L)
+      expect_error(native_add_prior(case$module, id, parameter, mean = 1:3),
+                   "must have length 1 or match")
+      expect_error(native_add_prior(case$module, id, "missing"),
+                   "Could not find native prior target")
+      expect_error(native_add_prior(case$module, 2147483647L, parameter),
+                   "Could not find native prior target")
+    }
+  }
+})
